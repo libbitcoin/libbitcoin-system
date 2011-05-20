bf012ee742ba8d59ea40942cb0fdf0e155073533
genjix <fake@lol.u>
2011-05-20 07:20:04 +0200

    Peer uses a reference counted temporary buffer object instead of buffering a send queue inside the class and needing logic to deque items in handle_send.

---
+++ src/net/shared_const_buffer.h
@@ -0,0 +1,38 @@
+#ifndef LIBBITCOIN_NET_SHARED_CONST_BUFFER_H
+#define LIBBITCOIN_NET_SHARED_CONST_BUFFER_H
+
+#include <boost/asio.hpp>
+#include <vector>
+
+#include "net/serializer.h"
+
+namespace libbitcoin {
+namespace net {
+
+// A reference-counted non-modifiable buffer class.
+class shared_const_buffer
+{
+public:
+  // Construct from a stream object
+  explicit shared_const_buffer(const serializer::stream& user_data)
+    : data(new std::vector<char>(user_data.begin(), user_data.end())),
+      buffer(boost::asio::buffer(*data))
+  {
+  }
+
+  // Implement the ConstBufferSequence requirements.
+  typedef boost::asio::const_buffer value_type;
+  typedef const boost::asio::const_buffer* const_iterator;
+  const boost::asio::const_buffer* begin() const { return &buffer; }
+  const boost::asio::const_buffer* end() const { return &buffer + 1; }
+
+private:
+  boost::shared_ptr<std::vector<char>> data;
+  boost::asio::const_buffer buffer;
+};
+
+} // net
+} // libbitcoin
+
+#endif
+
