e76aadd1f7148d2a04c90d3cb6adc10a665938f2
genjix <fake@lol.u>
2011-05-28 05:35:03 +0200

    Added storage base class.

--- include/bitcoin/storage/storage.hpp
+++ include/bitcoin/storage/storage.hpp
@@ -1,4 +1,18 @@
 #ifndef LIBBITCOIN_STORAGE_STORAGE_H
 #define LIBBITCOIN_STORAGE_STORAGE_H
+
+namespace libbitcoin {
+namespace storage {
+
+#include <boost/utility.hpp>
+
+class storage : boost::noncopyable
+{
+public:
+};
+
+} // storage
+} // libbitcoin
+
 #endif
 
