b7cdfb3f91c8b0760d278fbd0b675c1d9f0b8922
genjix <fake@lol.u>
2011-05-29 03:26:27 +0200

    Added demonstration convenience header.

---
+++ include/bitcoin/bitcoin.hpp
@@ -0,0 +1,11 @@
+#ifndef LIBBITCOIN_BITCOIN_H
+#define LIBBITCOIN_BITCOIN_H
+
+// Convenience header that includes everything
+// Not to be used internally. For API users.
+#include "bitcoin/block.hpp"
+#include "bitcoin/script.hpp"
+// ... And so on
+
+#endif
+
