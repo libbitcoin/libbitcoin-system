5e8e2b234720a0ea37fe5dc4c7f922d61a2c3df8
genjix <fake@lol.u>
2011-08-14 01:41:33 +0000

    poller has quicker wait time now.

--- tests/psql.cpp
+++ tests/psql.cpp
@@ -98,7 +98,7 @@ void recv_block(std::error_code ec, libbitcoin::message::block block, psql_ptr p
         log_error() << ec.message();
         exit(0);
     }
-    if (block_depth == (180 + 100))
+    if (block_depth == (180 + 1000))
     {
         exit(0);
     }
