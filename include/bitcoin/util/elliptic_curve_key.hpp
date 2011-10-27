4e8e7dc2ba6a725d91d1f24e80002b827c86b06d
genjix <fake@lol.u>
2011-10-27 12:21:55 +0000

    Completed EC key class + unit test.

--- include/bitcoin/util/elliptic_curve_key.hpp
+++ include/bitcoin/util/elliptic_curve_key.hpp
@@ -8,13 +8,23 @@
 
 namespace libbitcoin {
 
+typedef data_chunk private_data;
+
 class elliptic_curve_key
 {
 public:
     elliptic_curve_key();
+    elliptic_curve_key(const elliptic_curve_key& other) = delete;
+    elliptic_curve_key& operator=(const elliptic_curve_key& other) = delete;
+    ~elliptic_curve_key();
 
-    bool set_public_key(data_chunk pubkey);
-    bool verify(hash_digest hash, data_chunk signature);
+    bool set_public_key(const data_chunk& pubkey);
+    bool verify(hash_digest hash, const data_chunk& signature);
+
+    bool new_key_pair();
+    bool set_private_key(const data_chunk& privkey);
+    private_data get_private_key() const;
+    data_chunk sign(hash_digest hash) const;
 
 private:
     bool initialize();
--- src/util/elliptic_curve_key.cpp
+++ src/util/elliptic_curve_key.cpp
@@ -5,6 +5,8 @@
 #include <openssl/ecdsa.h>
 #include <openssl/obj_mac.h>
 
+#include <bitcoin/util/assert.hpp>
+
 namespace libbitcoin {
 
 elliptic_curve_key::elliptic_curve_key()
@@ -12,7 +14,13 @@ elliptic_curve_key::elliptic_curve_key()
 {
 }
 
-bool elliptic_curve_key::set_public_key(data_chunk pubkey)
+elliptic_curve_key::~elliptic_curve_key()
+{
+    if (key_ != nullptr)
+        EC_KEY_free(key_);
+}
+
+bool elliptic_curve_key::set_public_key(const data_chunk& pubkey)
 {
     if (!initialize())
         return false;
@@ -22,8 +30,9 @@ bool elliptic_curve_key::set_public_key(data_chunk pubkey)
     return true;
 }
 
-bool elliptic_curve_key::verify(hash_digest hash, data_chunk signature)
+bool elliptic_curve_key::verify(hash_digest hash, const data_chunk& signature)
 {
+    BITCOIN_ASSERT(key_ != nullptr);
     // SSL likes a reversed hash
     std::reverse(hash.begin(), hash.end());
     // -1 = error, 0 = bad sig, 1 = good
@@ -33,8 +42,57 @@ bool elliptic_curve_key::verify(hash_digest hash, data_chunk signature)
     return false;
 }
 
+bool elliptic_curve_key::new_key_pair()
+{
+    if (!initialize())
+        return false;
+    if (!EC_KEY_generate_key(key_))
+        return false;
+    return true;
+}
+
+bool elliptic_curve_key::set_private_key(const private_data& privkey)
+{
+    if (!initialize())
+        return false;
+    const byte* privkey_begin = &privkey[0];
+    if (!d2i_ECPrivateKey(&key_, &privkey_begin, privkey.size()))
+        return false;
+    return true;
+}
+
+private_data elliptic_curve_key::get_private_key() const
+{
+    size_t length = i2d_ECPrivateKey(key_, NULL);
+    if (!length)
+        return private_data();
+    private_data privkey(length, 0);
+    byte* privkey_begin = &privkey[0];
+    if (i2d_ECPrivateKey(key_, &privkey_begin) != length)
+        return private_data();
+    return privkey;
+}
+
+data_chunk elliptic_curve_key::sign(hash_digest hash) const
+{
+    BITCOIN_ASSERT(key_ != nullptr);
+    // SSL likes a reversed hash
+    std::reverse(hash.begin(), hash.end());
+    data_chunk signature;
+    signature.resize(10000);
+    unsigned int signature_length = signature.size();
+    if (!ECDSA_sign(0, hash.data(), hash.size(), 
+            &signature[0], &signature_length, key_))
+        return data_chunk();
+    signature.resize(signature_length);
+    return signature;
+}
+
 bool elliptic_curve_key::initialize()
 {
+    // Already initialized
+    if (key_ != nullptr)
+        return true;
     key_ = EC_KEY_new_by_curve_name(NID_secp256k1);
     return key_ != nullptr;
 }
