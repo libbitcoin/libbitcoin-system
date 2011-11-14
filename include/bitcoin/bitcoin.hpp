1db7684e69ffb334841f639081aa21355e416627
genjix <fake@lol.u>
2011-11-14 02:49:42 +0000

    Convenience header.

--- include/bitcoin/bitcoin.hpp
+++ include/bitcoin/bitcoin.hpp
@@ -3,9 +3,33 @@
 
 // Convenience header that includes everything
 // Not to be used internally. For API users.
-#include <bitcoin/block.hpp>
-#include <bitcoin/script.hpp>
-// ... And so on
+#include <error.hpp>
+#include <validate.hpp>
+#include <messages.hpp>
+#include <util/elliptic_curve_key.hpp>
+#include <util/sha256.hpp>
+#include <util/postbind.hpp>
+#include <util/threaded_service.hpp>
+#include <util/serializer.hpp>
+#include <util/assert.hpp>
+#include <util/big_number.hpp>
+#include <util/clock.hpp>
+#include <util/base58.hpp>
+#include <util/ripemd.hpp>
+#include <util/logger.hpp>
+#include <kernel.hpp>
+#include <script.hpp>
+#include <dialect.hpp>
+#include <address.hpp>
+#include <transaction.hpp>
+#include <storage/postgresql_storage.hpp>
+#include <storage/storage.hpp>
+#include <storage/bdb_storage.hpp>
+#include <constants.hpp>
+#include <block.hpp>
+#include <types.hpp>
+#include <network/network.hpp>
+#include <network/types.hpp>
 
 namespace bc = libbitcoin;
 
