7d7ff173f17e4dc06a1373d09d7da0aa733d8678
genjix <fake@lol.u>
2012-04-07 16:27:17 +0100

    remove obselete comment from address.hpp

--- include/bitcoin/address.hpp
+++ include/bitcoin/address.hpp
@@ -1,12 +1,6 @@
 #ifndef LIBBITCOIN_ADDRESS_H
 #define LIBBITCOIN_ADDRESS_H
 
-/*
- * This is a convenience file which is going to be subject
- * to heavy revision as the API is more finely revised.
- * It is strictly temporary.
- */
-
 #include <bitcoin/types.hpp>
 #include <bitcoin/script.hpp>
 
