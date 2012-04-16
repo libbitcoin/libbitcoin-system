8b68468aa21f3f8afa6e9a09355e41d2e3a4a7d3
genjix <fake@lol.u>
2012-04-16 15:57:11 +0100

    fan out for coroutines.

--- tests/corot.cpp
+++ tests/corot.cpp
@@ -28,16 +28,24 @@ void fetch_block_t::run(const std::error_code& ec, stack_ptr stack)
 {
     auto this_ptr = shared_from_this();
     message::output_point outpoint;
-    outpoint.hash = hash_from_pretty<decltype(outpoint.hash)>("f4184fc596403b9d638783cf57adfe4c75c605f6356fbc91338530e9831e9e16");
+    outpoint.hash = hash_from_pretty<hash_digest>("f4184fc596403b9d638783cf57adfe4c75c605f6356fbc91338530e9831e9e16");
     outpoint.index = 1;
     reenter(this)
     {
-        yield blockchain_->fetch_last_depth(
-            [this_ptr, stack](const std::error_code& ec, size_t depth)
-            {
-                stack->last = depth;
-                this_ptr->run(ec, stack);
-            });
+        yield
+        {
+            auto count = std::make_shared<atomic_counter>(0);
+            for (size_t i = 0; i < 4; ++i)
+                blockchain_->fetch_last_depth(
+                    [this_ptr, stack, count](
+                        const std::error_code& ec, size_t depth)
+                    {
+                        stack->last = depth;
+                        ++(*count);
+                        if (*count == 4)
+                            this_ptr->run(ec, stack);
+                    });
+        }
         log_debug() << stack->last;
         yield blockchain_->fetch_block_header(stack->last,
             [this_ptr, stack](const std::error_code& ec,
@@ -61,10 +69,20 @@ void fetch_block_t::run(const std::error_code& ec, stack_ptr stack)
     }
 }
 
+void blockchain_started(const std::error_code& ec, blockchain_ptr)
+{
+    if (ec)
+        log_error() << "Error: " << ec.message();
+    else
+        log_info() << "Blockchain initialized!";
+}
+
+
 int main()
 {
     async_service service(1);
-    blockchain_ptr chain = bdb_blockchain::create(service, "database");
+    blockchain_ptr chain =
+        bdb_blockchain::create(service, "database", blockchain_started);
     auto f = std::make_shared<fetch_block_t>(chain);
     f->run();
     std::cin.get();
