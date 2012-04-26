48782b77cf526273b98877d7503b2bc9a8a257ce
genjix <fake@lol.u>
2012-04-26 20:29:05 +0100

    Respomd to get_data requests for transactions.

--- src/getx_responder.cpp
+++ src/getx_responder.cpp
@@ -1,6 +1,84 @@
 #include <bitcoin/getx_responder.hpp>
 
+#include <bitcoin/network/channel.hpp>
+#include <bitcoin/transaction_pool.hpp>
+#include <bitcoin/blockchain/blockchain.hpp>
+
 namespace libbitcoin {
+
+using std::placeholders::_1;
+using std::placeholders::_2;
+
+getx_responder::getx_responder(async_service& service,
+    blockchain_ptr chain, transaction_pool_ptr txpool)
+  : service_(service.get_service()), chain_(chain), txpool_(txpool)
+{
+}
+
+void getx_responder::monitor(channel_ptr node)
+{
+    node->subscribe_get_data(
+        service_.wrap(std::bind(
+            &getx_responder::receive_get_data,
+                shared_from_this(), _1, _2, node)));
+}
+
+void getx_responder::receive_get_data(const std::error_code& ec,
+    const message::get_data packet, channel_ptr node)
+{
+    if (ec)
+        return;
+    for (const message::inventory_vector& inv: packet.inventories)
+    {
+        switch (inv.type)
+        {
+            case message::inventory_type::transaction:
+                // First attempt lookup in faster pool, then do slow
+                // lookup in blockchain after.
+                txpool_->fetch(inv.hash,
+                    service_.wrap(std::bind(
+                        &getx_responder::pool_tx,
+                            shared_from_this(), _1, _2, inv.hash, node)));
+                break;
+
+            case message::inventory_type::block:
+                break;
+
+            // Ignore everything else
+            case message::inventory_type::error:
+            case message::inventory_type::none:
+            default:
+                break;
+        }
+    }
+    node->subscribe_get_data(
+        service_.wrap(std::bind(
+            &getx_responder::receive_get_data,
+                shared_from_this(), _1, _2, node)));
+}
+
+void getx_responder::pool_tx(const std::error_code& ec,
+    const message::transaction& tx, const hash_digest& tx_hash,
+    channel_ptr node)
+{
+    if (ec)
+    {
+        chain_->fetch_transaction(tx_hash,
+            service_.wrap(std::bind(
+                &getx_responder::chain_tx,
+                    shared_from_this(), _1, _2, node)));
+    }
+    else
+        node->send(tx, [](const std::error_code& ec) {});
+}
+
+void getx_responder::chain_tx(const std::error_code& ec,
+    const message::transaction& tx, channel_ptr node)
+{
+    if (ec)
+        return;
+    node->send(tx, [](const std::error_code& ec) {});
+}
 
 } // libbitcoin
 
--- tests/getx.cpp
+++ tests/getx.cpp
@@ -1,9 +1,62 @@
 #include <bitcoin/bitcoin.hpp>
 using namespace bc;
 
+async_service service(4);
+network_ptr net;
+acceptor_ptr acc;
+channel_ptr send_node, recv_node;
+handshake_ptr hs;
+blockchain_ptr chain;
+transaction_pool_ptr txpool;
+getx_responder_ptr getx;
+
+void handle_accept(const std::error_code& ec, channel_ptr node)
+{
+    send_node = node;
+    hs->ready(node, [node](const std::error_code&) { getx->monitor(node); });
+}
+
+void handle_listen(const std::error_code& ec, acceptor_ptr accpt)
+{
+    acc = accpt;
+    acc->accept(handle_accept);
+}
+
+void handle_send(const std::error_code& ec)
+{
+}
+
+void handle_connect(const std::error_code& ec, channel_ptr node)
+{
+    recv_node = node;
+    message::get_data getdat;
+    getdat.inventories.push_back(
+        {message::inventory_type::transaction,
+            hash_from_pretty<hash_digest>("e72e4f025695446cfd5c5349d1720beb38801f329a00281f350cb7e847153397")});
+    sleep(1);
+    node->send(getdat, handle_send);
+}
+
+void blockchain_started(const std::error_code& ec, blockchain_ptr blkchain)
+{
+    if (ec)
+    {
+        log_error() << ec.message();
+        return;
+    }
+    chain = blkchain;
+    net = std::make_shared<network>(service);
+    net->listen(8045, handle_listen);
+    hs = std::make_shared<handshake>(service);
+    hs->connect(net, "localhost", 8045, handle_connect);
+    txpool = transaction_pool::create(service, chain);
+    getx = std::make_shared<getx_responder>(service, chain, txpool);
+}
+
 int main()
 {
-    async_service service(1);
+    chain = bdb_blockchain::create(service, "database", blockchain_started);
+    std::cin.get();
     return 0;
 }
 
