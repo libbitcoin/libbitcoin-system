0c5c246e36d37dd39ac0e5de0cb3b956d483a17c
Amir Taaki <genjix@riseup.net>
2013-02-15 16:11:02 +0000

    fix unitiatlised last_block_hash_ in poller variable.

--- src/poller.cpp
+++ src/poller.cpp
@@ -8,7 +8,8 @@ using std::placeholders::_1;
 using std::placeholders::_2;
 
 poller::poller(async_service& service, blockchain& chain)
-  : strand_(service.get_service()), chain_(chain), last_hash_end_(null_hash)
+  : strand_(service.get_service()), chain_(chain),
+    last_hash_end_(null_hash), last_block_hash_(null_hash)
 {
 }
 
