8287d9085ff62e9f5b3c950d248029f448be1bf9
Amir Taaki <genjix@riseup.net>
2013-05-25 15:52:27 +0100

    test reorganising the blockchain.

---
+++ tests/reorg/reorg.cpp
@@ -0,0 +1,158 @@
+#include <future>
+#include <bitcoin/bitcoin.hpp>
+
+#include "chains.hpp"
+
+using namespace bc;
+using std::placeholders::_1;
+using std::placeholders::_2;
+
+blockchain::block_list load_chain(const string_list& raw_chain)
+{
+    blockchain::block_list blks;
+    for (const std::string& raw_repr: raw_chain)
+    {
+        auto block = std::make_shared<block_type>();
+        data_chunk raw_block = bytes_from_pretty(raw_repr);
+        satoshi_load(raw_block.begin(), raw_block.end(), *block);
+        blks.push_back(block);
+    }
+    return blks;
+}
+
+blockchain::block_list chain[3] = {
+    load_chain(raw_chain_0),
+    load_chain(raw_chain_1),
+    load_chain(raw_chain_2)
+};
+
+void display_chain(const blockchain::block_list& chain)
+{
+    hash_digest previous = null_hash;
+    for (const auto& blk: chain)
+    {
+        if (previous != null_hash)
+        {
+            BITCOIN_ASSERT(blk->previous_block_hash == previous);
+        }
+        previous = hash_block_header(*blk);
+        log_info() << previous;
+    }
+}
+
+blockchain* c = nullptr;
+
+std::string block_status_str(block_status status)
+{
+    switch (status)
+    {
+        case block_status::orphan:
+            return "orphan";
+        case block_status::confirmed:
+            return "confirmed";
+        case block_status::rejected:
+            return "rejected";
+    }
+}
+
+void handle_final_store(const std::error_code& ec, block_info info,
+    const block_type& blk)
+{
+    log_info() << "Block " << hash_block_header(blk)
+        << " [" << block_status_str(info.status) << "]";
+    if (ec)
+    {
+        log_info() << "  NOT stored.";
+    }
+    else if (info.status == block_status::confirmed)
+    {
+        log_info() << "  Stored at " << info.depth << ".";
+    }
+}
+
+void next_store(const std::error_code& ec, block_info info,
+    const block_type& blk, size_t i, size_t cidx)
+{
+    log_info() << "Block " << hash_block_header(blk)
+        << " [" << block_status_str(info.status) << "]";
+    if (ec)
+    {
+        log_info() << "  NOT stored.";
+    }
+    else if (info.status == block_status::confirmed)
+    {
+        log_info() << "  Stored at " << info.depth << ".";
+    }
+    ++cidx;
+    if (cidx >= 3)
+    {
+        BITCOIN_ASSERT(cidx == 3);
+        cidx = 0;
+        ++i;
+        if (i >= 6)
+        {
+            BITCOIN_ASSERT(i == 6);
+            c->store(*chain[1][6],
+                std::bind(handle_final_store, _1, _2, *chain[1][6]));
+            log_info() << "STOP!";
+            return;
+        }
+    }
+    BITCOIN_ASSERT(cidx < sizeof(chain));
+    BITCOIN_ASSERT(i < chain[cidx].size());
+    c->store(*chain[cidx][i],
+        std::bind(next_store, _1, _2, *chain[cidx][i], i, cidx));
+}
+
+void handle_reorganize(const std::error_code& ec, size_t fork_point,
+    const blockchain::block_list& arrivals,
+    const blockchain::block_list& replaced)
+{
+    log_info() << "Reorganize! " << fork_point << " " << ec.message();
+    for (auto blk: arrivals)
+        log_info() << "Arrive: " << hash_block_header(*blk);
+    for (auto blk: replaced)
+        log_info() << "Remove: " << hash_block_header(*blk);
+    c->subscribe_reorganize(handle_reorganize);
+}
+
+int main()
+{
+    system("rm -fr database/*");
+    log_info() << "Chain 0 -----";
+    display_chain(chain[0]);
+    log_info() << "Chain 1 -----";
+    display_chain(chain[1]);
+    log_info() << "Chain 2 -----";
+    display_chain(chain[2]);
+
+    threadpool pool(1);
+    leveldb_blockchain blkchain(pool);
+    std::promise<std::error_code> ec_promise;
+    auto blockchain_started =
+        [&ec_promise](const std::error_code& ec)
+        {
+            ec_promise.set_value(ec);
+        };
+    blkchain.start("database", blockchain_started);
+    std::error_code ec = ec_promise.get_future().get();
+    if (ec)
+        return 1;
+
+    blkchain.import(genesis_block(), 0, [](const std::error_code& ec) {});
+    c = &blkchain;
+
+    size_t i = 0, cidx = 0;
+    c->store(*chain[cidx][i],
+        std::bind(next_store, _1, _2, *chain[cidx][i], i, cidx));
+
+    c->subscribe_reorganize(handle_reorganize);
+
+    std::cin.get();
+
+    pool.stop();
+    pool.join();
+    blkchain.stop();
+    return 0;
+}
+
