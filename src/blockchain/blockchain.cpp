03bdd9b33de20f5f483a755d1cae3be93873a971
Amir Taaki <genjix@riseup.net>
2013-06-14 20:11:24 +0200

    fetch_history and fetch_output_values composed operations.

--- src/blockchain/blockchain.cpp
+++ src/blockchain/blockchain.cpp
@@ -1,5 +1,6 @@
 #include <bitcoin/blockchain/blockchain.hpp>
 
+#include <bitcoin/constants.hpp>
 #include <bitcoin/utility/assert.hpp>
 
 namespace libbitcoin {
@@ -7,6 +8,7 @@ namespace libbitcoin {
 using std::placeholders::_1;
 using std::placeholders::_2;
 
+// fetch_block
 typedef blockchain_fetch_handler_block handler_block;
 
 class fetch_block_t
@@ -95,7 +97,7 @@ private:
     handler_block handle_;
 
     block_type block_;
-    size_t count_;
+    atomic_counter count_;
     bool stopped_;
 };
 
@@ -191,6 +193,162 @@ void fetch_block_locator(blockchain& chain, handler_locator handle_fetch)
 {
     auto fetcher = std::make_shared<fetch_locator>(chain);
     fetcher->start(handle_fetch);
+}
+
+// fetch_history
+typedef blockchain_fetch_handler_history handler_history;
+
+class fetch_history_t
+  : public std::enable_shared_from_this<fetch_history_t>
+{
+public:
+    fetch_history_t(blockchain& chain)
+      : chain_(chain), stopped_(false) {}
+
+    void start(const payment_address& address, handler_history handle)
+    {
+        handle_ = handle;
+        auto this_ptr = shared_from_this();
+        chain_.fetch_outputs(address,
+            [this, this_ptr](const std::error_code& ec,
+                const output_point_list& outpoints)
+            {
+                if (stop_on_error(ec))
+                    return;
+                outpoints_ = outpoints;
+                fetch_spends();
+            });
+    }
+
+private:
+    bool stop_on_error(const std::error_code& ec)
+    {
+        if (stopped_)
+            return true;
+        else if (ec)
+        {
+            stopped_ = true;
+            handle_(ec, output_point_list(), input_point_list());
+            return true;
+        }
+        return false;
+    }
+
+    void fetch_spends()
+    {
+        inpoints_.resize(outpoints_.size());
+        count_ = 0;
+        for (size_t i = 0; i < inpoints_.size(); ++i)
+            chain_.fetch_spend(outpoints_[i],
+                std::bind(&fetch_history_t::spend_fetched,
+                    shared_from_this(), _1, _2, i));
+    }
+
+    void spend_fetched(const std::error_code& ec,
+        const input_point& inpoint, size_t i)
+    {
+        if (ec == error::unspent_output)
+        {
+            inpoints_[i].hash = null_hash;
+            inpoints_[i].index = std::numeric_limits<uint32_t>::max();
+        }
+        else if (stop_on_error(ec))
+            return;
+        else
+            inpoints_[i] = inpoint;
+        ++count_;
+        if (count_ == inpoints_.size())
+        {
+            BITCOIN_ASSERT(inpoints_.size() == outpoints_.size());
+            handle_(std::error_code(), outpoints_, inpoints_);
+        }
+    }
+
+    blockchain& chain_;
+    handler_history handle_;
+
+    output_point_list outpoints_;
+    input_point_list inpoints_;
+    bool stopped_;
+    atomic_counter count_;
+};
+
+void fetch_history(blockchain& chain, const payment_address& address,
+    handler_history handle_fetch)
+{
+    auto fetcher = std::make_shared<fetch_history_t>(chain);
+    fetcher->start(address, handle_fetch);
+}
+
+// fetch_output_values
+typedef blockchain_fetch_handler_output_values handler_output_values;
+
+class fetch_output_values_t
+  : public std::enable_shared_from_this<fetch_output_values_t>
+{
+public:
+    fetch_output_values_t(blockchain& chain)
+      : chain_(chain), stopped_(false) {}
+
+    void start(const output_point_list& outpoints, handler_output_values handle)
+    {
+        // Just finish. No outpoints to fetch.
+        if (outpoints.empty())
+        {
+            handle(std::error_code(), output_value_list());
+            return;
+        }
+        handle_ = handle;
+        values_.resize(outpoints.size());
+        count_ = 0;
+        for (size_t i = 0; i < outpoints.size(); ++i)
+        {
+            const output_point& outpoint = outpoints[i];
+            chain_.fetch_transaction(outpoint.hash,
+                std::bind(&fetch_output_values_t::tx_fetched,
+                    shared_from_this(), _1, _2, outpoint.index, i));
+        }
+    }
+
+private:
+    bool stop_on_error(const std::error_code& ec)
+    {
+        if (stopped_)
+            return true;
+        else if (ec)
+        {
+            stopped_ = true;
+            handle_(ec, output_value_list());
+            return true;
+        }
+        return false;
+    }
+
+    void tx_fetched(const std::error_code& ec, const transaction_type& tx,
+        size_t output_index, size_t index)
+    {
+        if (stop_on_error(ec))
+            return;
+        BITCOIN_ASSERT(output_index < tx.outputs.size());
+        values_[index] = tx.outputs[output_index].value;
+        ++count_;
+        if (count_ == values_.size())
+            handle_(std::error_code(), values_);
+    }
+
+    blockchain& chain_;
+    handler_output_values handle_;
+
+    output_value_list values_;
+    atomic_counter count_;
+    bool stopped_;
+};
+
+void fetch_output_values(blockchain& chain, const output_point_list& outpoints,
+    handler_output_values handle_fetch)
+{
+    auto fetcher = std::make_shared<fetch_output_values_t>(chain);
+    fetcher->start(outpoints, handle_fetch);
 }
 
 } // namespace libbitcoin
