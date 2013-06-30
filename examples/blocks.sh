49050350dfeba36dcdb5ba20975014a58ee88674
Amir Taaki <genjix@riseup.net>
2013-06-30 22:33:00 +0200

    made the instructions in blocks.sh file to a README file.

--- examples/blocks.sh
+++ examples/blocks.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-
-# Run these commands to start the fullnode after installing libbitcoin:
-#
-#   $ make
-#   $ mkdir database
-#   $ ./initchain database
-#   $ screen -S fullnode
-#   $ ./fullnode
-#   ... press CTRL-A, D
-#   $ ./blocks.sh
-#
 watch -n 0.5 "grep Block debug.log | tail -n 20"
 
