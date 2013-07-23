6b288e766ac99f2aac4bd4e496498fc80f061e7d
Amir Taaki <genjix@riseup.net>
2013-07-23 15:58:53 +0200

    use rsync for blockchain download. display command.

--- download-blockchain.sh
+++ download-blockchain.sh
@@ -1,6 +1,8 @@
-#!/bin/bash -x
+#!/bin/bash
 # If you know a better download method, then please let me know.
 #wget http://46.4.92.107/blockchain.tar.bz2
 echo "Type 'public' at the password prompt."
-rsync -avz public@46.4.92.107:/var/www/blockchain/ blockchain/
+COMMAND="rsync -avz public@46.4.92.107:/var/www/blockchain/ blockchain/"
+echo "> $COMMAND"
+$COMMAND
 
