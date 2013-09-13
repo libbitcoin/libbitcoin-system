57766fd9babaf7c3519c74c38054aa769b3d67e2
Amir Taaki <genjix@riseup.net>
2013-09-13 17:39:59 +0200

    replace main std promise with a spinlock.

--- tools/bootstrap.cpp
+++ tools/bootstrap.cpp
@@ -5,15 +5,18 @@
 */
 #include <future>
 #include <bitcoin/bitcoin.hpp>
+
+#define LOG_BOOTSTRAP "bootstrap"
+
 using namespace bc;
- 
 using std::placeholders::_1;
 using std::placeholders::_2;
 
 bool stopped = false;
-void signal_handler(int sig)
+
+void signal_handler(int signal)
 {
-    log_info("bootstrap") << "Caught signal: " << sig;
+    log_info(LOG_BOOTSTRAP) << "Caught signal: " << signal;
     stopped = true;
 }
 
@@ -21,7 +24,8 @@ int main(int argc, char** argv)
 {
     if (argc != 3)
     {
-        std::cout << "usage: bootstrap <blockchain directory> <bootstrap.dat file>" << std::endl;
+        std::cout << "Usage: "
+            "bootstrap BLOCKCHAIN_DIR BOOTSTRAP_DATFILE" << std::endl;
         return 1;
     }
 
@@ -39,6 +43,7 @@ int main(int argc, char** argv)
     chain.start(dbpath, blockchain_start);
     // First block is the genesis block.
     block_type first_block = genesis_block();
+    const hash_digest genesis_hash = hash_block_header(first_block.header);
 
     // Total number of blocks parsed from the bootstrap.dat
     int blocks_parsed = 0;
@@ -46,60 +51,59 @@ int main(int argc, char** argv)
     // position of the pointer in the buffer
     int buffer_position = 0;
 
-    std::promise<std::error_code> ec_promise;
-    std::promise<block_header_type> blk_header_promise;
+    block_header_type blk_header;
+    std::promise<std::error_code> ec_fetch_promise;
 
-    auto genesis_block_fetched_handler = 
-        [&ec_promise, &blk_header_promise](const std::error_code& ec,
-                                             const block_header_type& header)
+    auto genesis_block_fetched_handler =
+        [&ec_fetch_promise, &blk_header](
+            const std::error_code& ec, const block_header_type& header)
         {
-            ec_promise.set_value(ec);
-            blk_header_promise.set_value(header);
+            blk_header = header;
+            ec_fetch_promise.set_value(ec);
         };
 
     // See if we can fetch the genesis block from the leveldb database
     chain.fetch_block_header(0, genesis_block_fetched_handler);
 
-    std::error_code ec = ec_promise.get_future().get();
-    block_header_type blk_header = blk_header_promise.get_future().get();
-
-
+    std::error_code ec = ec_fetch_promise.get_future().get();
     if (ec)
     {
+        BITCOIN_ASSERT(ec == error::not_found);
         //Import the genesis_block() if it does not yet exist in the leveldb
-        std::promise<std::error_code> ec_promise;
+        std::promise<std::error_code> ec_import_promise;
 
-        auto genesis_block_imported_handler = 
-            [&ec_promise](const std::error_code& ec)
+        auto genesis_block_imported_handler =
+            [&ec_import_promise](const std::error_code& ec)
             {
-                ec_promise.set_value(ec);
+                ec_import_promise.set_value(ec);
             };
 
         chain.import(first_block, 0, genesis_block_imported_handler);
-        ec = ec_promise.get_future().get();
+        ec = ec_import_promise.get_future().get();
 
         if (ec)
         {
-            log_error() << "Importing genesis block failed: " << ec.message();
+            log_error(LOG_BOOTSTRAP)
+                << "Importing genesis block failed: " << ec.message();
             return -1;
         }
-        else 
-            log_info() << "Imported genesis block";
 
+        log_info(LOG_BOOTSTRAP) << "Imported genesis block";
     }
-    // If we already have a block stored in the leveldb, check its hash 
-    // matches the one coded into genesis_block()
-    else 
-        BITCOIN_ASSERT(hash_block_header(blk_header) ==
-                         hash_block_header(first_block.header));
+    else
+    {
+        // If we already have a block stored in the leveldb, check its hash
+        // matches the one coded into genesis_block()
+        BITCOIN_ASSERT(hash_block_header(blk_header) == genesis_hash);
+    }
 
     std::ifstream fin(bootstrapfile, std::ios::in | std::ios::binary);
 
     if (!fin)
     {
-        log_error() << "Could not open bootstrap.dat file";
+        log_error(LOG_BOOTSTRAP) << "Could not open bootstrap.dat file";
         fin.close();
- 
+
         pool.stop();
         pool.join();
         chain.stop();
@@ -107,9 +111,9 @@ int main(int argc, char** argv)
     }
 
     constexpr size_t max_block_size = 1000000;
-    // Allocate a 2 * max_block_size buffer, so we are sure to read at least 
+    // Allocate a 2 * max_block_size buffer, so we are sure to read at least
     // one full block into the buffer, or many smaller ones.
-    constexpr size_t buffer_size = 2 * max_block_size; 
+    constexpr size_t buffer_size = 2 * max_block_size;
 
     int block_size;
     data_chunk buffer(buffer_size);
@@ -118,13 +122,13 @@ int main(int argc, char** argv)
     // This lambda function will read the chunk from its
     // first byte at position pos in the buffer and return
     // the integer representation of the block size in bytes.
-    auto get_block_size = 
+    auto get_block_size =
         [](const data_chunk& buffer, const int pos)
         {
-            return (int) (buffer[pos + 3] << 24) + 
-                   (int) (buffer[pos + 2] << 16) + 
-                   (int) (buffer[pos + 1] << 8) + 
-                   (int) (buffer[pos]); 
+            return (int) (buffer[pos + 3] << 24) +
+                   (int) (buffer[pos + 2] << 16) +
+                   (int) (buffer[pos + 1] << 8) +
+                   (int) (buffer[pos]);
         };
 
     // This will return false when there are not enough bytes left
@@ -143,80 +147,82 @@ int main(int argc, char** argv)
         while(buffer_position < buffer_size - 8 && !stopped)
         {
             // Assert we have the magic bytes.
-            BITCOIN_ASSERT( buffer[buffer_position] == 0xf9 && 
-                        buffer[buffer_position + 1] == 0xbe && 
-                        buffer[buffer_position + 2] == 0xb4 && 
-                        buffer[buffer_position + 3] == 0xd9 );
- 
+            BITCOIN_ASSERT(
+                buffer[buffer_position] == 0xf9 &&
+                buffer[buffer_position + 1] == 0xbe &&
+                buffer[buffer_position + 2] == 0xb4 &&
+                buffer[buffer_position + 3] == 0xd9);
+
             // Get the block size from the reversed endain field.
             block_size = get_block_size(buffer, buffer_position + 4);
- 
+
             // If the rest of the block isn't inside the buffer break out
             if (buffer_position + block_size + 8 > buffer_size)
                 break;
-            
-            std::promise<std::error_code> ec_promise;
-            std::promise<block_info> info_promise;
-            block_type blk;
 
-            satoshi_load(buffer.begin() + buffer_position + 8,
-                         buffer.begin() + buffer_position + 8 + block_size,
-                         blk);
+            auto begin_iter = buffer.begin() + buffer_position + 8;
+            auto end_iter = begin_iter + block_size;
+            block_type blk;
+            satoshi_load(begin_iter, end_iter, blk);
 
             // Assert we match the genesis block hash
-            if (blocks_parsed == 0)
-                BITCOIN_ASSERT(hash_block_header(blk.header) ==
-                                 hash_block_header(first_block.header));
+            BITCOIN_ASSERT(blocks_parsed != 0 ||
+                hash_block_header(blk.header) == genesis_hash);
 
-            auto block_imported = 
-            [&ec_promise, &info_promise](const std::error_code& ec,
-                                         const block_info info)
-            {  
-                // handle_store(ec, info, block_hash);
-                ec_promise.set_value(ec);
-                info_promise.set_value(info);
+            std::error_code ec;
+            block_info info;
+            bool finished = false;
+
+            auto block_imported = [&ec, &info, &finished](
+                const std::error_code& cec, const block_info blk_info)
+            {
+                log_debug(LOG_BOOTSTRAP) << "Block imported.";
+                info = blk_info;
+                ec = cec;
+                finished = true;
             };
 
-            chain.store(blk, std::bind(block_imported, _1, _2));
+            log_debug(LOG_BOOTSTRAP) << "Storing block. Parse count = "
+                << blocks_parsed;
+            chain.store(blk, block_imported);
+
+            while (!finished)
+                usleep(1000);
+
             hash_digest block_hash = hash_block_header(blk.header);
-
-            std::error_code ec = ec_promise.get_future().get();
-            block_info info = info_promise.get_future().get();
-
             // We need orphan blocks so we can do the next getblocks round
             if (ec && info.status != block_status::orphan)
             {
-                log_error("bootstrap")
+                log_error(LOG_BOOTSTRAP)
                     << "Storing block " << encode_hex(block_hash)
                     << ": " << ec.message();
             }
             else
-            switch (info.status)
             {
-                // There should be no orphans
-                case block_status::orphan:
-                    log_error("bootstrap")
-                        << "Orphan block " << encode_hex(block_hash); 
-                break;
- 
-                case block_status::rejected:
-                    log_error("bootstrap")
-                        << "Rejected block " << encode_hex(block_hash);
-                break;
- 
-                case block_status::confirmed:
-                    log_info("bootstrap")
-                        << "Block #" << info.height << " " << encode_hex(block_hash);
-                break;
-            }
+                switch (info.status)
+                {
+                    // There should be no orphans
+                    case block_status::orphan:
+                        log_error(LOG_BOOTSTRAP)
+                            << "Orphan block " << block_hash;
+                    break;
 
-            // Fixes segmentation fault, maybe this is an issue with ec_promise and .get_future() 
-            usleep(1000);
+                    case block_status::rejected:
+                        log_error(LOG_BOOTSTRAP)
+                            << "Rejected block " << block_hash;
+                    break;
+
+                    case block_status::confirmed:
+                        log_info(LOG_BOOTSTRAP)
+                            << "Block #" << info.height << " " << block_hash;
+                    break;
+                }
+            }
 
             buffer_position += block_size + 8;
             blocks_parsed++;
         }
- 
+
         // Rewind to the last good magic bytes, which didn't have enough space
         // left in the buffer for the block
         fin.seekg(buffer_position - buffer_size, std::ios::cur);
