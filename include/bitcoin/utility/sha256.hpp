14c2e0d6b0a2ea3b3bdecab4575c33385ee6a271
Amir Taaki <genjix@riseup.net>
2013-09-30 19:26:20 +0200

    sha256 minor tidying.

--- include/bitcoin/utility/sha256.hpp
+++ include/bitcoin/utility/sha256.hpp
@@ -11,8 +11,8 @@ namespace libbitcoin {
 constexpr size_t sha256_digest_size = SHA256_DIGEST_LENGTH;
 static_assert(sha256_digest_size == 32, "SHA256_DIGEST_LENGTH not defined correctly!");
 
-hash_digest generate_sha256_hash(const data_chunk& chunk);
-uint32_t generate_sha256_checksum(const data_chunk& chunk);
+hash_digest generate_sha256_hash(const data_chunk& data);
+uint32_t generate_sha256_checksum(const data_chunk& data);
 
 } // namespace libbitcoin
 
--- src/utility/sha256.cpp
+++ src/utility/sha256.cpp
@@ -1,16 +1,16 @@
 #include <bitcoin/utility/sha256.hpp>
-#include <bitcoin/format.hpp>
 
 #include <boost/detail/endian.hpp>
+#include <bitcoin/format.hpp>
 
 namespace libbitcoin {
 
-hash_digest generate_sha256_hash(const data_chunk& chunk)
+hash_digest generate_sha256_hash(const data_chunk& data)
 {
     SHA256_CTX ctx;
     hash_digest digest;
     SHA256_Init(&ctx);
-    SHA256_Update(&ctx, &chunk[0], chunk.size());
+    SHA256_Update(&ctx, &data[0], data.size());
     SHA256_Final(digest.data(), &ctx);
     SHA256_Init(&ctx);
     SHA256_Update(&ctx, digest.data(), sha256_digest_size);
@@ -20,9 +20,9 @@ hash_digest generate_sha256_hash(const data_chunk& chunk)
     return digest;
 }
 
-uint32_t generate_sha256_checksum(const data_chunk& chunk)
+uint32_t generate_sha256_checksum(const data_chunk& data)
 {
-    hash_digest hash = generate_sha256_hash(chunk);
+    hash_digest hash = generate_sha256_hash(data);
     data_chunk begin_bytes(hash.rbegin(), hash.rbegin() + 4);
     return cast_chunk<uint32_t>(begin_bytes);
 }
