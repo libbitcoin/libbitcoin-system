46777affc06be43526586e0cff0541e87101dc5d
Chang Lan <changlan9@gmail.com>
2013-12-08 17:08:07 -0800

    Fix Mac OS X compilation issue

--- src/utility/timed_section.cpp
+++ src/utility/timed_section.cpp
@@ -2,6 +2,23 @@
 
 #include <bitcoin/utility/logger.hpp>
 
+#ifdef __MACH__
+#include <mach/clock.h>
+#include <mach/mach.h>
+#define CLOCK_REALTIME 0
+
+void clock_gettime(int ign, struct timespec * ts)
+{
+    clock_serv_t cclock;
+    mach_timespec_t mts;
+    host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+    clock_get_time(cclock, &mts);
+    mach_port_deallocate(mach_task_self(), cclock);
+    ts->tv_sec = mts.tv_sec;
+    ts->tv_nsec = mts.tv_nsec;
+}
+#endif
+
 namespace libbitcoin {
 
 timed_section::timed_section(
