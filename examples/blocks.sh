a57bf365fc2fd6f0de93397a4673f91831758522
Pablo Castellano <pablo@anche.no>
2013-12-12 23:05:45 +0100

    Last recommended changes to clarify License
    For more info see:
    https://wiki.unsystem.net/index.php/Libbitcoin/License

--- examples/blocks.sh
+++ examples/blocks.sh
@@ -5,9 +5,10 @@
 # This file is part of libbitcoin.
 #
 # libbitcoin is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
+# it under the terms of the GNU Affero General Public License with
+# additional permissions to the one published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) 
+# any later version. For more information see LICENSE.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
