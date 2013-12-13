cd9b8bb3b321b0fffcf2c16335edc36060dc2b4c
Amir Taaki <genjix@riseup.net>
2013-12-13 23:55:47 +0000

    moved select_outputs to libwallet.

--- include/bitcoin/transaction.hpp
+++ include/bitcoin/transaction.hpp
@@ -26,7 +26,7 @@ namespace libbitcoin {
 
 typedef std::vector<uint64_t> output_value_list;
 
-// Used by both transaction_indexer and select_outputs()
+// Used by transaction_indexer
 struct output_info_type
 {
     output_point point;
@@ -34,19 +34,6 @@ struct output_info_type
 };
 
 typedef std::vector<output_info_type> output_info_list;
-
-// Result for call to select_outputs()
-struct select_outputs_result
-{
-    output_point_list points;
-    uint64_t change;
-};
-
-// Algorithm for call to select_outputs()
-enum class select_outputs_algorithm
-{
-    greedy
-};
 
 hash_digest hash_transaction(const transaction_type& tx);
 // hash_type_code is used by OP_CHECKSIG
@@ -67,15 +54,6 @@ bool operator!=(const output_point& output_a, const output_point& output_b);
 
 bool is_final(const transaction_type& tx,
     size_t block_height, uint32_t block_time);
-
-/**
- * Select optimal outputs for a send from unspent outputs list.
- * Returns output list and remaining change to be sent to
- * a change address.
- */
-select_outputs_result select_outputs(
-    output_info_list unspent, uint64_t min_value,
-    select_outputs_algorithm alg=select_outputs_algorithm::greedy);
 
 } // namespace libbitcoin
 
