4c6d4ab32c8b5939f3806b207fd041a6dfc9e986
William Swanson <swansontec@gmail.com>
2014-03-12 14:48:21 -0700

    Do not use 64-bit file IO calls in mmfile
    These functions are not available on iOS or Android.
    
    If libbitcoin is being compiled for a 64-bit platform, the standard
    functions already work fine (size_t and off_t are already 64-bits).
    
    If libbitcoin is being compiled for a 32-bit platform, files larger than
    64GiB won't fit in the address space anyhow, so there is no point in being
    64-bit aware here.

--- src/utility/mmfile.cpp
+++ src/utility/mmfile.cpp
@@ -12,16 +12,16 @@ namespace libbitcoin {
 
 mmfile::mmfile(const std::string& filename)
 {
-    file_handle_ = open64(filename.c_str(),
+    file_handle_ = open(filename.c_str(),
         O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
     if (file_handle_ == -1)
         return;
-    struct stat64 sbuf;
-    if (fstat64(file_handle_, &sbuf) == -1)
+    struct stat sbuf;
+    if (fstat(file_handle_, &sbuf) == -1)
         return;
     size_ = (long long)sbuf.st_size;
     // You can static_cast void* pointers.
-    data_ = static_cast<uint8_t*>(mmap64(
+    data_ = static_cast<uint8_t*>(mmap(
         0, size_, PROT_READ | PROT_WRITE, MAP_SHARED, file_handle_, 0));
     if (data_ == MAP_FAILED)
         data_ = nullptr;
