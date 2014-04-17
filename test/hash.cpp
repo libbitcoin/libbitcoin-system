291d30423e96d2dffa564ec726f5b90aa7b9976e
Amir Taaki <genjix@riseup.net>
2014-04-17 17:18:49 +0000

    add sha256 hash test case

---
+++ test/hash.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2011-2013 libbitcoin developers (see AUTHORS)
+ *
+ * This file is part of libbitcoin.
+ *
+ * libbitcoin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License with
+ * additional permissions to the one published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) 
+ * any later version. For more information see LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <boost/test/unit_test.hpp>
+#include <bitcoin/bitcoin.hpp>
+using namespace bc;
+
+BOOST_AUTO_TEST_CASE(sha256_hash)
+{
+    auto genesis = genesis_block();
+    auto genesis_hash = hash_block_header(genesis.header);
+    BOOST_REQUIRE(encode_hex(genesis_hash) ==
+        "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f");
+}
+
