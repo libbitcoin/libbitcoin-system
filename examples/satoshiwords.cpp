5b371f1a231bc36e9888e656d5120fd4f7af5cc2
Amir Taaki <genjix@riseup.net>
2014-07-15 19:50:26 +0200

    tidy up satoshiwords.cpp

--- examples/satoshiwords.cpp
+++ examples/satoshiwords.cpp
@@ -20,31 +20,26 @@
 /*
   Display the genesis block message by Satoshi.
 */
-#ifdef _MSC_VER
 #include <iostream>
-#endif
 #include <bitcoin/bitcoin.hpp>
-
-using namespace bc;
 
 int main()
 {
     // Create genesis block.
-    block_type genblk = genesis_block();
+    bc::block_type block = bc::genesis_block();
     // Genesis block contains a single coinbase transaction.
-    assert(genblk.transactions.size() == 1);
+    assert(block.transactions.size() == 1);
     // Get first transaction in block (coinbase).
-    const transaction_type& coinbase_tx = genblk.transactions[0];
+    const bc::transaction_type& coinbase_tx = block.transactions[0];
     // Coinbase tx has a single input.
     assert(coinbase_tx.inputs.size() == 1);
-    const transaction_input_type& coinbase_input = coinbase_tx.inputs[0];
+    const bc::transaction_input_type& coinbase_input = coinbase_tx.inputs[0];
     // Convert the input script to its raw format.
-    const data_chunk& raw_block_message = save_script(coinbase_input.script);
+    const bc::data_chunk& raw_message = save_script(coinbase_input.script);
     // Convert this to an std::string.
     std::string message;
-    message.resize(raw_block_message.size());
-    std::copy(raw_block_message.begin(), raw_block_message.end(),
-        message.begin());
+    message.resize(raw_message.size());
+    std::copy(raw_message.begin(), raw_message.end(), message.begin());
     // Display the genesis block message.
     std::cout << message << std::endl;
     return 0;
