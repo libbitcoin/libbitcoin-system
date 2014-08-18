170cfc62f1dd0d2a379dc97a5868f8a62a8e3ec2
evoskuil <eric@voskuil.org>
2014-08-17 23:58:22 -0700

    Clean up constants.

--- src/stealth.cpp
+++ src/stealth.cpp
@@ -29,11 +29,11 @@ bool stealth_match(const stealth_prefix& prefix, const uint8_t* raw_bitfield)
 {
     for (size_t i = 0; i < prefix.size(); ++i)
     {
-        if (i > 0 && i % byte_size == 0)
+        if (i > 0 && i % byte_bits == 0)
             ++raw_bitfield;
-        size_t current_block = i / byte_size;
-        size_t bit_offset = i - current_block * byte_size;
-        BITCOIN_ASSERT(bit_offset < byte_size);
+        size_t current_block = i / byte_bits;
+        size_t bit_offset = i - current_block * byte_bits;
+        BITCOIN_ASSERT(bit_offset < byte_bits);
         bool value = (*raw_bitfield & (1 << bit_offset)) > 0;
         if (value != prefix[i])
             return false;
