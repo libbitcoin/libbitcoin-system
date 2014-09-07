5c40ba742b4a1e37549b0324df7e89fd534cae7a
evoskuil <eric@voskuil.org>
2014-09-07 15:17:22 -0700

    Normalize comments.

--- test/hd_keys.cpp
+++ test/hd_keys.cpp
@@ -17,9 +17,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-/*
-  Demonstration of HD wallet key generation.
-*/
 #include <boost/test/unit_test.hpp>
 #include <bitcoin/bitcoin.hpp>
 
