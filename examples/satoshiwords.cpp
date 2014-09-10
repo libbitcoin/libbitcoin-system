896ca89272231223d40f8eff994ab5c323aea7ba
evoskuil <eric@voskuil.org>
2014-09-10 10:42:55 -0700

    Use namespace.

--- examples/satoshiwords.cpp
+++ examples/satoshiwords.cpp
@@ -23,19 +23,21 @@
 #include <iostream>
 #include <bitcoin/bitcoin.hpp>
 
+using namespace bc;
+
 int main()
 {
     // Create genesis block.
-    bc::block_type block = bc::genesis_block();
+    block_type block = genesis_block();
     // Genesis block contains a single coinbase transaction.
     assert(block.transactions.size() == 1);
     // Get first transaction in block (coinbase).
-    const bc::transaction_type& coinbase_tx = block.transactions[0];
+    const transaction_type& coinbase_tx = block.transactions[0];
     // Coinbase tx has a single input.
     assert(coinbase_tx.inputs.size() == 1);
-    const bc::transaction_input_type& coinbase_input = coinbase_tx.inputs[0];
+    const transaction_input_type& coinbase_input = coinbase_tx.inputs[0];
     // Convert the input script to its raw format.
-    const bc::data_chunk& raw_message = save_script(coinbase_input.script);
+    const data_chunk& raw_message = save_script(coinbase_input.script);
     // Convert this to an std::string.
     std::string message;
     message.resize(raw_message.size());
