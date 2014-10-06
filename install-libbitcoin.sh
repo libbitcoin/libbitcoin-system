aa40d666bb212d262ed635bbc48506d0b197a944
Phillip Mienk <mienkphi@gmail.com>
2014-10-05 19:48:43 -0700

    Normalize install script.

--- install-libbitcoin.sh
+++ install-libbitcoin.sh
@@ -18,13 +18,13 @@
 # This is meant to be temporary, just to facilitate the install.
 
 if [ "$TRAVIS" = "true" ]; then
-    PARALLEL_MAKE="-j4"
+    PARALLEL="4"
 else
     NPROC=$(nproc)
-    PARALLEL_MAKE="-j$NPROC"
+    PARALLEL="$NPROC"
 fi
 
-SEQUENTIAL_MAKE="-j1"
+SEQUENTIAL="1"
 
 BUILD_DIRECTORY="libbitcoin_build"
 
@@ -61,12 +61,12 @@ display_message()
 
 automake_current_directory()
 {
-    MAKE_ARGS=$1
+    JOBS=$1
     shift 1
 
     ./autogen.sh
     ./configure "$@"
-    make "$MAKE_ARGS"
+    make "-j$JOBS"
     sudo make install
     sudo ldconfig
 }
@@ -76,14 +76,14 @@ build_from_github()
     ACCOUNT=$1
     REPO=$2
     BRANCH=$3
-    MAKE_ARGS=$4
+    JOBS=$4
 
     # Shift the first three parameters out of @.
     shift 4
 
     # Show the user what repo we are building.
     FORK="$ACCOUNT/$REPO"
-    display_message "Download $FORK/$BRANCH/$MAKE_ARGS"
+    display_message "Download $FORK/$BRANCH"
     
     # Clone the repo locally.
     rm -rf $REPO
@@ -91,25 +91,28 @@ build_from_github()
 
     # Build the local repo clone.
     pushd $REPO
-    automake_current_directory "$MAKE_ARGS" "$@"
+    automake_current_directory "$JOBS" "$@"
     popd
 }
 
 build_primary()
 {
+    JOBS=$1
+    shift 1
+
     if [ "$TRAVIS" = "true" ]; then
         # If the environment is Travis drop out of build directory.
         cd ..
         display_message "Local $TRAVIS_REPO_SLUG"
-	    automake_current_directory "$@"
+	    automake_current_directory "$JOBS" "$@"
         build_tests
     else
         # Otherwise we pull the primary repo down for the single file install.
-        build_from_github $BUILD_ACCOUNT $BUILD_REPO $BUILD_BRANCH "$@"
+        build_from_github $BUILD_ACCOUNT $BUILD_REPO $BUILD_BRANCH "$JOBS" "$@"
 
         # Build the tests and drop out of build directory.
         pushd $BUILD_REPO
-        build_tests
+        build_tests "$JOBS"
         popd
         cd ..
     fi
@@ -117,6 +120,8 @@ build_primary()
 
 build_tests()
 {
+    JOBS=$1
+
     # Build and run unit tests relative to the primary directory.
     pushd test
     ./make.sh
@@ -164,10 +169,10 @@ build_library()
     create_build_directory
 
     # Download, build and install all unpackaged dependencies.
-    build_from_github bitcoin secp256k1 master "$SEQUENTIAL_MAKE" "$@" $SECP256K1_OPTIONS
+    build_from_github bitcoin secp256k1 master "$SEQUENTIAL" "$@" $SECP256K1_OPTIONS
 
     # The primary build is not downloaded if we are running in Travis.
-    build_primary "$PARALLEL_MAKE" "$@"
+    build_primary "$PARALLEL" "$@"
 
     # If the build succeeded clean up the build directory.
     delete_build_directory
