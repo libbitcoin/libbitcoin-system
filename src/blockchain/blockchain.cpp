6c22075e1798fb35136e7a95a2984583816de779
Phillip Mienk <mienkphi@gmail.com>
2014-10-06 13:46:03 -0700

    Correct build breaks due to moved blockchain.hpp.

--- src/blockchain/blockchain.cpp
+++ src/blockchain/blockchain.cpp
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-#include <bitcoin/bitcoin/blockchain/blockchain.hpp>
+#include <bitcoin/bitcoin/blockchain.hpp>
 #include <bitcoin/bitcoin/constants.hpp>
 #include <bitcoin/bitcoin/utility/assert.hpp>
 #include <bitcoin/bitcoin/utility/async_parallel.hpp>
