da1b6218e7eede3d9a45dae5a24cc14e87df2432
evoskuil <eric@voskuil.org>
2014-10-22 22:43:01 -0700

    Remove conversion through double precision in fractional coin tests.

--- test/format.cpp
+++ test/format.cpp
@@ -95,7 +95,7 @@ BOOST_AUTO_TEST_CASE(btc_to_satoshi_overflow_precision_test)
 BOOST_AUTO_TEST_CASE(btc_to_satoshi_no_leading_zero_test)
 {
     uint64_t satoshi;
-    uint64_t expected = .5 * coin_price(1);
+    uint64_t expected = coin_price(1) / 2;
     BOOST_REQUIRE(btc_to_satoshi(satoshi, ".5"));
     BOOST_REQUIRE_EQUAL(satoshi, expected);
 }
@@ -103,7 +103,7 @@ BOOST_AUTO_TEST_CASE(btc_to_satoshi_no_leading_zero_test)
 BOOST_AUTO_TEST_CASE(btc_to_satoshi_trailing_zero_test)
 {
     uint64_t satoshi;
-    uint64_t expected = .5 * coin_price(1);
+    uint64_t expected = coin_price(1) / 2;
     BOOST_REQUIRE(btc_to_satoshi(satoshi, "0.50"));
     BOOST_REQUIRE_EQUAL(satoshi, expected);
 }
@@ -111,7 +111,7 @@ BOOST_AUTO_TEST_CASE(btc_to_satoshi_trailing_zero_test)
 BOOST_AUTO_TEST_CASE(btc_to_satoshi_offset_zero_test)
 {
     uint64_t satoshi;
-    uint64_t expected = .05 * coin_price(1);
+    uint64_t expected = coin_price(1) / 20;
     BOOST_REQUIRE(btc_to_satoshi(satoshi, "0.05"));
     BOOST_REQUIRE_EQUAL(satoshi, expected);
 }
