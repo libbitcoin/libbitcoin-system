5df14fd3899abc01480f2b03a0fca989cecc836b
evoskuil <eric@voskuil.org>
2014-11-08 23:28:13 -0800

    Move node-related tools to libbitcoin-node.

--- tools/update_changelog.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-FILE="$(dirname "$0")/../ChangeLog"
-
-echo -e "Updated on `date`\n" > $FILE
-echo -e "If working with git repository, use command 'git log --oneline --decorate' for latest change log\n" >> $FILE
-git log --oneline --decorate >> $FILE
