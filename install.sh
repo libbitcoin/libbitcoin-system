d59e87e56afb1c69d4ed560b2f0cf669cedc0aa6
evoskuil <eric@voskuil.org>
2014-12-05 17:31:48 -0800

    Update generated compiler flags.

--- install.sh
+++ install.sh
@@ -143,9 +143,9 @@ if [[ $PREFIX ]]; then
         with_boost="--with-boost=$PREFIX"
     fi
     
-    # Set public prefix_flags variable (because GMP has no pkg-config).
+    # Set public gmp_flags variable (because GMP has no pkg-config).
     if [[ $BUILD_GMP == yes ]]; then
-        prefix_flags="CPPFLAGS=-I$PREFIX/include LDFLAGS=-L$PREFIX/lib"
+        gmp_flags="CPPFLAGS=-I$PREFIX/include LDFLAGS=-L$PREFIX/lib"
     fi
     
     # Set public prefix variable (to tell Boost where to build).
@@ -157,7 +157,7 @@ fi
 echo "Published dynamic options:"
 echo "  boost_link: $boost_link"
 echo "  prefix: $prefix"
-echo "  prefix_flags: $prefix_flags"
+echo "  gmp_flags: $gmp_flags"
 echo "  with_boost: $with_boost"
 echo "  with_pkgconfigdir: $with_pkgconfigdir"
 
@@ -212,12 +212,12 @@ SECP256K1_OPTIONS=\
 "--enable-benchmark=no "\
 "--enable-tests=no "\
 "--enable-endomorphism=no "\
-"${prefix_flags} "
+"${gmp_flags} "
 
 # Define bitcoin options.
 #------------------------------------------------------------------------------
 BITCOIN_OPTIONS=\
-"${prefix_flags} "\
+"${gmp_flags} "\
 "${with_boost} "\
 "${with_pkgconfigdir} "
 
