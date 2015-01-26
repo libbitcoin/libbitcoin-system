ab0c9f7f39a63f7d91cfad3d67e8ad8247410433
evoskuil <eric@voskuil.org>
2015-01-25 23:56:36 -0800

    Remove unused and dated LIBBITCOIN_VERSION, correct LIBBITCOIN_LIB_VERSION.

--- include/bitcoin/bitcoin/version.hpp
+++ include/bitcoin/bitcoin/version.hpp
@@ -20,22 +20,11 @@
 #ifndef LIBBITCOIN_VERSION_HPP
 #define LIBBITCOIN_VERSION_HPP
 
-// Caution, this is the only libbitcoin header that is guarenteed
-// to change with every libbitcoin release, including this header
-// will cause a recompile every time a new libbitcoin version is
-// released.
-//
-// LIBBITCOIN_VERSION % 100 is the patch level
-// LIBBITCOIN_VERSION / 100 % 1000 is the minor version
-// LIBBITCOIN_VERSION / 100000 is the major version
-
-#define LIBBITCOIN_VERSION 200000
-
 // LIBBITCOIN_LIB_VERSION is defined to be the same as LIBBITCOIN_VERSION
 // but as a *string* in the form "x.y[.z]" where x is the major version
 // number, y is the minor version number, and z is the patch level if not 0.
 // This is used for the user agent string during the connection handshake.
 
-#define LIBBITCOIN_LIB_VERSION "2.0"
+#define LIBBITCOIN_LIB_VERSION "2.8.0"
 
 #endif
