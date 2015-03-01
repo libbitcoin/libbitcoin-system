dd196d29bb67352502c2a4eb420fa93f67251efe
Neill Miller <neillm@thecodefactory.org>
2015-03-01 16:08:39 -0600

    - add more pkcs5_pbkdf2_hmac_sha512 test vectors (still untested) - add untested mnemonic test vectors to the build suite

---
+++ test/mnemonic.cpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2011-2015 libbitcoin developers (see AUTHORS)
+ *
+ * This file is part of libbitcoin.
+ *
+ * libbitcoin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License with
+ * additional permissions to the one published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version. For more information see LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "mnemonic.hpp"
+
+#include <boost/test/unit_test.hpp>
+#include <bitcoin/bitcoin.hpp>
+
+using namespace bc;
+
+BOOST_AUTO_TEST_SUITE(mnemonic_tests)
+
+BOOST_AUTO_TEST_CASE(entropy_to_mnemonic)
+{
+    for (const mnemonic_result& result: entropy_to_mnemonic_tests)
+    {
+        data_slice data = to_data_slice(result.input);
+        const auto words = create_mnemonic(data, bip39::language::en);
+        BOOST_REQUIRE(word_list.size());
+        BOOST_REQUIRE_EQUAL(join(words), result.result);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(mnemonic_to_seed)
+{
+    const auto passphrase = "TREZOR";
+    for (const mnemonic_result& result: mnemonic_to_seed_tests)
+    {
+        const auto words = split(result.input);
+        const data = decode_mnemonic(words, passphrase);
+        BOOST_REQUIRE(not data.empty());
+        BOOST_REQUIRE_EQUAL(encode_base16(data), result.result);
+    }
+}
+
+BOOST_AUTO_TEST_SUITE_END()
