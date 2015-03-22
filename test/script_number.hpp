67efa9d564beb06aac2ea3ed533fd1f61df24472
evoskuil <eric@voskuil.org>
2015-03-22 16:22:00 -0700

    Remove all c++11 from tests. Allows tests to validate non-c++11 linkage.

--- test/script_number.hpp
+++ test/script_number.hpp
@@ -58,10 +58,10 @@ const int64_t script_number_offsets[] =
     0x10000
 };
 
-constexpr size_t script_number_values_count =
+BC_CONSTEXPR size_t script_number_values_count =
 sizeof(script_number_values) / sizeof(script_number_values[0]);
 
-constexpr size_t script_number_offsets_count =
+BC_CONSTEXPR size_t script_number_offsets_count =
 sizeof(script_number_offsets) / sizeof(script_number_offsets[0]);
 
 struct script_number_buffer
