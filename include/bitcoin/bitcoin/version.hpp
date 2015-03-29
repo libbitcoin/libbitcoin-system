454074a51f43409a2e1bac64787b9d340f0275bc
evoskuil <eric@voskuil.org>
2015-03-29 16:22:31 -0700

    Update generated version.hpp.

--- include/bitcoin/bitcoin/version.hpp
+++ include/bitcoin/bitcoin/version.hpp
@@ -7,6 +7,14 @@
 #ifndef LIBBITCOIN_VERSION_HPP
 #define LIBBITCOIN_VERSION_HPP
 
+/**
+ * The semantic version of this repository as: [major].[minor].[patch]
+ * For interpretation of the versioning scheme see: http://semver.org
+ */
+
 #define LIBBITCOIN_VERSION "2.8.0"
+#define LIBBITCOIN_MAJOR_VERSION 2
+#define LIBBITCOIN_MINOR_VERSION 8
+#define LIBBITCOIN_PATCH_VERSION 0
 
 #endif
