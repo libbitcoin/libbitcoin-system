0fc640ecd6471618f7d716667f0631a3c1578d93
William Swanson <swansontec@gmail.com>
2015-04-04 16:10:27 -0700

    Use the bip39 unit tests verbatim
    There is no need to split the tests in two,
    which adds redundancy and makes them harder to compare with upstream.

--- test/mnemonic.cpp
+++ test/mnemonic.cpp
@@ -29,25 +29,25 @@ BOOST_AUTO_TEST_SUITE(mnemonic_tests)
 
 BOOST_AUTO_TEST_CASE(entropy_to_en_mnemonic)
 {
-    for (const mnemonic_result& result: entropy_to_mnemonic_en)
+    for (const mnemonic_result& result: mnemonic_bip39_vectors)
     {
         data_chunk entropy;
-        decode_base16(entropy, result.input);
+        decode_base16(entropy, result.entropy);
         const auto mnemonic = create_mnemonic(entropy, bip39::language::en);
         BOOST_REQUIRE(mnemonic.size() > 0);
-        BOOST_REQUIRE_EQUAL(join(mnemonic), result.expectation);
+        BOOST_REQUIRE_EQUAL(join(mnemonic), result.mnemonic);
     }
 }
 
 BOOST_AUTO_TEST_CASE(en_mnemonic_to_seed)
 {
     const auto& passphrase = "TREZOR";
-    for (const mnemonic_result& result: mnemonic_to_seed_en)
+    for (const mnemonic_result& result: mnemonic_bip39_vectors)
     {
-        const auto words = split(result.input);
+        const auto words = split(result.mnemonic);
         const auto seed = decode_mnemonic(words, passphrase);
         BOOST_REQUIRE(!seed.empty());
-        BOOST_REQUIRE_EQUAL(encode_base16(seed), result.expectation);
+        BOOST_REQUIRE_EQUAL(encode_base16(seed), result.seed);
     }
 }
 
