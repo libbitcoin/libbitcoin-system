ca9cf0d7a8dc68d275cceb40fad51b2d57fcd5bd
Phillip Mienk <mienkphi@gmail.com>
2015-05-18 02:53:57 -0700

    Add boost.iostreams dependency, replace data_buffer derived std::streambuf with data_source template for use with iostreams.

--- src/chain/transaction_input.cpp
+++ src/chain/transaction_input.cpp
@@ -19,8 +19,9 @@
  */
 #include <bitcoin/bitcoin/chain/transaction_input.hpp>
 #include <sstream>
+#include <boost/iostreams/stream.hpp>
 #include <bitcoin/bitcoin/constants.hpp>
-#include <bitcoin/bitcoin/utility/data_stream_host.hpp>
+#include <bitcoin/bitcoin/utility/data_source.hpp>
 #include <bitcoin/bitcoin/utility/istream.hpp>
 
 namespace libbitcoin {
@@ -35,8 +36,9 @@ void transaction_input::reset()
 
 bool transaction_input::from_data(const data_chunk& data)
 {
-    data_chunk_stream_host host(data);
-    return from_data(host.stream);
+    byte_source<data_chunk> source(data);
+    boost::iostreams::stream<byte_source<data_chunk>> istream(source);
+    return from_data(istream);
 }
 
 bool transaction_input::from_data(std::istream& stream)
--- src/message/nonce.cpp
+++ src/message/nonce.cpp
@@ -18,7 +18,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 #include <bitcoin/bitcoin/message/nonce.hpp>
-#include <bitcoin/bitcoin/utility/data_stream_host.hpp>
+#include <boost/iostreams/stream.hpp>
+#include <bitcoin/bitcoin/utility/data_source.hpp>
 #include <bitcoin/bitcoin/utility/istream.hpp>
 #include <bitcoin/bitcoin/utility/serializer.hpp>
 
@@ -32,8 +33,9 @@ void nonce_base::reset()
 
 bool nonce_base::from_data(const data_chunk& data)
 {
-    data_chunk_stream_host host(data);
-    return from_data(host.stream);
+    byte_source<data_chunk> source(data);
+    boost::iostreams::stream<byte_source<data_chunk>> istream(source);
+    return from_data(istream);
 }
 
 bool nonce_base::from_data(std::istream& stream)
