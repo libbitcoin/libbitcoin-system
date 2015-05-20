13c8ed54eff9b477e7ec9f0b7450b81606596887
evoskuil <eric@voskuil.org>
2015-05-19 22:51:42 -0700

    Remove test runner random option (incorrectly specified).

--- libbitcoin_test_runner.sh
+++ libbitcoin_test_runner.sh
@@ -10,7 +10,6 @@
 #==============================================================================
 BOOST_UNIT_TEST_OPTIONS=\
 "--run_test=* "\
-"--random=1 "\
 "--show_progress=no "\
 "--detect_memory_leak=0 "\
 "--report_level=no "\
