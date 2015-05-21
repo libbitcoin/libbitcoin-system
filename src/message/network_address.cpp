5d61be01d647427d9b9b40b183e4fcc4ec024eed
Phillip Mienk <mienkphi@gmail.com>
2015-05-20 21:03:22 -0700

    Correct error in assert.

--- src/message/network_address.cpp
+++ src/message/network_address.cpp
@@ -91,7 +91,7 @@ data_chunk network_address::to_data(bool with_timestamp) const
     data_chunk data;
     boost::iostreams::stream<byte_sink<data_chunk>> ostream(data);
     to_data(ostream, with_timestamp);
-    BOOST_ASSERT(data.size() == satoshi_size());
+    BOOST_ASSERT(data.size() == satoshi_size(with_timestamp));
     return data;
 }
 
