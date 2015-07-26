298648a9c04c58fbc081b1628fdbe746b97a213b
evoskuil <eric@voskuil.org>
2015-07-25 22:20:14 -0700

    Update version to 2.10.0, add min Automake comment to configure.ac.

--- include/bitcoin/bitcoin/version.hpp
+++ include/bitcoin/bitcoin/version.hpp
@@ -12,9 +12,9 @@
  * For interpretation of the versioning scheme see: http://semver.org
  */
 
-#define LIBBITCOIN_VERSION "2.9.0"
+#define LIBBITCOIN_VERSION "2.10.0"
 #define LIBBITCOIN_MAJOR_VERSION 2
-#define LIBBITCOIN_MINOR_VERSION 9
+#define LIBBITCOIN_MINOR_VERSION 10
 #define LIBBITCOIN_PATCH_VERSION 0
 
 #endif
