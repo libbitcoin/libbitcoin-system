0a009d6c7a29ef8fe73029d856ca89cd69fd798d
evoskuil <eric@voskuil.org>
2015-08-08 13:30:01 -0700

    Sort include.

--- src/utility/logger.cpp
+++ src/utility/logger.cpp
@@ -17,9 +17,10 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+#include <bitcoin/bitcoin/utility/logger.hpp>
+
 #include <iostream>
 #include <sstream>
-#include <bitcoin/bitcoin/utility/logger.hpp>
 #include <bitcoin/bitcoin/unicode/unicode.hpp>
 
 namespace libbitcoin {
