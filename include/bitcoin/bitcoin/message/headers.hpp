920c07c9f5147030dbeb1a0757932a7f76669ae9
Phillip Mienk <mienkphi@gmail.com>
2015-11-14 04:08:06 -0800

    Add headers message.

---
+++ include/bitcoin/bitcoin/message/headers.hpp
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2011-2015 libbitcoin developers (see AUTHORS)
+ *
+ * This file is part of libbitcoin.
+ *
+ * libbitcoin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License with
+ * additional permissions to the one published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version. For more information see LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef LIBBITCOIN_MESSAGE_HEADERS_HPP
+#define LIBBITCOIN_MESSAGE_HEADERS_HPP
+
+#include <cstddef>
+#include <cstdint>
+#include <initializer_list>
+#include <istream>
+#include <string>
+#include <bitcoin/bitcoin/define.hpp>
+#include <bitcoin/bitcoin/chain/header.hpp>
+#include <bitcoin/bitcoin/utility/data.hpp>
+#include <bitcoin/bitcoin/utility/reader.hpp>
+#include <bitcoin/bitcoin/utility/writer.hpp>
+
+namespace libbitcoin {
+namespace message {
+
+class BC_API headers
+{
+public:
+    static headers factory_from_data(const data_chunk& data);
+    static headers factory_from_data(std::istream& stream);
+    static headers factory_from_data(reader& source);
+
+    headers();
+    headers(const std::initializer_list<chain::header> elements);
+
+    bool from_data(const data_chunk& data);
+    bool from_data(std::istream& stream);
+    bool from_data(reader& source);
+    data_chunk to_data() const;
+    void to_data(std::ostream& stream) const;
+    void to_data(writer& sink) const;
+    bool is_valid() const;
+    void reset();
+    uint64_t serialized_size() const;
+
+    static const std::string command;
+
+    chain::header::list elements;
+};
+
+BC_API bool operator==(const headers& left, const headers& right);
+BC_API bool operator!=(const headers& left, const headers& right);
+
+} // namspace message
+} // namspace libbitcoin
+
+#endif
---
+++ test/message/headers.cpp
@@ -0,0 +1,117 @@
+/**
+ * Copyright (c) 2011-2013 libbitcoin developers (see AUTHORS)
+ *
+ * This file is part of libbitcoin.
+ *
+ * libbitcoin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License with
+ * additional permissions to the one published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version. For more information see LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <boost/test/unit_test.hpp>
+#include <boost/iostreams/stream.hpp>
+#include <bitcoin/bitcoin.hpp>
+
+using namespace bc;
+
+BOOST_AUTO_TEST_SUITE(headers_tests)
+
+BOOST_AUTO_TEST_CASE(from_data_insufficient_bytes_failure)
+{
+    const data_chunk raw{ 0xab, 0xcd };
+    message::headers instance{};
+
+    BOOST_REQUIRE_EQUAL(false, instance.from_data(raw));
+}
+
+BOOST_AUTO_TEST_CASE(roundtrip_to_data_factory_from_data_chunk)
+{
+    const message::headers expected
+    {
+        {
+            {
+                10,
+                hash_literal("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"),
+                hash_literal("4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"),
+                531234,
+                6523454,
+                68644,
+                65
+            }
+        }
+    };
+
+    const auto data = expected.to_data();
+    const auto result = message::headers::factory_from_data(data);
+
+    BOOST_REQUIRE(result.is_valid());
+    BOOST_REQUIRE(expected == result);
+    BOOST_REQUIRE_EQUAL(data.size(), result.serialized_size());
+    BOOST_REQUIRE_EQUAL(expected.serialized_size(), result.serialized_size());
+}
+
+BOOST_AUTO_TEST_CASE(roundtrip_to_data_factory_from_data_stream)
+{
+    const message::headers expected
+    {
+        {
+            {
+                15,
+                hash_literal("00acadae0019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"),
+                hash_literal("4a5e1e4bbbccddee32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"),
+                542344,
+                1247742,
+                34632,
+                146
+            }
+        }
+    };
+
+    const auto data = expected.to_data();
+    data_source istream(data);
+    auto result = message::headers::factory_from_data(istream);
+
+    BOOST_REQUIRE(result.is_valid());
+    BOOST_REQUIRE(expected == result);
+    BOOST_REQUIRE_EQUAL(data.size(), result.serialized_size());
+    BOOST_REQUIRE_EQUAL(expected.serialized_size(), result.serialized_size());
+}
+
+BOOST_AUTO_TEST_CASE(roundtrip_to_data_factory_from_data_reader)
+{
+    const message::headers expected
+    {
+        {
+            {
+                7,
+                hash_literal("1234123412341234123412341234123412341234123412341234123412341234"),
+                hash_literal("4321432143214321432143214321432143214321432143214321432143214321"),
+                83221,
+                4353212,
+                54234,
+                367
+            }
+        }
+    };
+
+    const auto data = expected.to_data();
+    data_source istream(data);
+    istream_reader source(istream);
+    const auto result = message::headers::factory_from_data(source);
+
+    BOOST_REQUIRE(result.is_valid());
+    BOOST_REQUIRE(expected == result);
+    BOOST_REQUIRE_EQUAL(data.size(), result.serialized_size());
+    BOOST_REQUIRE_EQUAL(expected.serialized_size(), result.serialized_size());
+}
+
+BOOST_AUTO_TEST_SUITE_END()
