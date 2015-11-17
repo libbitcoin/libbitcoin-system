ddeab9ceaa457368a604dff75b8af8faa56b188d
evoskuil <eric@voskuil.org>
2015-11-16 23:47:54 -0800

    Build from sync branch.

--- install.sh
+++ install.sh
@@ -561,7 +561,7 @@ build_all()
     build_from_tarball_icu $ICU_URL $ICU_ARCHIVE icu $PARALLEL $ICU_OPTIONS
     build_from_tarball_boost $BOOST_URL $BOOST_ARCHIVE boost $PARALLEL $BOOST_OPTIONS
     build_from_github libbitcoin secp256k1 version3 $PARALLEL "$@" $SECP256K1_OPTIONS
-    build_from_travis libbitcoin libbitcoin master $PARALLEL "$@" $BITCOIN_OPTIONS
+    build_from_travis libbitcoin libbitcoin sync $PARALLEL "$@" $BITCOIN_OPTIONS
 }
 
 
