77a0c3547ae0cc609dfa07e4ff322a4a12e1ea84
evoskuil <eric@voskuil.org>
2016-08-04 15:59:28 -0700

    Fix up version tests, add neg test for matching sender services.

--- test/message/version.cpp
+++ test/message/version.cpp
@@ -37,7 +37,6 @@ bool equal(const message::version& left, const message::version& right)
     return (left.value == right.value)
         && (left.services_sender == right.services_sender)
         && (left.timestamp == right.timestamp)
-        ////&& (left.services_recevier == right.services_recevier)
         && equal_without_timestamp(left.address_recevier, right.address_recevier)
         && equal_without_timestamp(left.address_sender, right.address_sender)
         && (left.nonce == right.nonce)
@@ -56,14 +55,14 @@ BOOST_AUTO_TEST_CASE(from_data_insufficient_bytes_failure)
     BOOST_REQUIRE_EQUAL(false, instance.from_data(raw));
 }
 
-BOOST_AUTO_TEST_CASE(roundtrip_to_data_factory_from_data_chunk)
+BOOST_AUTO_TEST_CASE(version__from_data_chunk__mismatched_sender_services__invalid)
 {
+    const auto sender_services = 1515u;
     const message::version expected
     {
         210u,
-        1515u,
+        sender_services,
         979797u,
-        ////424242u,
         {
             734678u,
             5357534u,
@@ -77,7 +76,48 @@ BOOST_AUTO_TEST_CASE(roundtrip_to_data_factory_from_data_chunk)
         },
         {
             46324u,
-            861275u,
+            sender_services + 1,
+            {
+                {
+                    0xab, 0xcd, 0x6a, 0x40, 0x33, 0x92, 0x77, 0xb4,
+                    0xe0, 0xb8, 0xda, 0x43, 0x61, 0x66, 0x6a, 0x88
+                }
+            },
+            351u
+        },
+        13626u,
+        "my agent",
+        100u
+    };
+
+    const auto data = expected.to_data();
+    const auto result = message::version::factory_from_data(data);
+
+    BOOST_REQUIRE(!result.is_valid());
+}
+
+BOOST_AUTO_TEST_CASE(roundtrip_to_data_factory_from_data_chunk)
+{
+    const auto sender_services = 1515u;
+    const message::version expected
+    {
+        210u,
+        sender_services,
+        979797u,
+        {
+            734678u,
+            5357534u,
+            {
+                {
+                    0x47, 0x81, 0x6a, 0x40, 0xbb, 0x92, 0xbd, 0xb4,
+                    0xe0, 0xb8, 0x25, 0x68, 0x61, 0xf9, 0x6a, 0x55
+                }
+            },
+            123u
+        },
+        {
+            46324u,
+            sender_services,
             {
                 {
                     0xab, 0xcd, 0x6a, 0x40, 0x33, 0x92, 0x77, 0xb4,
@@ -102,12 +142,12 @@ BOOST_AUTO_TEST_CASE(roundtrip_to_data_factory_from_data_chunk)
 
 BOOST_AUTO_TEST_CASE(roundtrip_to_data_factory_from_data_stream)
 {
+    const auto sender_services = 1515u;
     const message::version expected
     {
         210u,
-        1515u,
+        sender_services,
         979797u,
-        ////424242u,
         {
             734678u,
             5357534u,
@@ -121,7 +161,7 @@ BOOST_AUTO_TEST_CASE(roundtrip_to_data_factory_from_data_stream)
         },
         {
             46324u,
-            861275u,
+            sender_services,
             {
                 {
                     0xab, 0xcd, 0x6a, 0x40, 0x33, 0x92, 0x77, 0xb4,
@@ -147,12 +187,12 @@ BOOST_AUTO_TEST_CASE(roundtrip_to_data_factory_from_data_stream)
 
 BOOST_AUTO_TEST_CASE(roundtrip_to_data_factory_from_data_reader)
 {
+    const auto sender_services = 1515u;
     const message::version expected
     {
         210u,
-        1515u,
+        sender_services,
         979797u,
-        ////424242u,
         {
             734678u,
             5357534u,
@@ -166,7 +206,7 @@ BOOST_AUTO_TEST_CASE(roundtrip_to_data_factory_from_data_reader)
         },
         {
             46324u,
-            861275u,
+            sender_services,
             {
                 {
                     0xab, 0xcd, 0x6a, 0x40, 0x33, 0x92, 0x77, 0xb4,
