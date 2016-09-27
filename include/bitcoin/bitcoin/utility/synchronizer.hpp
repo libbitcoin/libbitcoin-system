363f3de248482ae969ebe83049959bb2f02a63e0
evoskuil <eric@voskuil.org>
2016-09-26 20:04:44 -0700

    Generalize synchronizer to join in error|success|count.

--- include/bitcoin/bitcoin/utility/synchronizer.hpp
+++ include/bitcoin/bitcoin/utility/synchronizer.hpp
@@ -22,27 +22,80 @@
 
 #include <cstddef>
 #include <memory>
+#include <stdexcept>
+#include <string>
 #include <bitcoin/bitcoin/error.hpp>
 #include <bitcoin/bitcoin/utility/assert.hpp>
 #include <bitcoin/bitcoin/utility/thread.hpp>
 
 namespace libbitcoin {
 
-// It is not possible for this class to produce a deadlock.
+enum class synchronizer_terminate
+{
+    /// Terminate on first error or count.
+    /// Return code on first error, otherwise success.
+    on_error,
+
+    /// Terminate first success (priority) or count.
+    /// Return success on first success, otherwise operation_failed.
+    on_success,
+
+    /// Terminate on count only.
+    /// Return success once count is reached (always).
+    on_count
+};
 
 template <typename Handler>
 class synchronizer
 {
 public:
     synchronizer(Handler handler, size_t clearance_count,
-        const std::string& name, bool suppress_errors)
+        const std::string& name, synchronizer_terminate mode)
       : handler_(handler),
         name_(name),
         clearance_count_(clearance_count),
         counter_(std::make_shared<size_t>(0)),
         mutex_(std::make_shared<upgrade_mutex>()),
-        suppress_errors_(suppress_errors)
+        terminate_(mode)
     {
+    }
+
+    // Determine if the code is cause for termination.
+    bool complete(const code& ec)
+    {
+        switch (terminate_)
+        {
+            case synchronizer_terminate::on_error:
+                return !!ec;
+
+            case synchronizer_terminate::on_success:
+                return !ec;
+
+            case synchronizer_terminate::on_count:
+                return false;
+
+            default:
+                throw std::invalid_argument("mode");
+        }
+    }
+
+    // Assuming we are terminating, generate the proper result code.
+    code result(const code& ec)
+    {
+        switch (terminate_)
+        {
+            case synchronizer_terminate::on_error:
+                return ec ? ec : error::success;
+
+            case synchronizer_terminate::on_success:
+                return !ec ? ec : error::operation_failed;
+
+            case synchronizer_terminate::on_count:
+                return error::success;
+
+            default:
+                throw std::invalid_argument("mode");
+        }
     }
 
     template <typename... Args>
@@ -55,6 +108,7 @@ public:
         const auto initial_count = *counter_;
         BITCOIN_ASSERT(initial_count <= clearance_count_);
 
+        // Another handler cleared this and shortcircuited the count, ignore.
         if (initial_count == clearance_count_)
         {
             mutex_->unlock_upgrade();
@@ -62,8 +116,7 @@ public:
             return;
         }
 
-        const auto terminate = ec && !suppress_errors_;
-        const auto count = terminate ? clearance_count_ : initial_count + 1;
+        const auto count = complete(ec) ? clearance_count_ : initial_count + 1;
         const auto cleared = count == clearance_count_;
 
         //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -73,19 +126,14 @@ public:
         ///////////////////////////////////////////////////////////////////////
 
         if (cleared)
-        {
-            // If not suppressing errors pass the last result code.
-            // This is useful when clearance count is one.
-            const auto result = suppress_errors_ ? error::success : ec;
-            handler_(result, std::forward<Args>(args)...);
-        }
+            handler_(result(ec), std::forward<Args>(args)...);
     }
 
 private:
     Handler handler_;
     const std::string name_;
     const size_t clearance_count_;
-    const bool suppress_errors_;
+    const synchronizer_terminate terminate_;
 
     // We use pointer to reference the same value/mutex across instance copies.
     std::shared_ptr<size_t> counter_;
@@ -94,10 +142,10 @@ private:
 
 template <typename Handler>
 synchronizer<Handler> synchronize(Handler handler, size_t clearance_count,
-    const std::string& name, bool suppress_errors=false)
+    const std::string& name, synchronizer_terminate mode=
+    synchronizer_terminate::on_error)
 {
-    return synchronizer<Handler>(handler, clearance_count, name,
-        suppress_errors);
+    return synchronizer<Handler>(handler, clearance_count, name, mode);
 }
 
 } // libbitcoin
