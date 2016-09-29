546a9a70e0ab508b5ab25905b1b7ba7e9d32e927
Phillip Mienk <mienkphi@gmail.com>
2016-09-29 14:59:44 -0700

    Remove constexpr network_address constructor due to C++14 requirement due to std::move.

--- include/bitcoin/bitcoin/message/network_address.hpp
+++ include/bitcoin/bitcoin/message/network_address.hpp
@@ -54,7 +54,8 @@ public:
       : timestamp_(timestamp), services_(services), ip_(ip), port_(port)
     {}
 
-    BC_CONSTCTOR network_address(uint32_t timestamp, uint64_t services,
+    // Disabled BC_CONSTCTOR due to C++14 dependence on std::move constexpr.
+    /*BC_CONSTCTOR*/ network_address(uint32_t timestamp, uint64_t services,
         ip_address&& ip, uint16_t port)
       : timestamp_(timestamp), services_(services),
         ip_(std::move(ip)), port_(port)
