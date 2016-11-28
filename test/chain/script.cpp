ed94b22fc4ade9cb801063df9ae78b7ea20fd7ab
evoskuil <eric@voskuil.org>
2016-11-28 13:06:59 -0800

    Add block 438513 test case (CLTV P2SH).

--- test/chain/script.cpp
+++ test/chain/script.cpp
@@ -17,9 +17,11 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+#include <boost/test/unit_test.hpp>
+
+#include <utility>
 #include <boost/algorithm/string.hpp>
 #include <boost/lexical_cast.hpp>
-#include <boost/test/unit_test.hpp>
 #include <bitcoin/bitcoin.hpp>
 #include "script.hpp"
 
@@ -324,6 +326,46 @@ BOOST_AUTO_TEST_CASE(script__from_data__internal_invalid_wire_code__success)
 
     script instance;
     BOOST_REQUIRE(instance.from_data(raw, false));
+}
+
+BOOST_AUTO_TEST_CASE(script__native__block_438513_tx__valid)
+{
+    ////06:21:05.532171 DEBUG [blockchain] Input validation failed (stack false)
+    //// lib         : false
+    //// forks       : 62
+    //// outpoint    : 8e51d775e0896e03149d585c0655b3001da0c55068b0885139ac6ec34cf76ba0:0
+    //// script      : a914faa558780a5767f9e3be14992a578fc1cbcf483087
+    //// inpoint     : 6b7f50afb8448c39f4714a73d2b181d3e3233e84670bdfda8f141db668226c54:0
+    //// transaction : 0100000001a06bf74cc36eac395188b06850c5a01d00b355065c589d14036e89e075d7518e000000009d483045022100ba555ac17a084e2a1b621c2171fa563bc4fb75cd5c0968153f44ba7203cb876f022036626f4579de16e3ad160df01f649ffb8dbf47b504ee56dc3ad7260af24ca0db0101004c50632102768e47607c52e581595711e27faffa7cb646b4f481fe269bd49691b2fbc12106ad6704355e2658b1756821028a5af8284a12848d69a25a0ac5cea20be905848eb645fd03d3b065df88a9117cacfeffffff0158920100000000001976a9149d86f66406d316d44d58cbf90d71179dd8162dd388ac355e2658
+
+    static const auto input_index = 0u;
+    static const auto forks = static_cast<rule_fork>(62);
+    static const auto encoded_script = "a914faa558780a5767f9e3be14992a578fc1cbcf483087";
+    static const auto encoded_tx = "0100000001a06bf74cc36eac395188b06850c5a01d00b355065c589d14036e89e075d7518e000000009d483045022100ba555ac17a084e2a1b621c2171fa563bc4fb75cd5c0968153f44ba7203cb876f022036626f4579de16e3ad160df01f649ffb8dbf47b504ee56dc3ad7260af24ca0db0101004c50632102768e47607c52e581595711e27faffa7cb646b4f481fe269bd49691b2fbc12106ad6704355e2658b1756821028a5af8284a12848d69a25a0ac5cea20be905848eb645fd03d3b065df88a9117cacfeffffff0158920100000000001976a9149d86f66406d316d44d58cbf90d71179dd8162dd388ac355e2658";
+
+    data_chunk decoded_tx;
+    BOOST_REQUIRE(decode_base16(decoded_tx, encoded_tx));
+
+    data_chunk decoded_script;
+    BOOST_REQUIRE(decode_base16(decoded_script, encoded_script));
+
+    transaction tx;
+    BOOST_REQUIRE(tx.from_data(decoded_tx));
+
+    auto& input = tx.inputs()[input_index];
+    auto& prevout = input.previous_output().validation.cache;
+
+    prevout.set_script(script::factory_from_data(decoded_script, false));
+    BOOST_REQUIRE(prevout.script().is_valid());
+
+    // [3045022100ba555ac17a084e2a1b621c2171fa563bc4fb75cd5c0968153f44ba7203cb876f022036626f4579de16e3ad160df01f649ffb8dbf47b504ee56dc3ad7260af24ca0db01] [00] [632102768e47607c52e581595711e27faffa7cb646b4f481fe269bd49691b2fbc12106ad6704355e2658b1756821028a5af8284a12848d69a25a0ac5cea20be905848eb645fd03d3b065df88a9117cac]
+    ////std::cout << prevout.script().to_string(forks) << std::endl;
+
+    // hash160 [faa558780a5767f9e3be14992a578fc1cbcf4830] equal
+    ////std::cout << input.script().to_string(forks) << std::endl;
+
+    const auto result = script::verify(tx, input_index, forks);
+    BOOST_REQUIRE_EQUAL(result.value(), error::success);
 }
 
 BOOST_AUTO_TEST_CASE(script__bip16__valid)
