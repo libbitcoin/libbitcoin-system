e390a526f2b37f86522ed70003e5476ede4265a3
evoskuil <eric@voskuil.org>
2016-12-06 01:09:24 -0800

    Move service.reset to spawn.

--- src/utility/threadpool.cpp
+++ src/utility/threadpool.cpp
@@ -63,6 +63,9 @@ size_t threadpool::size() const
 // Not thread safe.
 void threadpool::spawn(size_t number_threads, thread_priority priority)
 {
+    // This allows the pool to be restarted.
+    service_.reset();
+
     for (size_t i = 0; i < number_threads; ++i)
         spawn_once(priority);
 }
@@ -139,9 +142,6 @@ void threadpool::join()
 
     threads_mutex_.unlock();
     ///////////////////////////////////////////////////////////////////////////
-
-    // This allows the pool to be cleanly restarted by calling spawn.
-    service_.reset();
 }
 
 asio::service& threadpool::service()
