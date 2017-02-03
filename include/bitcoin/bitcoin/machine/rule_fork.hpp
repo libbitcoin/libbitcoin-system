ebd45aceb97cd742190f48d9f59dcdf4b22147a6
evoskuil <eric@voskuil.org>
2017-02-02 23:58:14 -0800

    Hardwire collision fork, fix chain_state caching for collision hash.

--- include/bitcoin/bitcoin/machine/rule_fork.hpp
+++ include/bitcoin/bitcoin/machine/rule_fork.hpp
@@ -47,11 +47,11 @@ enum rule_fork : uint32_t
     /// Operation nop2 becomes check locktime verify (soft fork)
     bip65_rule = 1u << 5,
 
-    /// Assume hash collisions cannot happen (hard fork, optimization)
-    allow_collisions = 1u << 6,
-
     /// Hard code activation heights (hard fork, optimization)
     bip90_rule = 1u << 7,
+
+    /// Assume hash collisions cannot happen (hard fork, optimization)
+    allow_collisions = 1u << 29,
 
     /// Sentinel bit to indicate tx was validated under a checkpoint.
     checkpointed = 1u << 30,
