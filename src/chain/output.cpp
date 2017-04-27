dc1892338dfa73a15dfe20bccf3123f6b7bed5fc
evoskuil <eric@voskuil.org>
2017-04-27 13:54:26 -0700

    Unspendable outputs are not dust at any value.

--- src/chain/output.cpp
+++ src/chain/output.cpp
@@ -240,6 +240,12 @@ void output::set_script(chain::script&& value)
     invalidate_cache();
 }
 
+bool output::is_dust(uint64_t minimum_value) const
+{
+    // If provably unspendable it does not expand the unspent output set.
+    return value_ < minimum_value && !script_.is_unspendable();
+}
+
 // protected
 void output::invalidate_cache() const
 {
