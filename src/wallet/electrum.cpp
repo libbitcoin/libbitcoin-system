d06c36e213ddd2750cf802f464ff3a7a86de70ca
Neill Miller <neillm@thecodefactory.org>
2017-09-16 14:30:41 -0500

    Add initial implementation of electrum 2.x mnemonic support

---
+++ src/wallet/electrum.cpp
@@ -0,0 +1,231 @@
+/**
+ * Copyright (c) 2011-2017 libbitcoin developers (see AUTHORS)
+ *
+ * This file is part of libbitcoin.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <bitcoin/bitcoin/wallet/electrum.hpp>
+
+#include <algorithm>
+#include <cstdint>
+#include <boost/locale.hpp>
+#include <boost/range/adaptor/reversed.hpp>
+#include <bitcoin/bitcoin/define.hpp>
+#include <bitcoin/bitcoin/unicode/unicode.hpp>
+#include <bitcoin/bitcoin/unicode/unicode_ostream.hpp>
+#include <bitcoin/bitcoin/utility/assert.hpp>
+#include <bitcoin/bitcoin/utility/binary.hpp>
+#include <bitcoin/bitcoin/utility/collection.hpp>
+#include <bitcoin/bitcoin/utility/string.hpp>
+#include <bitcoin/bitcoin/utility/container_sink.hpp>
+#include <bitcoin/bitcoin/utility/ostream_writer.hpp>
+#include <bitcoin/bitcoin/wallet/electrum_dictionary.hpp>
+#include "../math/external/pkcs5_pbkdf2.h"
+
+namespace libbitcoin {
+namespace wallet {
+namespace electrum {
+
+// Electrum mnemonic private constants.
+static const std::string passphrase_prefix = "electrum";
+static constexpr size_t hmac_iterations = 2048;
+static const auto hmac_data = to_chunk("Seed version");
+
+#ifdef WITH_ICU
+
+static uint32_t special_modulo(int index_distance, int dictionary_length)
+{
+    if (index_distance < 0)
+        return dictionary_length - (-index_distance % dictionary_length);
+
+    return index_distance % dictionary_length;
+}
+
+static data_chunk old_mnemonic_decode(const word_list& mnemonic)
+{
+    static constexpr size_t mnemonic_word_multiple = 3;
+
+    if ((mnemonic.size() < mnemonic_word_multiple) ||
+        (mnemonic.size() % mnemonic_word_multiple) != 0)
+        return {};
+
+    const auto& lexicon = language::electrum::en_v1;
+    const auto dictionary_length = lexicon.size();
+    const auto seed_size = sizeof(uint32_t) * mnemonic.size() / 3;
+
+    data_chunk seed;
+    seed.reserve(seed_size);
+    data_sink ostream(seed);
+    ostream_writer sink(ostream);
+
+    for (size_t i = 0; i < mnemonic.size() / 3; i += 3)
+    {
+        const auto index_1 = find_position(lexicon, mnemonic[i]);
+        const auto index_2 = find_position(lexicon, mnemonic[i+1]);
+        const auto index_3 = find_position(lexicon, mnemonic[i+2]);
+
+        if ((index_1 == -1) || (index_2 == -1) || (index_3 == -1))
+            return {};
+
+        const uint32_t value = index_1 + dictionary_length *
+            special_modulo(index_2 - index_1, dictionary_length) +
+            dictionary_length * dictionary_length *
+            special_modulo(index_3 - index_2, dictionary_length);
+
+        sink.write_variable_little_endian(value);
+    }
+
+    return seed;
+}
+
+static bool is_old_seed(const word_list& mnemonic)
+{
+    // valid electrum 1.x seed lengths
+    static constexpr size_t seed_size_v1_1 = 16;
+    static constexpr size_t seed_size_v1_2 = 32;
+
+    // valid electrum 1.x mnemonic lengths
+    static constexpr size_t mnemonic_size_v1_1 = 12;
+    static constexpr size_t mnemonic_size_v1_2 = 24;
+
+    const auto seed = old_mnemonic_decode(mnemonic);
+
+    return !seed.empty() && (
+        seed.size() == seed_size_v1_1 ||
+        seed.size() == seed_size_v1_2 ||
+        mnemonic.size() == mnemonic_size_v1_1 ||
+        mnemonic.size() == mnemonic_size_v1_2);
+}
+
+static bool is_new_seed(const word_list& mnemonic, const data_slice& prefix)
+{
+    const auto seed = hmac_sha512_hash(
+        to_chunk(to_normal_nfkd_form(join(mnemonic))), hmac_data);
+
+    return std::equal(prefix.begin(), prefix.end(), seed.begin());
+}
+
+static word_list mnemonic_encode(cpp_int entropy, const dictionary& lexicon)
+{
+    word_list mnemonic;
+    const auto dictionary_length = lexicon.size();
+
+    while (entropy)
+    {
+        const cpp_int index = entropy % dictionary_length;
+        entropy /= dictionary_length;
+        mnemonic.push_back(lexicon[static_cast<size_t>(index)]);
+    }
+
+    return mnemonic;
+}
+
+static cpp_int mnemonic_decode(const word_list& mnemonic,
+    const dictionary& lexicon)
+{
+    cpp_int entropy = 0;
+    const auto dictionary_length = lexicon.size();
+
+    for (const auto& word : boost::adaptors::reverse(mnemonic))
+    {
+        const auto position = find_position(lexicon, word);
+        if (position == -1)
+            return cpp_int(1);
+
+        entropy *= dictionary_length + position;
+    }
+
+    return entropy;
+}
+
+static data_chunk get_seed_prefix(const seed prefix)
+{
+    switch (prefix)
+    {
+        case seed::standard:
+            return seed_prefix_standard;
+        case seed::witness:
+            return seed_prefix_witness;
+        case seed::two_factor_authentication:
+            return seed_prefix_two_factor_authentication;
+    }
+
+    return {};
+}
+
+word_list create_mnemonic(const data_chunk& entropy, const dictionary& lexicon,
+    const seed prefix)
+{
+    cpp_int nonce = 0;
+    word_list mnemonic;
+
+    const auto electrum_prefix = get_seed_prefix(prefix);
+
+    // This usage requires the hex string for arbitrary precision
+    // integer construction
+    const auto numeric_entropy = cpp_int("0x" + encode_base16(entropy));
+
+    do
+    {
+        cpp_int current_entropy = numeric_entropy + nonce++;
+        mnemonic = mnemonic_encode(current_entropy, lexicon);
+        BITCOIN_ASSERT(mnemonic_decode(mnemonic, lexicon) == 0);
+
+    } while (is_old_seed(mnemonic) || !is_new_seed(mnemonic, electrum_prefix));
+
+    return mnemonic;
+}
+
+long_hash decode_mnemonic(const word_list& mnemonic,
+    const std::string& passphrase)
+{
+    const auto sentence = join(mnemonic);
+    const auto salt = to_normal_nfkd_form(passphrase_prefix + passphrase);
+    return pkcs5_pbkdf2_hmac_sha512(to_chunk(sentence), to_chunk(salt),
+        hmac_iterations);
+}
+
+bool validate_mnemonic(const word_list& mnemonic, const dictionary& lexicon,
+    const seed prefix)
+{
+    return is_new_seed(mnemonic, get_seed_prefix(prefix)) &&
+        (mnemonic_decode(mnemonic, lexicon) == 0);
+}
+
+bool validate_mnemonic(const word_list& mnemonic,
+    const dictionary_list& lexicons, const seed prefix)
+{
+    for (const auto& lexicon: lexicons)
+        if (validate_mnemonic(mnemonic, *lexicon, prefix))
+            return true;
+
+    return false;
+}
+
+#endif
+
+// This operation does not require normalization support
+long_hash decode_mnemonic(const word_list& mnemonic)
+{
+    const auto sentence = join(mnemonic);
+    const auto& salt = passphrase_prefix;
+    return pkcs5_pbkdf2_hmac_sha512(to_chunk(sentence), to_chunk(salt),
+        hmac_iterations);
+}
+
+
+} // namespace electrum
+} // namespace wallet
+} // namespace libbitcoin
---
+++ test/wallet/electrum.cpp
@@ -0,0 +1,114 @@
+/**
+ * Copyright (c) 2011-2017 libbitcoin developers (see AUTHORS)
+ *
+ * This file is part of libbitcoin.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <boost/test/unit_test.hpp>
+#include <bitcoin/bitcoin.hpp>
+
+using namespace bc;
+using namespace bc::wallet;
+
+BOOST_AUTO_TEST_SUITE(electrum_tests)
+
+// These test cases were generated using electrum code and verified to
+// match output from:
+//
+// https://github.com/spesmilo/electrum/blob/master/lib/mnemonic.py
+
+BOOST_AUTO_TEST_CASE(electrum__decode_mnemonic__no_passphrase_1)
+{
+    static const word_list mnemonic{ { "foo", "bar", "baz"} };
+    const auto seed = electrum::decode_mnemonic(mnemonic);
+    BOOST_REQUIRE_EQUAL(encode_base16(seed), "c4033901dd61ba26cfd0a1cf1ceb4b347606635aa3cb951eb6e819d58beedc04dd400a2e600d783c83c75879d6538abeecc7bb1b292b2a4d775d348d5d686427");
+}
+
+BOOST_AUTO_TEST_CASE(electrum__decode_mnemonic__no_passphrase_2)
+{
+    static const word_list mnemonic
+    {
+        {
+            "giggle", "crush", "argue", "inflict", "wear", "defy", "combine", "evolve", "tiger", "spatial", "crumble", "fury"
+        }
+    };
+    const auto seed = electrum::decode_mnemonic(mnemonic);
+    BOOST_REQUIRE_EQUAL(encode_base16(seed), "eb095e70f0eb8d24f2cef276c75b136c2ff41eb72a9291eb22ba45d11fbb875a965bd6ae09873543eea98bf3ae05067203cd56388e0978aebc3a511932704d80");
+}
+
+#ifdef WITH_ICU
+
+BOOST_AUTO_TEST_CASE(electrum__decode_mnemonic__passphrase)
+{
+    static const word_list mnemonic{ { "foobar" } };
+    const auto passphrase = "none";
+    const auto seed = electrum::decode_mnemonic(mnemonic, passphrase);
+    BOOST_REQUIRE_EQUAL(encode_base16(seed), "741b72fd15effece6bfe5a26a52184f66811bd2be363190e07a42cca442b1a5bb22b3ad0eb338197287e6d314866c7fba863ac65d3f156087a5052ebc7157fce");
+}
+
+BOOST_AUTO_TEST_CASE(electrum__create_mnemonic__dictionary)
+{
+    data_chunk entropy;
+    decode_base16(entropy, "05E669B4270F4E25BCE6FC3736170D423C");
+    const auto mnemonic = electrum::create_mnemonic(entropy, language::en);
+    BOOST_REQUIRE(!mnemonic.empty());
+    BOOST_REQUIRE_EQUAL(join(mnemonic), "giggle crush argue inflict wear defy combine evolve tiger spatial crumble fury");
+    BOOST_REQUIRE(electrum::validate_mnemonic(mnemonic, language::en));
+}
+
+BOOST_AUTO_TEST_CASE(electrum__create_mnemonic__dictionary_prefix)
+{
+    data_chunk entropy;
+    decode_base16(entropy, "0B0D80F992A51348A89AEB5196FA0BD0D6");
+    const auto mnemonic = electrum::create_mnemonic(entropy, language::en, electrum::seed::standard);
+    BOOST_REQUIRE(!mnemonic.empty());
+    BOOST_REQUIRE_EQUAL(join(mnemonic), "crawl consider laptop bonus stove chase earn battle feed town scatter radio");
+    BOOST_REQUIRE(electrum::validate_mnemonic(mnemonic, language::en, electrum::seed::standard));
+}
+
+BOOST_AUTO_TEST_CASE(electrum__create_mnemonic__large_entropy_1)
+{
+    data_chunk entropy;
+    decode_base16(entropy, "2B18CF24D3C26529B18D733ABADFF49F06E65EBF74263F0BA00D11A9B0CBA8D303");
+    const auto mnemonic = electrum::create_mnemonic(entropy);
+    BOOST_REQUIRE(!mnemonic.empty());
+    BOOST_REQUIRE_EQUAL(join(mnemonic), "icon person grape only cash addict fringe disease luggage worry consider hover dignity wood street deny purpose shiver network chaos pole since shoe climb");
+    BOOST_REQUIRE(electrum::validate_mnemonic(mnemonic, language::en, electrum::seed::standard));
+}
+
+BOOST_AUTO_TEST_CASE(electrum__create_mnemonic__large_entropy_2)
+{
+    data_chunk entropy;
+    decode_base16(entropy, "B0756302179E800B182514C729F1D6814C377FF06097569EF540E6C1F1950F08");
+    const auto mnemonic = electrum::create_mnemonic(entropy);
+    BOOST_REQUIRE(!mnemonic.empty());
+    BOOST_REQUIRE_EQUAL(join(mnemonic), "divide february web hire limb run reject nuclear army zone brick below public ladder deer below again cluster divorce ketchup aerobic flee lonely absent");
+    BOOST_REQUIRE(electrum::validate_mnemonic(mnemonic, language::en, electrum::seed::standard));
+}
+
+BOOST_AUTO_TEST_CASE(electrum__create_mnemonic__invalid)
+{
+    data_chunk entropy;
+    decode_base16(entropy, "2B18CF24D3C26529B18D733ABADFF49F06E65EBF74263F0BA00D11A9B0CBA8D303");
+    auto mnemonic = electrum::create_mnemonic(entropy);
+    BOOST_REQUIRE(!mnemonic.empty());
+    mnemonic[0] = "invalid";
+    BOOST_REQUIRE(!electrum::validate_mnemonic(mnemonic, language::en, electrum::seed::standard));
+}
+
+#endif
+
+BOOST_AUTO_TEST_SUITE_END()
