37838ad08d676d0aa4e0e570898135a0094949ba
evoskuil <eric@voskuil.org>
2017-11-17 23:16:48 -0800

    Add witness inventory vector types to enumeration.

--- include/bitcoin/bitcoin/message/inventory_vector.hpp
+++ include/bitcoin/bitcoin/message/inventory_vector.hpp
@@ -36,13 +36,17 @@ class BC_API inventory_vector
 public:
     typedef std::vector<inventory_vector> list;
 
-    enum class type_id
+    enum class type_id : uint64_t
     {
         error = 0,
         transaction = 1,
         block = 2,
         filtered_block = 3,
-        compact_block = 4
+        compact_block = 4,
+        witness = (1u << 30),
+        witness_transaction = witness | transaction,
+        witness_block = witness | block,
+        reserved = witness | filtered_block
     };
 
     static type_id to_type(uint32_t value);
