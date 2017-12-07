6a6343ccebb89c2e3301ce5a097bf528b4b82440
evoskuil <eric@voskuil.org>
2017-12-07 14:35:18 -0800

    Use brace elision for non-aggregate initializers.

--- test/wallet/electrum.cpp
+++ test/wallet/electrum.cpp
@@ -32,7 +32,7 @@ BOOST_AUTO_TEST_SUITE(electrum_tests)
 
 BOOST_AUTO_TEST_CASE(electrum__decode_mnemonic__no_passphrase_1)
 {
-    static const word_list mnemonic{ { "foo", "bar", "baz"} };
+    static const word_list mnemonic{ "foo", "bar", "baz" };
     const auto seed = electrum::decode_mnemonic(mnemonic);
     BOOST_REQUIRE_EQUAL(encode_base16(seed), "c4033901dd61ba26cfd0a1cf1ceb4b347606635aa3cb951eb6e819d58beedc04dd400a2e600d783c83c75879d6538abeecc7bb1b292b2a4d775d348d5d686427");
 }
@@ -41,9 +41,7 @@ BOOST_AUTO_TEST_CASE(electrum__decode_mnemonic__no_passphrase_2)
 {
     static const word_list mnemonic
     {
-        {
-            "giggle", "crush", "argue", "inflict", "wear", "defy", "combine", "evolve", "tiger", "spatial", "crumble", "fury"
-        }
+        "giggle", "crush", "argue", "inflict", "wear", "defy", "combine", "evolve", "tiger", "spatial", "crumble", "fury"
     };
     const auto seed = electrum::decode_mnemonic(mnemonic);
     BOOST_REQUIRE_EQUAL(encode_base16(seed), "eb095e70f0eb8d24f2cef276c75b136c2ff41eb72a9291eb22ba45d11fbb875a965bd6ae09873543eea98bf3ae05067203cd56388e0978aebc3a511932704d80");
@@ -53,7 +51,7 @@ BOOST_AUTO_TEST_CASE(electrum__decode_mnemonic__no_passphrase_2)
 
 BOOST_AUTO_TEST_CASE(electrum__decode_mnemonic__passphrase)
 {
-    static const word_list mnemonic{ { "foobar" } };
+    static const word_list mnemonic{ "foobar" };
     const auto passphrase = "none";
     const auto seed = electrum::decode_mnemonic(mnemonic, passphrase);
     BOOST_REQUIRE_EQUAL(encode_base16(seed), "741b72fd15effece6bfe5a26a52184f66811bd2be363190e07a42cca442b1a5bb22b3ad0eb338197287e6d314866c7fba863ac65d3f156087a5052ebc7157fce");
