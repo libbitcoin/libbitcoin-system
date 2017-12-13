89037c116ac88fbaff65355de5050cdb6116a042
evoskuil <eric@voskuil.org>
2017-12-12 16:53:01 -0800

    Use decimal recursive_money constant and add consensus comment.

--- include/bitcoin/bitcoin/constants.hpp
+++ include/bitcoin/bitcoin/constants.hpp
@@ -255,7 +255,7 @@ BC_CONSTFUNC uint64_t initial_block_subsidy_satoshi()
     return bitcoin_to_satoshi(initial_block_subsidy_bitcoin);
 }
 
-BC_CONSTEXPR uint64_t recursive_money = 0x02540be3f5;
+BC_CONSTEXPR uint64_t recursive_money = 9999999989u;
 BC_CONSTEXPR uint64_t retarget_subsidy_interval = 210000;
 BC_CONSTEXPR uint64_t no_retarget_subsidy_interval = 150;
 BC_CONSTFUNC uint64_t subsidy_interval(bool retarget=true)
@@ -263,6 +263,13 @@ BC_CONSTFUNC uint64_t subsidy_interval(bool retarget=true)
     return retarget ? retarget_subsidy_interval : no_retarget_subsidy_interval;
 }
 
+//*****************************************************************************
+// CONSENSUS: This is the true maximum amount of money that can be created.
+// The satoshi client uses a "sanity check" value that is effectively based on
+// a round but incorrect value of recursive_money, which is higher than this
+// true value. Despite comments to the contrary in the satoshi code, no value
+// could be consensus critical unless it was *less* than the true value.
+//*****************************************************************************
 BC_CONSTFUNC uint64_t max_money(bool retarget=true)
 {
     ////// Optimize out the derivation of recursive_money.
