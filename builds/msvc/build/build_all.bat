984f3baa409fc302f49fa61e806d63ece649945e
evoskuil <eric@voskuil.org>
2018-12-18 19:19:23 -0800

    Adapt (non-generated) VS batch build to repo name change.

--- builds/msvc/build/build_all.bat
+++ builds/msvc/build/build_all.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 CALL nuget_all.bat
 ECHO.
-CALL build_base.bat vs2017 libbitcoin "Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build"
-CALL build_base.bat vs2015 libbitcoin "Microsoft Visual Studio 14.0\VC"
-CALL build_base.bat vs2013 libbitcoin "Microsoft Visual Studio 12.0\VC"
+CALL build_base.bat vs2017 libbitcoin-system "Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build"
+CALL build_base.bat vs2015 libbitcoin-system "Microsoft Visual Studio 14.0\VC"
+CALL build_base.bat vs2013 libbitcoin-system "Microsoft Visual Studio 12.0\VC"
 PAUSE
