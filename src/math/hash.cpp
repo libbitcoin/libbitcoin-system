7ad92d1845849241aee92da1507c17cde21ccd85
evoskuil <eric@voskuil.org>
2021-03-25 21:02:47 -0700

    Change data_slice parameters to const&.

--- src/math/hash.cpp
+++ src/math/hash.cpp
@@ -35,59 +35,59 @@
 
 namespace libbitcoin {
 
-hash_digest bitcoin_hash(data_slice data)
+hash_digest bitcoin_hash(const data_slice& data)
 {
     return sha256_hash(sha256_hash(data));
 }
 
-short_hash bitcoin_short_hash(data_slice data)
+short_hash bitcoin_short_hash(const data_slice& data)
 {
     return ripemd160_hash(sha256_hash(data));
 }
 
-short_hash ripemd160_hash(data_slice data)
+short_hash ripemd160_hash(const data_slice& data)
 {
     short_hash hash;
     RMD160(data.data(), data.size(), hash.data());
     return hash;
 }
 
-data_chunk ripemd160_hash_chunk(data_slice data)
+data_chunk ripemd160_hash_chunk(const data_slice& data)
 {
     data_chunk hash(short_hash_size);
     RMD160(data.data(), data.size(), hash.data());
     return hash;
 }
 
-short_hash sha1_hash(data_slice data)
+short_hash sha1_hash(const data_slice& data)
 {
     short_hash hash;
     SHA1_(data.data(), data.size(), hash.data());
     return hash;
 }
 
-data_chunk sha1_hash_chunk(data_slice data)
+data_chunk sha1_hash_chunk(const data_slice& data)
 {
     data_chunk hash(short_hash_size);
     SHA1_(data.data(), data.size(), hash.data());
     return hash;
 }
 
-hash_digest sha256_hash(data_slice data)
+hash_digest sha256_hash(const data_slice& data)
 {
     hash_digest hash;
     SHA256_(data.data(), data.size(), hash.data());
     return hash;
 }
 
-data_chunk sha256_hash_chunk(data_slice data)
+data_chunk sha256_hash_chunk(const data_slice& data)
 {
     data_chunk hash(hash_size);
     SHA256_(data.data(), data.size(), hash.data());
     return hash;
 }
 
-hash_digest sha256_hash(data_slice first, data_slice second)
+hash_digest sha256_hash(const data_slice& first, const data_slice& second)
 {
     hash_digest hash;
     SHA256CTX context;
@@ -98,29 +98,29 @@ hash_digest sha256_hash(data_slice first, data_slice second)
     return hash;
 }
 
-hash_digest hmac_sha256_hash(data_slice data, data_slice key)
+hash_digest hmac_sha256_hash(const data_slice& data, const data_slice& key)
 {
     hash_digest hash;
     HMACSHA256(data.data(), data.size(), key.data(), key.size(), hash.data());
     return hash;
 }
 
-long_hash sha512_hash(data_slice data)
+long_hash sha512_hash(const data_slice& data)
 {
     long_hash hash;
     SHA512_(data.data(), data.size(), hash.data());
     return hash;
 }
 
-long_hash hmac_sha512_hash(data_slice data, data_slice key)
+long_hash hmac_sha512_hash(const data_slice& data, const data_slice& key)
 {
     long_hash hash;
     HMACSHA512(data.data(), data.size(), key.data(), key.size(), hash.data());
     return hash;
 }
 
-long_hash pkcs5_pbkdf2_hmac_sha512(data_slice passphrase,
-    data_slice salt, size_t iterations)
+long_hash pkcs5_pbkdf2_hmac_sha512(const data_slice& passphrase,
+    const data_slice& salt, size_t iterations)
 {
     long_hash hash;
     const auto result = pkcs5_pbkdf2(passphrase.data(), passphrase.size(),
@@ -150,8 +150,8 @@ static void handle_script_result(int result)
     }
 }
 
-data_chunk scrypt(data_slice data, data_slice salt, uint64_t N, uint32_t p,
-    uint32_t r, size_t length)
+data_chunk scrypt(const data_slice& data, const data_slice& salt, uint64_t N,
+    uint32_t p, uint32_t r, size_t length)
 {
     data_chunk output(length);
     const auto result = crypto_scrypt(data.data(), data.size(), salt.data(),
