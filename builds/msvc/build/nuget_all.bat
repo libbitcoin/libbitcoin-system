d49831f50e7adb258a2c0da6997768e6637d00d8
Eric Voskuil <eric@voskuil.org>
2021-04-04 11:48:38 -0700

    Update nuget_all.bat

--- builds/msvc/build/nuget_all.bat
+++ builds/msvc/build/nuget_all.bat
@@ -1,15 +1,15 @@
 @ECHO OFF
 ECHO Downloading libbitcoin vs2017 dependencies from NuGet
-CALL nuget.exe install ..\vs2017\libbitcoin\packages.config
-CALL nuget.exe install ..\vs2017\libbitcoin-examples\packages.config
-CALL nuget.exe install ..\vs2017\libbitcoin-test\packages.config
+CALL nuget.exe install ..\vs2017\libbitcoin-system\packages.config
+CALL nuget.exe install ..\vs2017\libbitcoin-system-examples\packages.config
+CALL nuget.exe install ..\vs2017\libbitcoin-system-test\packages.config
 ECHO.
 ECHO Downloading libbitcoin vs2015 dependencies from NuGet
-CALL nuget.exe install ..\vs2015\libbitcoin\packages.config
-CALL nuget.exe install ..\vs2015\libbitcoin-examples\packages.config
-CALL nuget.exe install ..\vs2015\libbitcoin-test\packages.config
+CALL nuget.exe install ..\vs2015\libbitcoin-system\packages.config
+CALL nuget.exe install ..\vs2015\libbitcoin-system-examples\packages.config
+CALL nuget.exe install ..\vs2015\libbitcoin-system-test\packages.config
 ECHO.
 ECHO Downloading libbitcoin vs2013 dependencies from NuGet
-CALL nuget.exe install ..\vs2013\libbitcoin\packages.config
-CALL nuget.exe install ..\vs2013\libbitcoin-examples\packages.config
-CALL nuget.exe install ..\vs2013\libbitcoin-test\packages.config
+CALL nuget.exe install ..\vs2013\libbitcoin-system\packages.config
+CALL nuget.exe install ..\vs2013\libbitcoin-system-examples\packages.config
+CALL nuget.exe install ..\vs2013\libbitcoin-system-test\packages.config
