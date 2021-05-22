c58045c358f5d3769807f86bd9fdcac209abbc27
evoskuil <eric@voskuil.org>
2021-05-22 03:09:44 -0700

    Style: use auto.

--- src/wallet/keys/encrypted_keys.cpp
+++ src/wallet/keys/encrypted_keys.cpp
@@ -284,7 +284,7 @@ bool create_key_pair(encrypted_private& out_private, ec_compressed& out_point,
 // This call requires an ICU build, the other excluded calls are dependencies.
 static data_chunk normal(const std::string& passphrase)
 {
-    std::string copy = passphrase;
+    auto copy = passphrase;
     return to_canonical_composition(copy) ? to_chunk(copy) : data_chunk{};
 }
 
