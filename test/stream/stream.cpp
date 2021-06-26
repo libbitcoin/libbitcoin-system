d50424f6b21a0b8b90a4cdc0273a5be97a82f5d5
evoskuil <eric@voskuil.org>
2021-06-25 23:14:24 -0700

    Remove dead code and add stream test tub, regenerate artifacts.

---
+++ test/stream/stream.cpp
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2011-2021 libbitcoin developers (see AUTHORS)
+ *
+ * This file is part of libbitcoin.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "../test.hpp"
+
+BOOST_AUTO_TEST_SUITE(stream_tests)
+
+BOOST_AUTO_TEST_CASE(stream_in_move__todo)
+{
+}
+
+BOOST_AUTO_TEST_CASE(stream_in_from__string__todo)
+{
+}
+
+BOOST_AUTO_TEST_CASE(stream_in_from__array__todo)
+{
+}
+
+BOOST_AUTO_TEST_CASE(stream_in_copy__todo)
+{
+}
+
+BOOST_AUTO_TEST_CASE(stream_out_slab__todo)
+{
+}
+
+BOOST_AUTO_TEST_CASE(stream_out_text__todo)
+{
+}
+
+BOOST_AUTO_TEST_CASE(stream_out_push__todo)
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END()
