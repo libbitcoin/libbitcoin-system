e2566fc939649d240cc44c444c139fc3c84fbbb1
evoskuil <eric@voskuil.org>
2022-06-15 19:34:40 -0700

    Use const and constexpr (tests).

--- test/data/data_array.cpp
+++ test/data/data_array.cpp
@@ -24,7 +24,7 @@ BOOST_AUTO_TEST_SUITE(data_array_tests)
 
 BOOST_AUTO_TEST_CASE(data_array__to_array1__value__expected_size_and_value)
 {
-    const uint8_t expected = 42;
+    constexpr uint8_t expected = 42;
     const auto result = to_array(expected);
     BOOST_REQUIRE_EQUAL(result.size(), 1u);
     BOOST_REQUIRE_EQUAL(result[0], expected);
@@ -34,14 +34,14 @@ BOOST_AUTO_TEST_CASE(data_array__to_array1__value__expected_size_and_value)
 
 BOOST_AUTO_TEST_CASE(data_array__to_array2__to_string__inverse)
 {
-    data_array<3> result{ { 24, 0, 15 } };
+    const data_array<3> result{ { 24, 0, 15 } };
     BOOST_REQUIRE_EQUAL(to_array<3>(to_string(result)), result);
 }
 
 BOOST_AUTO_TEST_CASE(data_array__to_array2__double_long_hash__expected)
 {
-    const char l = 42;
-    const uint8_t u = 24;
+    constexpr char l = 42;
+    constexpr uint8_t u = 24;
 
     // Uses data_slice data initializer construction.
     const auto result = to_array<long_hash_size>(
@@ -113,7 +113,7 @@ BOOST_AUTO_TEST_CASE(data_array__build_array__overflow__truncated_values)
 
 BOOST_AUTO_TEST_CASE(data_array__extend1__copy_twice__expected)
 {
-    const uint8_t expected = 24;
+    constexpr uint8_t expected = 24;
     data_chunk buffer1{ 0 };
     extend(buffer1, null_hash);
     data_chunk buffer2{ expected };
@@ -125,7 +125,7 @@ BOOST_AUTO_TEST_CASE(data_array__extend1__copy_twice__expected)
 
 BOOST_AUTO_TEST_CASE(data_array__extend2__move_twice__expected)
 {
-    const uint8_t expected = 24;
+    constexpr uint8_t expected = 24;
     auto hash1 = null_hash;
     auto hash2 = null_hash;
     data_chunk buffer1{ 0 };
@@ -269,14 +269,14 @@ BOOST_AUTO_TEST_CASE(data_array__split__long_hash__expected)
 
 BOOST_AUTO_TEST_CASE(data_array__xor_data1__empty__empty)
 {
-    static const data_array<1> source{ { 0x0 } };
+    const data_array<1> source{ { 0x0 } };
     const auto result = xor_data<0>(source, source);
     BOOST_REQUIRE_EQUAL(result.size(), 0u);
 }
 
 BOOST_AUTO_TEST_CASE(data_array__xor_data1__same__zeros)
 {
-    static const data_array<2> source{ { 7, 11 } };
+    const data_array<2> source{ { 7, 11 } };
     const auto result = xor_data<2>(source, source);
     BOOST_REQUIRE_EQUAL(result.size(), 2u);
     BOOST_REQUIRE_EQUAL(result[0], 0u);
@@ -285,8 +285,8 @@ BOOST_AUTO_TEST_CASE(data_array__xor_data1__same__zeros)
 
 BOOST_AUTO_TEST_CASE(data_array__xor_data1__distinct__ones)
 {
-    static const data_array<2> source1{ { 0x00, 0xff } };
-    static const data_array<2> source2{ { 0xff, 0x00 } };
+    const data_array<2> source1{ { 0x00, 0xff } };
+    const data_array<2> source2{ { 0xff, 0x00 } };
     const auto result = xor_data<2>(source1, source2);
     BOOST_REQUIRE_EQUAL(result.size(), 2u);
     BOOST_REQUIRE_EQUAL(result[0], 0xff);
@@ -297,22 +297,22 @@ BOOST_AUTO_TEST_CASE(data_array__xor_data1__distinct__ones)
 
 BOOST_AUTO_TEST_CASE(data_array__xor_offset__empty__empty)
 {
-    static const data_array<1> source{ { 0x0 } };
+    const data_array<1> source{ { 0x0 } };
     const auto result = xor_offset<0, 0, 0>(source, source);
     BOOST_REQUIRE_EQUAL(result.size(), 0u);
 }
 
 BOOST_AUTO_TEST_CASE(data_array__xor_offset__empty_out__empty)
 {
-    static const data_array<2> source1{ { 0x00, 0xff } };
-    static const data_array<3> source2{ { 0xff, 0x00, 0xff } };
+    const data_array<2> source1{ { 0x00, 0xff } };
+    const data_array<3> source2{ { 0xff, 0x00, 0xff } };
     const auto result = xor_offset<0, 0, 0>(source1, source2);
     BOOST_REQUIRE_EQUAL(result.size(), 0u);
 }
 
 BOOST_AUTO_TEST_CASE(data_array__xor_offset__same__zeros)
 {
-    static const data_array<2> source1{ { 0x00, 0xff } };
+    const data_array<2> source1{ { 0x00, 0xff } };
     const auto result = xor_offset<2, 0, 0>(source1, source1);
     BOOST_REQUIRE_EQUAL(result.size(), 2u);
     BOOST_REQUIRE_EQUAL(result[0], 0);
@@ -321,8 +321,8 @@ BOOST_AUTO_TEST_CASE(data_array__xor_offset__same__zeros)
 
 BOOST_AUTO_TEST_CASE(data_array__xor_offset__distinct_trimmed__ones)
 {
-    static const data_array<3> source1{ { 0x00, 0xff, 0x00 } };
-    static const data_array<4> source2{ { 0xff, 0x00, 0xff, 0x00 } };
+    const data_array<3> source1{ { 0x00, 0xff, 0x00 } };
+    const data_array<4> source2{ { 0xff, 0x00, 0xff, 0x00 } };
     const auto result = xor_offset<3, 0, 0>(source1, source2);
     BOOST_REQUIRE_EQUAL(result.size(), 3u);
     BOOST_REQUIRE_EQUAL(result[0], 0xff);
@@ -332,10 +332,10 @@ BOOST_AUTO_TEST_CASE(data_array__xor_offset__distinct_trimmed__ones)
 
 BOOST_AUTO_TEST_CASE(data_array__xor_offset__distinct_same_offsets_expected)
 {
-    const uint8_t expected0 = 42u ^ 24u;
-    const uint8_t expected1 = 13u ^ 13u;
-    static const data_array<4> source1{ { 7, 42, 13, 0x00 } };
-    static const data_array<5> source2{ { 7, 24, 13, 0xff, 0x00 } };
+    constexpr uint8_t expected0 = bit_xor<uint8_t>(42, 24);
+    constexpr uint8_t expected1 = bit_xor<uint8_t>(13, 13);
+    const data_array<4> source1{ { 7, 42, 13, 0x00 } };
+    const data_array<5> source2{ { 7, 24, 13, 0xff, 0x00 } };
     const auto result = xor_offset<2, 1, 1>(source1, source2);
     BOOST_REQUIRE_EQUAL(result.size(), 2u);
     BOOST_REQUIRE_EQUAL(result[0], expected0);
@@ -344,10 +344,10 @@ BOOST_AUTO_TEST_CASE(data_array__xor_offset__distinct_same_offsets_expected)
 
 BOOST_AUTO_TEST_CASE(data_array__xor_offset__distinct_different_offsets_expected)
 {
-    const uint8_t expected0 = 42u ^ 24u;
-    const uint8_t expected1 = 13u ^ 13u;
-    static const data_array<4> source1{ { 7, 42, 13, 0x00 } };
-    static const data_array<5> source2{ { 0, 7, 24, 13, 0xff } };
+    constexpr uint8_t expected0 = bit_xor<uint8_t>(42, 24);
+    constexpr uint8_t expected1 = bit_xor<uint8_t>(13, 13);
+    const data_array<4> source1{ { 7, 42, 13, 0x00 } };
+    const data_array<5> source2{ { 0, 7, 24, 13, 0xff } };
     const auto result = xor_offset<2, 1, 2>(source1, source2);
     BOOST_REQUIRE_EQUAL(result.size(), 2u);
     BOOST_REQUIRE_EQUAL(result[0], expected0);
