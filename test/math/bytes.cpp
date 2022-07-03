15e7a1cdd77bde278add1ad4d4e0c893b9283f9f
evoskuil <eric@voskuil.org>
2022-07-03 05:46:16 -0700

    Add bytes endian tests.

--- test/math/bytes.cpp
+++ test/math/bytes.cpp
@@ -224,7 +224,7 @@ static_assert(byteswap(0x01020304_u32) == 0x04030201);
 static_assert(byteswap(0x0102030405060708_u64) == 0x0807060504030201);
 
 template <typename Integer>
-constexpr auto native(Integer value)
+constexpr auto normalize(Integer value)
 {
     if constexpr (is_big_endian)
         return byteswap(value);
@@ -232,29 +232,53 @@ constexpr auto native(Integer value)
     return value;
 }
 
-// to_big_end (constexpr)
+// This is the perspective of a little-endian machine.
+// The normalize() helper reverses the expecation on a big-endian machine.
 // Beware of type promotion (use explicit types).
-static_assert(native_to_big_end(native(0x01_i8)) == 0x01);
-static_assert(native_to_big_end(native(0x0102_i16)) == 0x0201);
-static_assert(native_to_big_end(native(0x01020304_i32)) == 0x04030201);
-static_assert(native_to_big_end(native(0x0102030405060708_i64)) == 0x0807060504030201);
-static_assert(native_to_big_end(native(0x01_u8)) == 0x01);
-static_assert(native_to_big_end(native(0x0102_u16)) == 0x0201);
-static_assert(native_to_big_end(native(0x01020304_u32)) == 0x04030201);
-static_assert(native_to_big_end(native(0x0102030405060708_u64)) == 0x0807060504030201);
 
-// to_little_end (constexpr)
-// Beware of type promotion (use explicit types).
-static_assert(native_to_little_end(native(0x01_i8)) == 0x01);
-static_assert(native_to_little_end(native(0x0102_i16)) == 0x0102);
-static_assert(native_to_little_end(native(0x01020304_i32)) == 0x01020304);
-static_assert(native_to_little_end(native(0x0102030405060708_i64)) == 0x0102030405060708);
-static_assert(native_to_little_end(native(0x01_u8)) == 0x01);
-static_assert(native_to_little_end(native(0x0102_u16)) == 0x0102);
-static_assert(native_to_little_end(native(0x01020304_u32)) == 0x01020304);
-static_assert(native_to_little_end(native(0x0102030405060708_u64)) == 0x0102030405060708);
+// native_to_big_end (constexpr, signed/unsigned)
+static_assert(native_to_big_end(normalize(0x01_i8)) == 0x01_i8);
+static_assert(native_to_big_end(normalize(0x0102_i16)) == 0x0201_i16);
+static_assert(native_to_big_end(normalize(0x01020304_i32)) == 0x04030201_i32);
+static_assert(native_to_big_end(normalize(0x0102030405060708_i64)) == 0x0807060504030201_i64);
+static_assert(native_to_big_end(normalize(0x01_u8)) == 0x01_u8);
+static_assert(native_to_big_end(normalize(0x0102_u16)) == 0x0201_u16);
+static_assert(native_to_big_end(normalize(0x01020304_u32)) == 0x04030201_u32);
+static_assert(native_to_big_end(normalize(0x0102030405060708_u64)) == 0x0807060504030201_u64);
+
+// native_to_little_end (constexpr, signed/unsigned)
+static_assert(native_to_little_end(normalize(0x01_i8)) == 0x01_i8);
+static_assert(native_to_little_end(normalize(0x0102_i16)) == 0x0102_i16);
+static_assert(native_to_little_end(normalize(0x01020304_i32)) == 0x01020304_i32);
+static_assert(native_to_little_end(normalize(0x0102030405060708_i64)) == 0x0102030405060708_i64);
+static_assert(native_to_little_end(normalize(0x01_u8)) == 0x01_u8);
+static_assert(native_to_little_end(normalize(0x0102_u16)) == 0x0102_u16);
+static_assert(native_to_little_end(normalize(0x01020304_u32)) == 0x01020304_u32);
+static_assert(native_to_little_end(normalize(0x0102030405060708_u64)) == 0x0102030405060708_u64);
+
+// native_from_big_end (constexpr, signed/unsigned)
+static_assert(native_from_big_end(normalize(0x01_i8)) == 0x01_i8);
+static_assert(native_from_big_end(normalize(0x0102_i16)) == 0x0201_i16);
+static_assert(native_from_big_end(normalize(0x01020304_i32)) == 0x04030201_i32);
+static_assert(native_from_big_end(normalize(0x0102030405060708_i64)) == 0x0807060504030201_i64);
+static_assert(native_from_big_end(normalize(0x01_u8)) == 0x01_u8);
+static_assert(native_from_big_end(normalize(0x0102_u16)) == 0x0201_u16);
+static_assert(native_from_big_end(normalize(0x01020304_u32)) == 0x04030201_u32);
+static_assert(native_from_big_end(normalize(0x0102030405060708_u64)) == 0x0807060504030201_u64);
+
+// native_from_little_end (constexpr, signed/unsigned)
+static_assert(native_from_little_end(normalize(0x01_i8)) == 0x01_i8);
+static_assert(native_from_little_end(normalize(0x0102_i16)) == 0x0102_i16);
+static_assert(native_from_little_end(normalize(0x01020304_i32)) == 0x01020304_i32);
+static_assert(native_from_little_end(normalize(0x0102030405060708_i64)) == 0x0102030405060708_i64);
+static_assert(native_from_little_end(normalize(0x01_u8)) == 0x01_u8);
+static_assert(native_from_little_end(normalize(0x0102_u16)) == 0x0102_u16);
+static_assert(native_from_little_end(normalize(0x01020304_u32)) == 0x01020304_u32);
+static_assert(native_from_little_end(normalize(0x0102030405060708_u64)) == 0x0102030405060708_u64);
 
 BOOST_AUTO_TEST_SUITE(bytes_tests)
+
+// byteswap
 
 BOOST_AUTO_TEST_CASE(bytes__byteswap__not_constant_evaluated_signed__swapped)
 {
@@ -272,37 +296,72 @@ BOOST_AUTO_TEST_CASE(bytes__byteswap__not_constant_evaluated_unsigned__swapped)
     BOOST_REQUIRE_EQUAL(byteswap(0x0102030405060708_u64), 0x0807060504030201_u64);
 }
 
+// to ends
+
 BOOST_AUTO_TEST_CASE(bytes__to_big_end__not_constant_evaluated_signed__expected)
 {
-
-    BOOST_REQUIRE_EQUAL(native_to_big_end(native(0x01_i8)), 0x01_i8);
-    BOOST_REQUIRE_EQUAL(native_to_big_end(native(0x0102_i16)), 0x0201_i16);
-    BOOST_REQUIRE_EQUAL(native_to_big_end(native(0x01020304_i32)), 0x04030201_i32);
-    BOOST_REQUIRE_EQUAL(native_to_big_end(native(0x0102030405060708_i64)), 0x0807060504030201_i64);
+    BOOST_REQUIRE_EQUAL(native_to_big_end(normalize(0x01_i8)), 0x01_i8);
+    BOOST_REQUIRE_EQUAL(native_to_big_end(normalize(0x0102_i16)), 0x0201_i16);
+    BOOST_REQUIRE_EQUAL(native_to_big_end(normalize(0x01020304_i32)), 0x04030201_i32);
+    BOOST_REQUIRE_EQUAL(native_to_big_end(normalize(0x0102030405060708_i64)), 0x0807060504030201_i64);
 }
 
 BOOST_AUTO_TEST_CASE(bytes__to_big_end__not_constant_evaluated_unsigned__expected)
 {
-    BOOST_REQUIRE_EQUAL(native_to_big_end(native(0x01_u8)), 0x01_u8);
-    BOOST_REQUIRE_EQUAL(native_to_big_end(native(0x0102_u16)), 0x0201_u16);
-    BOOST_REQUIRE_EQUAL(native_to_big_end(native(0x01020304_u32)), 0x04030201_u32);
-    BOOST_REQUIRE_EQUAL(native_to_big_end(native(0x0102030405060708_u64)), 0x0807060504030201_u64);
+    BOOST_REQUIRE_EQUAL(native_to_big_end(normalize(0x01_u8)), 0x01_u8);
+    BOOST_REQUIRE_EQUAL(native_to_big_end(normalize(0x0102_u16)), 0x0201_u16);
+    BOOST_REQUIRE_EQUAL(native_to_big_end(normalize(0x01020304_u32)), 0x04030201_u32);
+    BOOST_REQUIRE_EQUAL(native_to_big_end(normalize(0x0102030405060708_u64)), 0x0807060504030201_u64);
 }
 
-BOOST_AUTO_TEST_CASE(bytes__bytes__to_little_end__not_constant_evaluated_signed__expected)
+BOOST_AUTO_TEST_CASE(bytes__to_little_end__not_constant_evaluated_signed__expected)
 {
-    BOOST_REQUIRE_EQUAL(native_to_little_end(native(0x01_i8)), 0x01_i8);
-    BOOST_REQUIRE_EQUAL(native_to_little_end(native(0x0102_i16)), 0x0102_i16);
-    BOOST_REQUIRE_EQUAL(native_to_little_end(native(0x01020304_i32)), 0x01020304_i32);
-    BOOST_REQUIRE_EQUAL(native_to_little_end(native(0x0102030405060708_i64)), 0x0102030405060708_i64);
+    BOOST_REQUIRE_EQUAL(native_to_little_end(normalize(0x01_i8)), 0x01_i8);
+    BOOST_REQUIRE_EQUAL(native_to_little_end(normalize(0x0102_i16)), 0x0102_i16);
+    BOOST_REQUIRE_EQUAL(native_to_little_end(normalize(0x01020304_i32)), 0x01020304_i32);
+    BOOST_REQUIRE_EQUAL(native_to_little_end(normalize(0x0102030405060708_i64)), 0x0102030405060708_i64);
 }
 
 BOOST_AUTO_TEST_CASE(bytes__to_little_end__not_constant_evaluated_unsigned__expected)
 {
-    BOOST_REQUIRE_EQUAL(native_to_little_end(native(0x01_u8)), 0x01_u8);
-    BOOST_REQUIRE_EQUAL(native_to_little_end(native(0x0102_u16)), 0x0102_u16);
-    BOOST_REQUIRE_EQUAL(native_to_little_end(native(0x01020304_u32)), 0x01020304_u32);
-    BOOST_REQUIRE_EQUAL(native_to_little_end(native(0x0102030405060708_u64)), 0x0102030405060708_u64);
+    BOOST_REQUIRE_EQUAL(native_to_little_end(normalize(0x01_u8)), 0x01_u8);
+    BOOST_REQUIRE_EQUAL(native_to_little_end(normalize(0x0102_u16)), 0x0102_u16);
+    BOOST_REQUIRE_EQUAL(native_to_little_end(normalize(0x01020304_u32)), 0x01020304_u32);
+    BOOST_REQUIRE_EQUAL(native_to_little_end(normalize(0x0102030405060708_u64)), 0x0102030405060708_u64);
+}
+
+// from ends
+
+BOOST_AUTO_TEST_CASE(bytes__from_big_end__not_constant_evaluated_signed__expected)
+{
+    BOOST_REQUIRE_EQUAL(native_from_big_end(normalize(0x01_i8)), 0x01_i8);
+    BOOST_REQUIRE_EQUAL(native_from_big_end(normalize(0x0102_i16)), 0x0201_i16);
+    BOOST_REQUIRE_EQUAL(native_from_big_end(normalize(0x01020304_i32)), 0x04030201_i32);
+    BOOST_REQUIRE_EQUAL(native_from_big_end(normalize(0x0102030405060708_i64)), 0x0807060504030201_i64);
+}
+
+BOOST_AUTO_TEST_CASE(bytes__from_big_end__not_constant_evaluated_unsigned__expected)
+{
+    BOOST_REQUIRE_EQUAL(native_from_big_end(normalize(0x01_u8)), 0x01_u8);
+    BOOST_REQUIRE_EQUAL(native_from_big_end(normalize(0x0102_u16)), 0x0201_u16);
+    BOOST_REQUIRE_EQUAL(native_from_big_end(normalize(0x01020304_u32)), 0x04030201_u32);
+    BOOST_REQUIRE_EQUAL(native_from_big_end(normalize(0x0102030405060708_u64)), 0x0807060504030201_u64);
+}
+
+BOOST_AUTO_TEST_CASE(bytes__from_little_end__not_constant_evaluated_signed__expected)
+{
+    BOOST_REQUIRE_EQUAL(native_from_little_end(normalize(0x01_i8)), 0x01_i8);
+    BOOST_REQUIRE_EQUAL(native_from_little_end(normalize(0x0102_i16)), 0x0102_i16);
+    BOOST_REQUIRE_EQUAL(native_from_little_end(normalize(0x01020304_i32)), 0x01020304_i32);
+    BOOST_REQUIRE_EQUAL(native_from_little_end(normalize(0x0102030405060708_i64)), 0x0102030405060708_i64);
+}
+
+BOOST_AUTO_TEST_CASE(bytes__from_little_end__not_constant_evaluated_unsigned__expected)
+{
+    BOOST_REQUIRE_EQUAL(native_from_little_end(normalize(0x01_u8)), 0x01_u8);
+    BOOST_REQUIRE_EQUAL(native_from_little_end(normalize(0x0102_u16)), 0x0102_u16);
+    BOOST_REQUIRE_EQUAL(native_from_little_end(normalize(0x01020304_u32)), 0x01020304_u32);
+    BOOST_REQUIRE_EQUAL(native_from_little_end(normalize(0x0102030405060708_u64)), 0x0102030405060708_u64);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
