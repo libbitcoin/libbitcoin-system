7857ec90c3e1cd8c86f36a02ecc32619296c39f5
evoskuil <eric@voskuil.org>
2022-07-13 03:13:39 -0700

    Eliminate macros from sha256 native.

--- src/crypto/sha256/sha256_1_native.cpp
+++ src/crypto/sha256/sha256_1_native.cpp
@@ -27,6 +27,7 @@
 #include <bitcoin/system/crypto/sha256.hpp>
 
 #include <array>
+#include <bit>
 #include <bitcoin/system/define.hpp>
 #include <bitcoin/system/endian/endian.hpp>
 #include <bitcoin/system/math/math.hpp>
@@ -35,197 +36,215 @@ namespace libbitcoin {
 namespace system {
 namespace sha256 {
 
-#ifndef VISUAL
 // movable-type.co.uk/scripts/sha256.html
 
-// TODO: use std::rotr (intrinsic/inline).
-#define rotr(x, n)        ((x >> n) | (x << (32 - n)))
+constexpr auto choice(auto x, auto y, auto z) noexcept
+{
+    return (x & (y ^ z)) ^ z;
+}
 
-// TODO: eliminate macros and (constexpr).
-#define shr(x, n)         (x >> n)
-#define choice(x, y, z)   ((x & (y ^ z)) ^ z)
-#define majority(x, y, z) ((x & (y | z)) | (y & z))
+constexpr auto majority(auto x, auto y, auto z) noexcept
+{
+    return (x & (y | z)) | (y & z);
+}
 
-// TODO: eliminate macros (inline).
-#define SIGMA0(x)    (rotr(x,  2) ^ rotr(x, 13) ^ rotr(x, 22))
-#define SIGMA1(x)    (rotr(x,  6) ^ rotr(x, 11) ^ rotr(x, 25))
-#define sigma0(x)    (rotr(x,  7) ^ rotr(x, 18) ^  shr(x,  3))
-#define sigma1(x)    (rotr(x, 17) ^ rotr(x, 19) ^  shr(x, 10))
+// use inline vs. constexpr for intrinsic std::rotr.
 
-// TODO: eliminate macros (inline). d&, h&
-#define round(a, b, c, d, e, f, g, h, k) \
-    t0 = h + SIGMA1(e) + choice(e, f, g) + k;  \
-    t1 =     SIGMA0(a) + majority(a, b, c); \
-    d += t0; \
-    h = t0 + t1;
-    
-// TODO: eliminate macros and normalize case (inline). S&, <i>
-#define RNDr(S, W, i, k) \
-    round( \
-        S[(64 - i) % 8], \
-        S[(65 - i) % 8], \
-        S[(66 - i) % 8], \
-        S[(67 - i) % 8], \
-        S[(68 - i) % 8], \
-        S[(69 - i) % 8], \
-        S[(70 - i) % 8], \
-        S[(71 - i) % 8], \
-        W[i] + k)
+inline auto SIGMA0(auto x) noexcept
+{
+    return std::rotr(x, 2) ^ std::rotr(x, 13) ^ std::rotr(x, 22);
+}
 
-// TODO: eliminate macros and normalize case (inline). W&, <i>
-#define Wi(W, i) \
-    W[i] = sigma1(W[i - 2]) + W[i - 7] + sigma0(W[i - 15]) + W[i - 16]
-#endif
+inline auto SIGMA1(auto x) noexcept
+{
+    return std::rotr(x, 6) ^ std::rotr(x, 11) ^ std::rotr(x, 25);
+}
+
+inline auto sigma0(auto x) noexcept
+{
+    return std::rotr(x, 7) ^ std::rotr(x, 18) ^ (x >> 3);
+}
+
+inline auto sigma1(auto x) noexcept
+{
+    return std::rotr(x, 17) ^ std::rotr(x, 19) ^ (x >> 10);
+}
+
+inline void round(auto a, auto b, auto c, auto& out_d, auto e, auto f, auto g,
+    auto& out_h, auto k) noexcept
+{
+    const auto t0 = SIGMA1(e) + choice(e, f, g) + out_h + k;
+    const auto t1 = SIGMA0(a) + majority(a, b, c);
+    out_d += t0;
+    out_h = t0 + t1;
+}
+
+template<uint8_t Offset, uint32_t K>
+constexpr void round(auto& state, const auto& buffer) noexcept
+{
+    static_assert(Offset < 64u);
+    BC_PUSH_WARNING(NO_ARRAY_INDEXING)
+    round(
+        state[(64 - Offset) % 8],
+        state[(65 - Offset) % 8],
+        state[(66 - Offset) % 8],
+        state[(67 - Offset) % 8], // in/out
+        state[(68 - Offset) % 8],
+        state[(69 - Offset) % 8],
+        state[(70 - Offset) % 8],
+        state[(71 - Offset) % 8], // in/out
+        buffer[Offset] + K);
+    BC_POP_WARNING()
+}
+
+template<uint8_t Offset>
+inline void set(auto& buffer) noexcept
+{
+    static_assert(Offset < 64u);
+    BC_PUSH_WARNING(NO_ARRAY_INDEXING)
+    buffer[Offset] =
+        sigma1(buffer[Offset -  2]) + buffer[Offset -  7] +
+        sigma0(buffer[Offset - 15]) + buffer[Offset - 16];
+    BC_POP_WARNING()
+}
     
 ////void single_hash(state& state, const blocks& blocks) NOEXCEPT;
 static void single_hash(state& state, const block& block) NOEXCEPT
 {
-    BC_PUSH_WARNING(NO_ARRAY_INDEXING)
-    BC_PUSH_WARNING(NO_UNGUARDED_POINTERS)
-    BC_PUSH_WARNING(NO_POINTER_ARITHMETIC)
-    BC_PUSH_WARNING(NO_DYNAMIC_ARRAY_INDEXING)
-
     constexpr auto integers = block_size / sizeof(uint32_t);
+    std::array<uint32_t, block_size> buffer;
+    const sha256::state copy{ state };
 
-    uint32_t t0, t1;
-    sha256::state S{ state };
-    std::array<uint32_t, block_size> W;
-
-    // Populates W[0..15] with block integers (16 X 4 = 64).
+    // Set buffer[0..15] with all block big-endian integers (16 X 4 = 64 bytes).
     to_big_endian_set(
-        narrowing_array_cast<uint32_t, integers>(W),
+        narrowing_array_cast<uint32_t, integers>(buffer),
         array_cast<uint32_t>(block));
 
-#ifndef VISUAL
-    // Computes W[16..63].
-    Wi(W, 16);
-    Wi(W, 17);
-    Wi(W, 18);
-    Wi(W, 19);
-    Wi(W, 20);
-    Wi(W, 21);
-    Wi(W, 22);
-    Wi(W, 23);
-    Wi(W, 24);
-    Wi(W, 25);
-    Wi(W, 26);
-    Wi(W, 27);
-    Wi(W, 28);
-    Wi(W, 29);
-    Wi(W, 30);
-    Wi(W, 31);
-    Wi(W, 32);
-    Wi(W, 33);
-    Wi(W, 34);
-    Wi(W, 35);
-    Wi(W, 36);
-    Wi(W, 37);
-    Wi(W, 38);
-    Wi(W, 39);
-    Wi(W, 40);
-    Wi(W, 41);
-    Wi(W, 42);
-    Wi(W, 43);
-    Wi(W, 44);
-    Wi(W, 45);
-    Wi(W, 46);
-    Wi(W, 47);
-    Wi(W, 48);
-    Wi(W, 49);
-    Wi(W, 50);
-    Wi(W, 51);
-    Wi(W, 52);
-    Wi(W, 53);
-    Wi(W, 54);
-    Wi(W, 55);
-    Wi(W, 56);
-    Wi(W, 57);
-    Wi(W, 58);
-    Wi(W, 59);
-    Wi(W, 60);
-    Wi(W, 61);
-    Wi(W, 62);
-    Wi(W, 63);
+    // Set buffer[16..63].
+    set<16>(buffer);
+    set<17>(buffer);
+    set<18>(buffer);
+    set<19>(buffer);
+    set<20>(buffer);
+    set<21>(buffer);
+    set<22>(buffer);
+    set<23>(buffer);
+    set<24>(buffer);
+    set<25>(buffer);
+    set<26>(buffer);
+    set<27>(buffer);
+    set<28>(buffer);
+    set<29>(buffer);
+    set<30>(buffer);
+    set<31>(buffer);
+    set<32>(buffer);
+    set<33>(buffer);
+    set<34>(buffer);
+    set<35>(buffer);
+    set<36>(buffer);
+    set<37>(buffer);
+    set<38>(buffer);
+    set<39>(buffer);
+    set<40>(buffer);
+    set<41>(buffer);
+    set<42>(buffer);
+    set<43>(buffer);
+    set<44>(buffer);
+    set<45>(buffer);
+    set<46>(buffer);
+    set<47>(buffer);
+    set<48>(buffer);
+    set<49>(buffer);
+    set<50>(buffer);
+    set<51>(buffer);
+    set<52>(buffer);
+    set<53>(buffer);
+    set<54>(buffer);
+    set<55>(buffer);
+    set<56>(buffer);
+    set<57>(buffer);
+    set<58>(buffer);
+    set<59>(buffer);
+    set<60>(buffer);
+    set<61>(buffer);
+    set<62>(buffer);
+    set<63>(buffer);
 
     // 64 rounds.
-    RNDr(S, W, 0, 0x428a2f98);
-    RNDr(S, W, 1, 0x71374491);
-    RNDr(S, W, 2, 0xb5c0fbcf);
-    RNDr(S, W, 3, 0xe9b5dba5);
-    RNDr(S, W, 4, 0x3956c25b);
-    RNDr(S, W, 5, 0x59f111f1);
-    RNDr(S, W, 6, 0x923f82a4);
-    RNDr(S, W, 7, 0xab1c5ed5);
-    RNDr(S, W, 8, 0xd807aa98);
-    RNDr(S, W, 9, 0x12835b01);
-    RNDr(S, W, 10, 0x243185be);
-    RNDr(S, W, 11, 0x550c7dc3);
-    RNDr(S, W, 12, 0x72be5d74);
-    RNDr(S, W, 13, 0x80deb1fe);
-    RNDr(S, W, 14, 0x9bdc06a7);
-    RNDr(S, W, 15, 0xc19bf174);
-    RNDr(S, W, 16, 0xe49b69c1);
-    RNDr(S, W, 17, 0xefbe4786);
-    RNDr(S, W, 18, 0x0fc19dc6);
-    RNDr(S, W, 19, 0x240ca1cc);
-    RNDr(S, W, 20, 0x2de92c6f);
-    RNDr(S, W, 21, 0x4a7484aa);
-    RNDr(S, W, 22, 0x5cb0a9dc);
-    RNDr(S, W, 23, 0x76f988da);
-    RNDr(S, W, 24, 0x983e5152);
-    RNDr(S, W, 25, 0xa831c66d);
-    RNDr(S, W, 26, 0xb00327c8);
-    RNDr(S, W, 27, 0xbf597fc7);
-    RNDr(S, W, 28, 0xc6e00bf3);
-    RNDr(S, W, 29, 0xd5a79147);
-    RNDr(S, W, 30, 0x06ca6351);
-    RNDr(S, W, 31, 0x14292967);
-    RNDr(S, W, 32, 0x27b70a85);
-    RNDr(S, W, 33, 0x2e1b2138);
-    RNDr(S, W, 34, 0x4d2c6dfc);
-    RNDr(S, W, 35, 0x53380d13);
-    RNDr(S, W, 36, 0x650a7354);
-    RNDr(S, W, 37, 0x766a0abb);
-    RNDr(S, W, 38, 0x81c2c92e);
-    RNDr(S, W, 39, 0x92722c85);
-    RNDr(S, W, 40, 0xa2bfe8a1);
-    RNDr(S, W, 41, 0xa81a664b);
-    RNDr(S, W, 42, 0xc24b8b70);
-    RNDr(S, W, 43, 0xc76c51a3);
-    RNDr(S, W, 44, 0xd192e819);
-    RNDr(S, W, 45, 0xd6990624);
-    RNDr(S, W, 46, 0xf40e3585);
-    RNDr(S, W, 47, 0x106aa070);
-    RNDr(S, W, 48, 0x19a4c116);
-    RNDr(S, W, 49, 0x1e376c08);
-    RNDr(S, W, 50, 0x2748774c);
-    RNDr(S, W, 51, 0x34b0bcb5);
-    RNDr(S, W, 52, 0x391c0cb3);
-    RNDr(S, W, 53, 0x4ed8aa4a);
-    RNDr(S, W, 54, 0x5b9cca4f);
-    RNDr(S, W, 55, 0x682e6ff3);
-    RNDr(S, W, 56, 0x748f82ee);
-    RNDr(S, W, 57, 0x78a5636f);
-    RNDr(S, W, 58, 0x84c87814);
-    RNDr(S, W, 59, 0x8cc70208);
-    RNDr(S, W, 60, 0x90befffa);
-    RNDr(S, W, 61, 0xa4506ceb);
-    RNDr(S, W, 62, 0xbef9a3f7);
-    RNDr(S, W, 63, 0xc67178f2);
-#endif
-
-    state[0] += S[0];
-    state[1] += S[1];
-    state[2] += S[2];
-    state[3] += S[3];
-    state[4] += S[4];
-    state[5] += S[5];
-    state[6] += S[6];
-    state[7] += S[7];
-
-    BC_POP_WARNING()
-    BC_POP_WARNING()
-    BC_POP_WARNING()
+    round< 0, 0x428a2f98>(state, buffer);
+    round< 1, 0x71374491>(state, buffer);
+    round< 2, 0xb5c0fbcf>(state, buffer);
+    round< 3, 0xe9b5dba5>(state, buffer);
+    round< 4, 0x3956c25b>(state, buffer);
+    round< 5, 0x59f111f1>(state, buffer);
+    round< 6, 0x923f82a4>(state, buffer);
+    round< 7, 0xab1c5ed5>(state, buffer);
+    round< 8, 0xd807aa98>(state, buffer);
+    round< 9, 0x12835b01>(state, buffer);
+    round<10, 0x243185be>(state, buffer);
+    round<11, 0x550c7dc3>(state, buffer);
+    round<12, 0x72be5d74>(state, buffer);
+    round<13, 0x80deb1fe>(state, buffer);
+    round<14, 0x9bdc06a7>(state, buffer);
+    round<15, 0xc19bf174>(state, buffer);
+    round<16, 0xe49b69c1>(state, buffer);
+    round<17, 0xefbe4786>(state, buffer);
+    round<18, 0x0fc19dc6>(state, buffer);
+    round<19, 0x240ca1cc>(state, buffer);
+    round<20, 0x2de92c6f>(state, buffer);
+    round<21, 0x4a7484aa>(state, buffer);
+    round<22, 0x5cb0a9dc>(state, buffer);
+    round<23, 0x76f988da>(state, buffer);
+    round<24, 0x983e5152>(state, buffer);
+    round<25, 0xa831c66d>(state, buffer);
+    round<26, 0xb00327c8>(state, buffer);
+    round<27, 0xbf597fc7>(state, buffer);
+    round<28, 0xc6e00bf3>(state, buffer);
+    round<29, 0xd5a79147>(state, buffer);
+    round<30, 0x06ca6351>(state, buffer);
+    round<31, 0x14292967>(state, buffer);
+    round<32, 0x27b70a85>(state, buffer);
+    round<33, 0x2e1b2138>(state, buffer);
+    round<34, 0x4d2c6dfc>(state, buffer);
+    round<35, 0x53380d13>(state, buffer);
+    round<36, 0x650a7354>(state, buffer);
+    round<37, 0x766a0abb>(state, buffer);
+    round<38, 0x81c2c92e>(state, buffer);
+    round<39, 0x92722c85>(state, buffer);
+    round<40, 0xa2bfe8a1>(state, buffer);
+    round<41, 0xa81a664b>(state, buffer);
+    round<42, 0xc24b8b70>(state, buffer);
+    round<43, 0xc76c51a3>(state, buffer);
+    round<44, 0xd192e819>(state, buffer);
+    round<45, 0xd6990624>(state, buffer);
+    round<46, 0xf40e3585>(state, buffer);
+    round<47, 0x106aa070>(state, buffer);
+    round<48, 0x19a4c116>(state, buffer);
+    round<49, 0x1e376c08>(state, buffer);
+    round<50, 0x2748774c>(state, buffer);
+    round<51, 0x34b0bcb5>(state, buffer);
+    round<52, 0x391c0cb3>(state, buffer);
+    round<53, 0x4ed8aa4a>(state, buffer);
+    round<54, 0x5b9cca4f>(state, buffer);
+    round<55, 0x682e6ff3>(state, buffer);
+    round<56, 0x748f82ee>(state, buffer);
+    round<57, 0x78a5636f>(state, buffer);
+    round<58, 0x84c87814>(state, buffer);
+    round<59, 0x8cc70208>(state, buffer);
+    round<60, 0x90befffa>(state, buffer);
+    round<61, 0xa4506ceb>(state, buffer);
+    round<62, 0xbef9a3f7>(state, buffer);
+    round<63, 0xc67178f2>(state, buffer);
+    
+    BC_PUSH_WARNING(NO_ARRAY_INDEXING)
+    state[0] += copy[0];
+    state[1] += copy[1];
+    state[2] += copy[2];
+    state[3] += copy[3];
+    state[4] += copy[4];
+    state[5] += copy[5];
+    state[6] += copy[6];
+    state[7] += copy[7];
     BC_POP_WARNING()
 }
 
