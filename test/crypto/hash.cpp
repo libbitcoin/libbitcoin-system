352ca09d54ab7da406e792196bfa6fe5b56dce26
evoskuil <eric@voskuil.org>
2022-07-29 01:09:36 -0700

    Move one more scrypt test under HAVE_SLOW_TESTS.

--- test/crypto/hash.cpp
+++ test/crypto/hash.cpp
@@ -180,6 +180,7 @@ BOOST_AUTO_TEST_CASE(hash__scrypt__rfc7914_hash_1__expected)
     BOOST_REQUIRE_EQUAL(hash, expected);
 }
 
+#if defined(HAVE_SLOW_TESTS)
 BOOST_AUTO_TEST_CASE(hash__scrypt__rfc7914_hash_2__expected)
 {
     constexpr auto expected = base16_array("fdbabe1c9d3472007856e7190d01e9fe7c6ad7cbc8237830e77376634b3731622eaf30d92e22a3886ff109279d9830dac727afb94a83ee6d8360cbdfa2cc0640");
@@ -188,7 +189,6 @@ BOOST_AUTO_TEST_CASE(hash__scrypt__rfc7914_hash_2__expected)
     BOOST_REQUIRE_EQUAL(hash, expected);
 }
 
-#if defined(HAVE_SLOW_TESTS)
 BOOST_AUTO_TEST_CASE(hash__scrypt__rfc7914_hash_3__expected)
 {
     constexpr auto expected = base16_array("7023bdcb3afd7348461c06cd81fd38ebfda8fbba904f8e3ea9b543f6545da1f2d5432955613f0fcf62d49705242a9af9e61e85dc0d651e40dfcf017b45575887");
