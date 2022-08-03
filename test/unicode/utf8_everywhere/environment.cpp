3e6ca9f2e78bcdf4b33bc99b56ad511bc6a7d3d7
evoskuil <eric@voskuil.org>
2022-08-03 15:43:04 -0700

    Disable empty tests (delint).

--- test/unicode/utf8_everywhere/environment.cpp
+++ test/unicode/utf8_everywhere/environment.cpp
@@ -359,15 +359,15 @@ BOOST_AUTO_TEST_CASE(utf8_environment__allocate_environment_args__null_terminati
 
 // to_extended_path
 
-BOOST_AUTO_TEST_CASE(utf8_environment__to_extended_path__always__expected)
-{
-    ////std::string to_extended_path(const path& path);
-}
-
-BOOST_AUTO_TEST_CASE(utf8_environment__to_extended_path16__always__expected)
-{
-    ////std::wstring to_extended_path16(const std::string& path);
-}
+////BOOST_AUTO_TEST_CASE(utf8_environment__to_extended_path__always__expected)
+////{
+////    ////std::string to_extended_path(const path& path);
+////}
+////
+////BOOST_AUTO_TEST_CASE(utf8_environment__to_extended_path16__always__expected)
+////{
+////    ////std::wstring to_extended_path16(const std::string& path);
+////}
 
 #endif // HAVE_MSC
 
