88377c40c6233f705f07e1dccf325d833dc11529
evoskuil <eric@voskuil.org>
2022-08-08 11:36:31 -0700

    Fix baseline test endian namespace conflict.

--- test/hash/performance/baseline/endian.h
+++ test/hash/performance/baseline/endian.h
@@ -20,124 +20,124 @@ namespace baseline {
 
 #if (ENDIAN_ORDER==BIG_END)
 
-inline uint16_t htobe16(uint16_t host_16bits)
+inline uint16_t htobe16_(uint16_t host_16bits)
 {
     return host_16bits;
 }
 
-inline uint16_t htole16(uint16_t host_16bits)
+inline uint16_t htole16_(uint16_t host_16bits)
 {
     return bswap_16(host_16bits);
 }
 
-inline uint16_t be16toh(uint16_t big_endian_16bits)
+inline uint16_t be16toh_(uint16_t big_endian_16bits)
 {
     return big_endian_16bits;
 }
 
-inline uint16_t le16toh(uint16_t little_endian_16bits)
+inline uint16_t le16toh_(uint16_t little_endian_16bits)
 {
     return bswap_16(little_endian_16bits);
 }
 
-inline uint32_t htobe32(uint32_t host_32bits)
+inline uint32_t htobe32_(uint32_t host_32bits)
 {
     return host_32bits;
 }
 
-inline uint32_t htole32(uint32_t host_32bits)
+inline uint32_t htole32_(uint32_t host_32bits)
 {
     return bswap_32(host_32bits);
 }
 
-inline uint32_t be32toh(uint32_t big_endian_32bits)
+inline uint32_t be32toh_(uint32_t big_endian_32bits)
 {
     return big_endian_32bits;
 }
 
-inline uint32_t le32toh(uint32_t little_endian_32bits)
+inline uint32_t le32toh_(uint32_t little_endian_32bits)
 {
     return bswap_32(little_endian_32bits);
 }
 
-inline uint64_t htobe64(uint64_t host_64bits)
+inline uint64_t htobe64_(uint64_t host_64bits)
 {
     return host_64bits;
 }
 
-inline uint64_t htole64(uint64_t host_64bits)
+inline uint64_t htole64_(uint64_t host_64bits)
 {
     return bswap_64(host_64bits);
 }
 
-inline uint64_t be64toh(uint64_t big_endian_64bits)
+inline uint64_t be64toh_(uint64_t big_endian_64bits)
 {
     return big_endian_64bits;
 }
 
-inline uint64_t le64toh(uint64_t little_endian_64bits)
+inline uint64_t le64toh_(uint64_t little_endian_64bits)
 {
     return bswap_64(little_endian_64bits);
 }
 
 #elif (ENDIAN_ORDER==LITTLE_END)
 
-inline uint16_t htobe16(uint16_t host_16bits)
+inline uint16_t htobe16_(uint16_t host_16bits)
 {
     return bswap_16(host_16bits);
 }
 
-inline uint16_t htole16(uint16_t host_16bits)
+inline uint16_t htole16_(uint16_t host_16bits)
 {
     return host_16bits;
 }
 
-inline uint16_t be16toh(uint16_t big_endian_16bits)
+inline uint16_t be16toh_(uint16_t big_endian_16bits)
 {
     return bswap_16(big_endian_16bits);
 }
 
-inline uint16_t le16toh(uint16_t little_endian_16bits)
+inline uint16_t le16toh_(uint16_t little_endian_16bits)
 {
     return little_endian_16bits;
 }
 
-inline uint32_t htobe32(uint32_t host_32bits)
+inline uint32_t htobe32_(uint32_t host_32bits)
 {
     return bswap_32(host_32bits);
 }
 
-inline uint32_t htole32(uint32_t host_32bits)
+inline uint32_t htole32_(uint32_t host_32bits)
 {
     return host_32bits;
 }
 
-inline uint32_t be32toh(uint32_t big_endian_32bits)
+inline uint32_t be32toh_(uint32_t big_endian_32bits)
 {
     return bswap_32(big_endian_32bits);
 }
 
-inline uint32_t le32toh(uint32_t little_endian_32bits)
+inline uint32_t le32toh_(uint32_t little_endian_32bits)
 {
     return little_endian_32bits;
 }
 
-inline uint64_t htobe64(uint64_t host_64bits)
+inline uint64_t htobe64_(uint64_t host_64bits)
 {
     return bswap_64(host_64bits);
 }
 
-inline uint64_t htole64(uint64_t host_64bits)
+inline uint64_t htole64_(uint64_t host_64bits)
 {
     return host_64bits;
 }
 
-inline uint64_t be64toh(uint64_t big_endian_64bits)
+inline uint64_t be64toh_(uint64_t big_endian_64bits)
 {
     return bswap_64(big_endian_64bits);
 }
 
-inline uint64_t le64toh(uint64_t little_endian_64bits)
+inline uint64_t le64toh_(uint64_t little_endian_64bits)
 {
     return little_endian_64bits;
 }
