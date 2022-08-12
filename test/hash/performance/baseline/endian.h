532c94057f5a6c9b4a0c27dfdbd1c0dbc90107be
evoskuil <eric@voskuil.org>
2022-08-12 14:58:03 -0700

    Delint baseline perf endian warning.

--- test/hash/performance/baseline/endian.h
+++ test/hash/performance/baseline/endian.h
@@ -12,139 +12,103 @@
 BC_PUSH_WARNING(USE_CONSTEXPR_FOR_FUNCTION)
 BC_PUSH_WARNING(USE_NOEXCEPT_FOR_FUNCTION)
 
-#define LITTLE_END 0x41424344UL 
-#define BIG_END    0x44434241UL
-#define ENDIAN_ORDER  ('ABCD')
-
 namespace baseline {
 
-#if (ENDIAN_ORDER==BIG_END)
-
 inline uint16_t htobe16_(uint16_t host_16bits)
 {
-    return host_16bits;
+    if constexpr (is_big_endian)
+        return host_16bits;
+    else
+        return bswap_16(host_16bits);
 }
 
 inline uint16_t htole16_(uint16_t host_16bits)
 {
-    return bswap_16(host_16bits);
+    if constexpr (is_big_endian)
+        return bswap_16(host_16bits);
+    else
+        return host_16bits;
 }
 
 inline uint16_t be16toh_(uint16_t big_endian_16bits)
 {
-    return big_endian_16bits;
+    if constexpr (is_big_endian)
+        return big_endian_16bits;
+    else
+        return bswap_16(big_endian_16bits);
 }
 
 inline uint16_t le16toh_(uint16_t little_endian_16bits)
 {
-    return bswap_16(little_endian_16bits);
+    if constexpr (is_big_endian)
+        return bswap_16(little_endian_16bits);
+    else
+        return little_endian_16bits;
 }
 
 inline uint32_t htobe32_(uint32_t host_32bits)
 {
-    return host_32bits;
+    if constexpr (is_big_endian)
+        return host_32bits;
+    else
+        return bswap_32(host_32bits);
 }
 
 inline uint32_t htole32_(uint32_t host_32bits)
 {
-    return bswap_32(host_32bits);
+    if constexpr (is_big_endian)
+        return bswap_32(host_32bits);
+    else
+        return host_32bits;
 }
 
 inline uint32_t be32toh_(uint32_t big_endian_32bits)
 {
-    return big_endian_32bits;
+    if constexpr (is_big_endian)
+        return big_endian_32bits;
+    else
+        return bswap_32(big_endian_32bits);
 }
 
 inline uint32_t le32toh_(uint32_t little_endian_32bits)
 {
-    return bswap_32(little_endian_32bits);
+    if constexpr (is_big_endian)
+        return bswap_32(little_endian_32bits);
+    else
+        return little_endian_32bits;
 }
 
 inline uint64_t htobe64_(uint64_t host_64bits)
 {
-    return host_64bits;
+    if constexpr (is_big_endian)
+        return host_64bits;
+    else
+        return bswap_64(host_64bits);
 }
 
 inline uint64_t htole64_(uint64_t host_64bits)
 {
-    return bswap_64(host_64bits);
+    if constexpr (is_big_endian)
+        return bswap_64(host_64bits);
+    else
+        return host_64bits;
 }
 
 inline uint64_t be64toh_(uint64_t big_endian_64bits)
 {
-    return big_endian_64bits;
+    if constexpr (is_big_endian)
+        return big_endian_64bits;
+    else
+        return bswap_64(big_endian_64bits);
 }
 
 inline uint64_t le64toh_(uint64_t little_endian_64bits)
 {
-    return bswap_64(little_endian_64bits);
+    if constexpr (is_big_endian)
+        return bswap_64(little_endian_64bits);
+    else
+        return little_endian_64bits;
 }
-
-#elif (ENDIAN_ORDER==LITTLE_END)
-
-inline uint16_t htobe16_(uint16_t host_16bits)
-{
-    return bswap_16(host_16bits);
-}
-
-inline uint16_t htole16_(uint16_t host_16bits)
-{
-    return host_16bits;
-}
-
-inline uint16_t be16toh_(uint16_t big_endian_16bits)
-{
-    return bswap_16(big_endian_16bits);
-}
-
-inline uint16_t le16toh_(uint16_t little_endian_16bits)
-{
-    return little_endian_16bits;
-}
-
-inline uint32_t htobe32_(uint32_t host_32bits)
-{
-    return bswap_32(host_32bits);
-}
-
-inline uint32_t htole32_(uint32_t host_32bits)
-{
-    return host_32bits;
-}
-
-inline uint32_t be32toh_(uint32_t big_endian_32bits)
-{
-    return bswap_32(big_endian_32bits);
-}
-
-inline uint32_t le32toh_(uint32_t little_endian_32bits)
-{
-    return little_endian_32bits;
-}
-
-inline uint64_t htobe64_(uint64_t host_64bits)
-{
-    return bswap_64(host_64bits);
-}
-
-inline uint64_t htole64_(uint64_t host_64bits)
-{
-    return host_64bits;
-}
-
-inline uint64_t be64toh_(uint64_t big_endian_64bits)
-{
-    return bswap_64(big_endian_64bits);
-}
-
-inline uint64_t le64toh_(uint64_t little_endian_64bits)
-{
-    return little_endian_64bits;
-}
-
-#else
-static_assert(false, "unsupported endianness");
-#endif // ENDIAN_ORDER
 
 } // baseline
 
