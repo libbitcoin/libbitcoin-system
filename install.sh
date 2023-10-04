e9dfe4e9e20ff3a0528a015599fd4323db1b71d7
Phillip Mienk <mienkphi@gmail.com>
2023-10-04 14:21:59 -0700

    Regenerate with BOOST_ROOT assignemnt via installation scripts.

--- install.sh
+++ install.sh
@@ -389,6 +389,7 @@ set_with_boost_prefix()
         # ax_boost_base.m4 appends /include and adds to BOOST_CPPFLAGS
         # ax_boost_base.m4 searches for /lib /lib64 and adds to BOOST_LDFLAGS
         with_boost="--with-boost=$PREFIX"
+        export BOOST_ROOT="$PREFIX"
     fi
 }
 
@@ -408,6 +409,7 @@ display_configuration()
     display_message "WITH_ICU              : $WITH_ICU"
     display_message "BUILD_ICU             : $BUILD_ICU"
     display_message "BUILD_BOOST           : $BUILD_BOOST"
+    display_message "BOOST_ROOT            : $BOOST_ROOT"
     display_message "BUILD_DIR             : $BUILD_DIR"
     display_message "PREFIX                : $PREFIX"
     display_message "DISABLE_SHARED        : $DISABLE_SHARED"
