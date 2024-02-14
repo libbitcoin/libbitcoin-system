ebca31fba64a5a5175cffe14209006bdf485340e
evoskuil <eric@voskuil.org>
2024-02-14 10:32:20 -0500

    Regenerate artifacts.

--- build.cmd
+++ build.cmd
@@ -11,10 +11,10 @@ SET "relative_path_base=%~1"
 call cd /d "%relative_path_base%"
 SET "path_base=%cd%"
 SET "nuget_pkg_path=%path_base%\.nuget\packages"
-SET "msbuild_args=/verbosity:minimal /p:Platform=%~2 /p:Configuration=%~3"
-SET "proj_version=%~4"
+SET "msbuild_args=/verbosity:minimal /p:Platform=%~2 /p:Configuration=%~3 /p:PreferredToolArchitecture=%~4"
+SET "proj_version=%~5"
 SET "msbuild_exe=msbuild"
-IF EXIST "%~5" SET "msbuild_exe=%~5"
+IF EXIST "%~6" SET "msbuild_exe=%~6"
 
 call :pending "Build initialized..."
 IF NOT EXIST "%nuget_pkg_path%" (
