5beb479bb54f77d0842f32638d348f137b55c150
evoskuil <eric@voskuil.org>
2024-11-21 21:46:51 -0500

    Comment.

--- include/bitcoin/system/typelets.hpp
+++ include/bitcoin/system/typelets.hpp
@@ -85,7 +85,7 @@ constexpr size_t bits = to_bits(sizeof(Type));
 template <size_t Bits, std::enable_if_t<is_byte_sized(Bits), bool> = true>
 constexpr size_t bytes = Bits / byte_bits;
 
-/// The number of Smaller integrals that can pack into the Larger.
+/// The number of Smaller types that can pack into the Larger.
 template <typename Larger, typename Smaller, size_t Lanes = one,
     std::enable_if_t<!is_zero(Lanes), bool> = true,
     std::enable_if_t<Lanes <= (max_size_t / sizeof(Smaller)), bool> = true>
