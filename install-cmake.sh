758dc895660379818ecc3d090d4ca55f81e1a2d8
Phillip Mienk <mienkphi@gmail.com>
2025-01-04 00:44:04 -0800

    Regenerate artifacts.

--- install-cmake.sh
+++ install-cmake.sh
@@ -38,6 +38,11 @@
 
 # Define constants.
 #==============================================================================
+# Branches for github dependencies.
+#------------------------------------------------------------------------------
+SECP256K1_BRANCH="v0.5.1"
+BITCOIN_SYSTEM_BRANCH="master"
+
 # Sentinel for comparison of sequential build.
 #------------------------------------------------------------------------------
 SEQUENTIAL=1
@@ -919,7 +924,7 @@ build_all()
     export CPPFLAGS="$CPPFLAGS ${BOOST_FLAGS[@]}"
     build_from_tarball_boost "$BOOST_ARCHIVE" "$PARALLEL" "$BUILD_BOOST" "${BOOST_OPTIONS[@]}"
     export CPPFLAGS=$SAVE_CPPFLAGS
-    create_from_github bitcoin-core secp256k1 v0.5.1 "$BUILD_SECP256K1"
+    create_from_github bitcoin-core secp256k1 ${SECP256K1_BRANCH} "$BUILD_SECP256K1"
     local SAVE_CPPFLAGS="$CPPFLAGS"
     export CPPFLAGS="$CPPFLAGS ${SECP256K1_FLAGS[@]}"
     build_from_github secp256k1 "$PARALLEL" false "$BUILD_SECP256K1" "${SECP256K1_OPTIONS[@]}" $CUMULATIVE_FILTERED_ARGS
@@ -927,7 +932,7 @@ build_all()
     local SAVE_CPPFLAGS="$CPPFLAGS"
     export CPPFLAGS="$CPPFLAGS ${BITCOIN_SYSTEM_FLAGS[@]}"
     if [[ ! ($CI == true) ]]; then
-        create_from_github libbitcoin libbitcoin-system master "yes"
+        create_from_github libbitcoin libbitcoin-system ${BITCOIN_SYSTEM_BRANCH} "yes"
         build_from_github_cmake libbitcoin-system "$PARALLEL" true "yes" "${BITCOIN_SYSTEM_OPTIONS[@]}" $CUMULATIVE_FILTERED_ARGS_CMAKE "$@"
     else
         push_directory "$PRESUMED_CI_PROJECT_PATH"
--- install-cmakepresets.sh
+++ install-cmakepresets.sh
@@ -39,6 +39,11 @@
 
 # Define constants.
 #==============================================================================
+# Branches for github dependencies.
+#------------------------------------------------------------------------------
+SECP256K1_BRANCH="v0.5.1"
+BITCOIN_SYSTEM_BRANCH="master"
+
 
 # Declare associative array for computed presets.
 #------------------------------------------------------------------------------
@@ -962,7 +967,7 @@ build_all()
     export CPPFLAGS="$CPPFLAGS ${BOOST_FLAGS[@]}"
     build_from_tarball_boost "$BOOST_ARCHIVE" "$PARALLEL" "$BUILD_BOOST" "${BOOST_OPTIONS[@]}"
     export CPPFLAGS=$SAVE_CPPFLAGS
-    create_from_github bitcoin-core secp256k1 v0.5.1 "$BUILD_SECP256K1"
+    create_from_github bitcoin-core secp256k1 ${SECP256K1_BRANCH} "$BUILD_SECP256K1"
     local SAVE_CPPFLAGS="$CPPFLAGS"
     export CPPFLAGS="$CPPFLAGS ${SECP256K1_FLAGS[@]}"
     build_from_github secp256k1 "$PARALLEL" false "$BUILD_SECP256K1" "${SECP256K1_OPTIONS[@]}" $CUMULATIVE_FILTERED_ARGS
@@ -970,7 +975,7 @@ build_all()
     local SAVE_CPPFLAGS="$CPPFLAGS"
     export CPPFLAGS="$CPPFLAGS ${BITCOIN_SYSTEM_FLAGS[@]}"
     if [[ ! ($CI == true) ]]; then
-        create_from_github libbitcoin libbitcoin-system master "yes"
+        create_from_github libbitcoin libbitcoin-system ${BITCOIN_SYSTEM_BRANCH} "yes"
         display_message "libbitcoin-system PRESET ${REPO_PRESET[libbitcoin-system]}"
         build_from_github_cmake libbitcoin-system ${REPO_PRESET[libbitcoin-system]} "$PARALLEL" true                 "yes" "${BITCOIN_SYSTEM_OPTIONS[@]}" $CUMULATIVE_FILTERED_ARGS_CMAKE "$@"
     else
