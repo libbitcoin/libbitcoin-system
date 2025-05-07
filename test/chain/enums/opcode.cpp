19886bf0e221e75a90a4bb9a7d6893f5f67f09e0
evoskuil <eric@voskuil.org>
2025-05-06 21:04:39 -0400

    Conditionally serialize op_checksigadd.

--- test/chain/enums/opcode.cpp
+++ test/chain/enums/opcode.cpp
@@ -29,182 +29,162 @@ using namespace system::chain;
 // Use the traditional serializations for all codes (in this case 'zero' vs. 'push_0').
 BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__zero_any_forks__zero)
 {
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::push_size_0, flags::no_rules) == "zero");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::push_size_0, flags::all_rules) == "zero");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::push_size_0, flags::no_rules), "zero");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::push_size_0, flags::all_rules), "zero");
 }
 
 // We formerly serialized all 1-75 as 'special'.
 BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__push_size_42_any_forks__push_42)
 {
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::push_size_42, flags::no_rules) == "push_42");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::push_size_42, flags::all_rules) == "push_42");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::push_size_42, flags::no_rules), "push_42");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::push_size_42, flags::all_rules), "push_42");
 }
 
 // Use the traditional serializations for all codes (in this case 'pushdata1' vs. 'push_one').
 BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__push_one_size_any_forks__pushdata1)
 {
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::push_one_size, flags::no_rules) == "pushdata1");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::push_one_size, flags::all_rules) == "pushdata1");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::push_one_size, flags::no_rules), "pushdata1");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::push_one_size, flags::all_rules), "pushdata1");
 }
 
 // Use the traditional serializations for all codes (in this case 'pushdata2' vs. 'push_two').
 BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__push_two_size_size_any_forks__pushdata2)
 {
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::push_two_size, flags::no_rules) == "pushdata2");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::push_two_size, flags::all_rules) == "pushdata2");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::push_two_size, flags::no_rules), "pushdata2");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::push_two_size, flags::all_rules), "pushdata2");
 }
 
 // Use the traditional serializations for all codes (in this case 'pushdata4' vs. 'push_four').
 BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__push_four_size_any_forks__pushdata4)
 {
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::push_four_size, flags::no_rules) == "pushdata4");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::push_four_size, flags::all_rules) == "pushdata4");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::push_four_size, flags::no_rules), "pushdata4");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::push_four_size, flags::all_rules), "pushdata4");
 }
 
 // Use the traditional serializations for all codes (in this case 'reserved' vs. 0x50).
 BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__reserved_80_any_forks__reserved)
 {
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::reserved_80, flags::no_rules) == "reserved");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::reserved_80, flags::all_rules) == "reserved");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::reserved_80, flags::no_rules), "reserved");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::reserved_80, flags::all_rules), "reserved");
 }
 
 BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__push_positive_7_any_forks__7)
 {
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::push_positive_7, flags::no_rules) == "7");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::push_positive_7, flags::all_rules) == "7");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::push_positive_7, flags::no_rules), "7");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::push_positive_7, flags::all_rules), "7");
 }
 
 // Use the traditional serializations for all codes (in this case 'ver' vs. '0x62').
 BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__op_ver_any_forks__ver)
 {
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::op_ver, flags::no_rules) == "ver");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::op_ver, flags::all_rules) == "ver");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::op_ver, flags::no_rules), "ver");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::op_ver, flags::all_rules), "ver");
 }
 
 // Use the traditional serializations for all codes (in this case 'verif' vs. '0x65').
 BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__op_verif_any_forks__verif)
 {
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::op_verif, flags::no_rules) == "verif");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::op_verif, flags::all_rules) == "verif");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::op_verif, flags::no_rules), "verif");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::op_verif, flags::all_rules), "verif");
 }
 
 // Use the traditional serializations for all codes (in this case 'vernotif' vs. '0x66').
 BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__op_vernotif_any_forks__vernotif)
 {
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::op_vernotif, flags::no_rules) == "vernotif");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::op_vernotif, flags::all_rules) == "vernotif");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::op_vernotif, flags::no_rules), "vernotif");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::op_vernotif, flags::all_rules), "vernotif");
 }
 
 // Use the traditional serializations for all codes (in this case '2drop' vs. 'drop2').
 BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__drop2_any_forks__2drop)
 {
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::drop2, flags::no_rules) == "2drop");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::drop2, flags::all_rules) == "2drop");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::drop2, flags::no_rules), "2drop");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::drop2, flags::all_rules), "2drop");
 }
 
 // Use the traditional serializations for all codes (in this case '2dup' vs. 'dup2').
 BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__rdup2_any_forks__2dup)
 {
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::dup2, flags::no_rules) == "2dup");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::dup2, flags::all_rules) == "2dup");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::dup2, flags::no_rules), "2dup");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::dup2, flags::all_rules), "2dup");
 }
 
 // Use the traditional serializations for all codes (in this case '3dup' vs. 'dup3').
 BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__dup3_any_forks__3dup)
 {
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::dup3, flags::no_rules) == "3dup");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::dup3, flags::all_rules) == "3dup");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::dup3, flags::no_rules), "3dup");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::dup3, flags::all_rules), "3dup");
 }
 
 // Use the traditional serializations for all codes (in this case '2over' vs. 'over2').
 BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__over2_any_forks__vernotif)
 {
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::over2, flags::no_rules) == "2over");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::over2, flags::all_rules) == "2over");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::over2, flags::no_rules), "2over");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::over2, flags::all_rules), "2over");
 }
 
 // Use the traditional serializations for all codes (in this case '2rot' vs. 'rot2').
 BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__rot2_any_forks__2rot)
 {
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::rot2, flags::no_rules) == "2rot");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::rot2, flags::all_rules) == "2rot");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::rot2, flags::no_rules), "2rot");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::rot2, flags::all_rules), "2rot");
 }
 
 // Use the traditional serializations for all codes (in this case '2swap' vs. 'swap2').
 BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__swap2_any_forks__2swap)
 {
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::swap2, flags::no_rules) == "2swap");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::swap2, flags::all_rules) == "2swap");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::swap2, flags::no_rules), "2swap");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::swap2, flags::all_rules), "2swap");
 }
 
 // Use the traditional serializations for all codes (in this case '1add' vs. 'add1').
 BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__add1_any_forks__2rot)
 {
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::add1, flags::no_rules) == "1add");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::add1, flags::all_rules) == "1add");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::add1, flags::no_rules), "1add");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::add1, flags::all_rules), "1add");
 }
 
 // Use the traditional serializations for all codes (in this case '1sub' vs. 'sub1').
 BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__sub1_any_forks__2swap)
 {
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::sub1, flags::no_rules) == "1sub");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::sub1, flags::all_rules) == "1sub");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::sub1, flags::no_rules), "1sub");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::sub1, flags::all_rules), "1sub");
 }
 
-// Ensure nop2 still serializes as 'nop2' without bip65 fork.
-BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__nop2_or_checklocktimeverify_any_fork_except_bip65__nop2)
+// All nop1 to nop10.
+BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__nops_any_forks__nop)
 {
-    static_assert(opcode::checklocktimeverify == opcode::nop2, "nop2 drift");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::nop2, flags::no_rules) == "nop2");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::nop2, flags::bip16_rule) == "nop2");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::nop2, flags::bip30_rule) == "nop2");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::nop2, flags::bip34_rule) == "nop2");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::nop2, flags::bip42_rule) == "nop2");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::nop2, flags::bip66_rule) == "nop2");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::nop2, flags::bip112_rule) == "nop2");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::nop1, flags::no_rules), "nop1");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::nop1, flags::all_rules), "nop1");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::nop2, flags::no_rules), "nop2");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::nop2, flags::all_rules), "checklocktimeverify");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::nop3, flags::no_rules), "nop3");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::nop3, flags::all_rules), "checksequenceverify");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::nop4, flags::no_rules), "nop4");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::nop4, flags::all_rules), "nop4");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::nop5, flags::no_rules), "nop5");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::nop5, flags::all_rules), "nop5");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::nop6, flags::no_rules), "nop6");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::nop6, flags::all_rules), "nop6");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::nop7, flags::no_rules), "nop7");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::nop7, flags::all_rules), "nop7");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::nop8, flags::no_rules), "nop8");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::nop8, flags::all_rules), "nop8");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::nop9, flags::no_rules), "nop9");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::nop9, flags::all_rules), "nop9");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::nop10, flags::no_rules), "nop10");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::nop10, flags::all_rules), "nop10");
 }
 
-// Ensure nop2 and checklocktimeverify serialize as 'checklocktimeverify' with bip65 fork.
-BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__nop2_or_checklocktimeverify_bip65_fork__checklocktimeverify)
+// All reserved_186 to reserved_255.
+BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__reserved_186_to_255_any_forks__hex)
 {
-    static_assert(opcode::checklocktimeverify == opcode::nop2, "nop2 drift");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::nop2, flags::bip65_rule) == "checklocktimeverify");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::nop2, flags::all_rules) == "checklocktimeverify");
-}
-
-// Ensure nop3 still serializes as 'nop3' without bip112 fork.
-BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__nop3_or_checksequenceverify_any_fork_except_bip112__nop3)
-{
-    static_assert(opcode::checksequenceverify == opcode::nop3, "nop3 drift");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::nop3, flags::no_rules) == "nop3");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::nop3, flags::bip16_rule) == "nop3");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::nop3, flags::bip30_rule) == "nop3");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::nop3, flags::bip34_rule) == "nop3");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::nop2, flags::bip42_rule) == "nop2");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::nop3, flags::bip66_rule) == "nop3");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::nop3, flags::bip65_rule) == "nop3");
-}
-
-// Ensure nop3 and checksequenceverify serialize as 'checksequenceverify' with bip112 fork.
-BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__nop3_or_checksequenceverify_bip112_fork__checksequenceverify)
-{
-    static_assert(opcode::checksequenceverify == opcode::nop3, "nop3 drift");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::nop3, flags::bip112_rule) == "checksequenceverify");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::nop3, flags::all_rules) == "checksequenceverify");
-}
-
-// All codes above 'nop10' serialize as hex.
-BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__reserved_186_any_forks__0xba)
-{
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::reserved_186, flags::no_rules) == "0xba");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::reserved_186, flags::all_rules) == "0xba");
-}
-
-// All codes above 'nop10' serialize as hex.
-BOOST_AUTO_TEST_CASE(opcode__opcode_to_mnemonic__reserved_255_any_forks__0xff)
-{
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::reserved_255, flags::no_rules) == "0xff");
-    BOOST_REQUIRE(opcode_to_mnemonic(opcode::reserved_255, flags::all_rules) == "0xff");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::reserved_186, flags::no_rules), "0xba");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::reserved_186, flags::all_rules), "checksigadd");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::reserved_187, flags::no_rules), "0xbb");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::reserved_187, flags::all_rules), "0xbb");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::reserved_255, flags::no_rules), "0xff");
+    BOOST_REQUIRE_EQUAL(opcode_to_mnemonic(opcode::reserved_255, flags::all_rules), "0xff");
 }
 
 // opcode_from_mnemonic
@@ -552,6 +532,7 @@ BOOST_AUTO_TEST_CASE(opcode__opcode_from_mnemonic__op_vernotif__reserved_138)
 }
 
 // nop2/checklocktimeverify
+static_assert(opcode::checklocktimeverify == opcode::nop2, "checklocktimeverify drift");
 
 BOOST_AUTO_TEST_CASE(opcode__opcode_from_mnemonic__nop2__nop2)
 {
@@ -562,13 +543,13 @@ BOOST_AUTO_TEST_CASE(opcode__opcode_from_mnemonic__nop2__nop2)
 
 BOOST_AUTO_TEST_CASE(opcode__opcode_from_mnemonic__checklocktimeverify__nop2_or_checklocktimeverify)
 {
-    static_assert(opcode::checklocktimeverify == opcode::nop2, "nop2 drift");
     opcode out_code;
     BOOST_REQUIRE(opcode_from_mnemonic(out_code, "checklocktimeverify"));
     BOOST_REQUIRE(out_code == opcode::nop2);
 }
 
 // nop3/checksequenceverify
+static_assert(opcode::checksequenceverify == opcode::nop3, "checksequenceverify drift");
 
 BOOST_AUTO_TEST_CASE(opcode__opcode_from_mnemonic__nop3__nop3)
 {
@@ -579,10 +560,26 @@ BOOST_AUTO_TEST_CASE(opcode__opcode_from_mnemonic__nop3__nop3)
 
 BOOST_AUTO_TEST_CASE(opcode__opcode_from_mnemonic__checksequenceverify__nop3_or_checksequenceverify)
 {
-    static_assert(opcode::checksequenceverify == opcode::nop3, "nop3 drift");
     opcode out_code;
     BOOST_REQUIRE(opcode_from_mnemonic(out_code, "checksequenceverify"));
     BOOST_REQUIRE(out_code == opcode::nop3);
+}
+
+// reserved_186/checksigadd
+static_assert(opcode::checksigadd == opcode::reserved_186, "checksigadd drift");
+
+BOOST_AUTO_TEST_CASE(opcode__opcode_from_mnemonic__reserved_186__reserved_186)
+{
+    opcode out_code;
+    BOOST_REQUIRE(opcode_from_mnemonic(out_code, "reserved_186"));
+    BOOST_REQUIRE(out_code == opcode::reserved_186);
+}
+
+BOOST_AUTO_TEST_CASE(opcode__opcode_from_mnemonic__checksigadd__reserved_186_or_checksigadd)
+{
+    opcode out_code;
+    BOOST_REQUIRE(opcode_from_mnemonic(out_code, "checksigadd"));
+    BOOST_REQUIRE(out_code == opcode::reserved_186);
 }
 
 // opcode_to_hexadecimal
@@ -590,17 +587,17 @@ BOOST_AUTO_TEST_CASE(opcode__opcode_from_mnemonic__checksequenceverify__nop3_or_
 
 BOOST_AUTO_TEST_CASE(opcode__opcode_to_hexadecimal__zero__0x00)
 {
-    BOOST_REQUIRE(opcode_to_hexadecimal(opcode::push_size_0) == "0x00");
+    BOOST_REQUIRE_EQUAL(opcode_to_hexadecimal(opcode::push_size_0), "0x00");
 }
 
 BOOST_AUTO_TEST_CASE(opcode__opcode_to_hexadecimal__push_size_42__0x2a)
 {
-    BOOST_REQUIRE(opcode_to_hexadecimal(opcode::push_size_42) == "0x2a");
+    BOOST_REQUIRE_EQUAL(opcode_to_hexadecimal(opcode::push_size_42), "0x2a");
 }
 
 BOOST_AUTO_TEST_CASE(opcode__opcode_to_hexadecimal__reserved_255__0xff)
 {
-    BOOST_REQUIRE(opcode_to_hexadecimal(opcode::reserved_255) == "0xff");
+    BOOST_REQUIRE_EQUAL(opcode_to_hexadecimal(opcode::reserved_255), "0xff");
 }
 
 // opcode_from_hexadecimal
