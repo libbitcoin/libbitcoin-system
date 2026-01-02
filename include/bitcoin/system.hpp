12b2201357998bfdabc4f86a5528ed26515026f2
evoskuil <eric@voskuil.org>
2026-01-02 18:01:53 -0500

    Regenerate artifacts (bump secp256k1 versions).

--- include/bitcoin/system.hpp
+++ include/bitcoin/system.hpp
@@ -86,6 +86,7 @@
 #include <bitcoin/system/config/transaction.hpp>
 #include <bitcoin/system/config/url.hpp>
 #include <bitcoin/system/config/utilities.hpp>
+#include <bitcoin/system/config/version.hpp>
 #include <bitcoin/system/crypto/aes256.hpp>
 #include <bitcoin/system/crypto/crypto.hpp>
 #include <bitcoin/system/crypto/der_parser.hpp>
