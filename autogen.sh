c6e44442c1355fd0b7d9681e34a6998785ea025a
Phillip Mienk <mienkphi@gmail.com>
2026-04-24 20:28:04 -0700

    Regenerate with CMakePresets correction, minor other changes.

--- autogen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-###############################################################################
-#  Copyright (c) 2014-2026 libbitcoin-system developers (see COPYING).
-#
-#         GENERATED SOURCE CODE, DO NOT EDIT EXCEPT EXPERIMENTALLY
-#
-###############################################################################
-
-autoreconf -i
