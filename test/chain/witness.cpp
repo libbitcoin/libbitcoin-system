6790c5455de4ad4a461fc22dc4d9dc1a7ee581e6
evoskuil <eric@voskuil.org>
2026-05-28 00:56:06 -0400

    Fix up test side effect.

--- test/chain/witness.cpp
+++ test/chain/witness.cpp
@@ -24,18 +24,11 @@ BOOST_AUTO_TEST_SUITE(witness_tests)
 namespace json = boost::json;
 using namespace system::chain;
 
-const data_chunk& get_data(bool witness) NOEXCEPT
-{
-    static const auto data = test::mock_block_c.to_data(witness);
-    return data;
-}
-
 BOOST_AUTO_TEST_CASE(witness__block__with_witness__parse_round_trips)
 {
     constexpr auto witness = true;
     const auto& instance1 = test::mock_block_c;
-    const auto& data = get_data(witness);
-
+    const auto data = test::mock_block_c.to_data(witness);
     const block instance2(data, witness);
     BOOST_REQUIRE(instance1.is_valid());
     BOOST_REQUIRE(instance2.is_valid());
@@ -51,9 +44,8 @@ BOOST_AUTO_TEST_CASE(witness__block__genesis_block__arena_parse_round_trips)
     const auto genesis = settings(selection::mainnet).genesis_block;
     const auto data = genesis.to_data(true);
 
-    test::reporting_arena<false> arena{};
     stream::in::fast stream(data);
-    read::bytes::fast source(stream, &arena);
+    read::bytes::fast source(stream);
     const block instance(source, witness);
     BOOST_REQUIRE(!instance.check());
 }
@@ -61,24 +53,21 @@ BOOST_AUTO_TEST_CASE(witness__block__genesis_block__arena_parse_round_trips)
 BOOST_AUTO_TEST_CASE(witness__block__with_witness__checks)
 {
     constexpr auto witness = true;
-    test::reporting_arena<false> arena{};
-    const auto& data = get_data(witness);
+    const auto data = test::mock_block_c.to_data(witness);
     stream::in::fast stream(data);
-    read::bytes::fast source(stream, &arena);
+    read::bytes::fast source(stream);
     const block instance(source, witness);
     BOOST_REQUIRE(!instance.check());
 }
 
-// TODO:
-////BOOST_AUTO_TEST_CASE(witness__block__without_witness__checks)
-////{
-////    constexpr auto witness = false;
-////    test::reporting_arena<false> arena{};
-////    const auto& data = get_data(witness);
-////    stream::in::fast stream(data);
-////    read::bytes::fast source(stream, &arena);
-////    const block instance(source, witness);
-////    BOOST_REQUIRE(!instance.check());
-////}
+BOOST_AUTO_TEST_CASE(witness__block__without_witness__checks)
+{
+    constexpr auto witness = false;
+    const auto data = test::mock_block_c.to_data(witness);
+    stream::in::fast stream(data);
+    read::bytes::fast source(stream);
+    const block instance(source, witness);
+    BOOST_REQUIRE(!instance.check());
+}
 
 BOOST_AUTO_TEST_SUITE_END()
