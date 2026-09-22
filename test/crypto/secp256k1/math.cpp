9361ad9ad973646e032608e13c972e9ca36ed0b6
Eric Voskuil <eric@voskuil.org>
2026-09-22 01:42:41 -0400

    Add hash accumulator runtime and rmd streaming tests.

--- test/crypto/secp256k1/math.cpp
+++ test/crypto/secp256k1/math.cpp
@@ -93,4 +93,112 @@ BOOST_AUTO_TEST_CASE(secp256k1__ec_multiply__expected)
 
 // negation
 
+
+// uncompressed and point-to-point overloads
+
+BOOST_AUTO_TEST_CASE(secp256k1__ec_add__uncompressed_point_secret__expected)
+{
+    const ec_secret secret{ { 1, 2, 3 } };
+    const ec_secret tweak{ { 3, 2, 1 } };
+
+    ec_compressed compressed;
+    BOOST_REQUIRE(secret_to_public(compressed, secret));
+    ec_uncompressed uncompressed;
+    BOOST_REQUIRE(secret_to_public(uncompressed, secret));
+
+    BOOST_REQUIRE(ec_add(compressed, tweak));
+    BOOST_REQUIRE(ec_add(uncompressed, tweak));
+
+    ec_compressed expected;
+    BOOST_REQUIRE(compress(expected, uncompressed));
+    BOOST_REQUIRE_EQUAL(expected, compressed);
+}
+
+BOOST_AUTO_TEST_CASE(secp256k1__ec_add__compressed_points__matches_sum)
+{
+    const ec_secret one{ { 1, 2, 3 } };
+    const ec_secret two{ { 3, 2, 1 } };
+
+    ec_compressed left;
+    ec_compressed right;
+    BOOST_REQUIRE(secret_to_public(left, one));
+    BOOST_REQUIRE(secret_to_public(right, two));
+
+    ec_compressed expected;
+    BOOST_REQUIRE(ec_sum(expected, { left, right }));
+    BOOST_REQUIRE(ec_add(left, right));
+    BOOST_REQUIRE_EQUAL(left, expected);
+}
+
+BOOST_AUTO_TEST_CASE(secp256k1__ec_add__compressed_and_uncompressed_points__matches_sum)
+{
+    const ec_secret one{ { 1, 2, 3 } };
+    const ec_secret two{ { 3, 2, 1 } };
+
+    ec_compressed left;
+    ec_compressed right;
+    ec_uncompressed right_uncompressed;
+    BOOST_REQUIRE(secret_to_public(left, one));
+    BOOST_REQUIRE(secret_to_public(right, two));
+    BOOST_REQUIRE(secret_to_public(right_uncompressed, two));
+
+    ec_compressed expected;
+    BOOST_REQUIRE(ec_sum(expected, { left, right }));
+    BOOST_REQUIRE(ec_add(left, right_uncompressed));
+    BOOST_REQUIRE_EQUAL(left, expected);
+}
+
+BOOST_AUTO_TEST_CASE(secp256k1__ec_sum__empty__false)
+{
+    ec_compressed out;
+    BOOST_REQUIRE(!ec_sum(out, {}));
+}
+
+BOOST_AUTO_TEST_CASE(secp256k1__ec_sum__invalid_point__false)
+{
+    ec_compressed out;
+    BOOST_REQUIRE(!ec_sum(out, { ec_compressed{} }));
+}
+
+BOOST_AUTO_TEST_CASE(secp256k1__ec_multiply__uncompressed_point__matches_compressed)
+{
+    const ec_secret secret{ { 1, 2, 3 } };
+    const ec_secret factor{ { 3, 2, 1 } };
+
+    ec_compressed compressed;
+    ec_uncompressed uncompressed;
+    BOOST_REQUIRE(secret_to_public(compressed, secret));
+    BOOST_REQUIRE(secret_to_public(uncompressed, secret));
+
+    BOOST_REQUIRE(ec_multiply(compressed, factor));
+    BOOST_REQUIRE(ec_multiply(uncompressed, factor));
+
+    ec_compressed expected;
+    BOOST_REQUIRE(compress(expected, uncompressed));
+    BOOST_REQUIRE_EQUAL(expected, compressed);
+}
+
+BOOST_AUTO_TEST_CASE(secp256k1__ec_negate__secret_and_points__round_trips)
+{
+    const ec_secret secret{ { 1, 2, 3 } };
+
+    ec_secret negated{ secret };
+    BOOST_REQUIRE(ec_negate(negated));
+    BOOST_REQUIRE(ec_negate(negated));
+    BOOST_REQUIRE_EQUAL(negated, secret);
+
+    ec_compressed compressed;
+    BOOST_REQUIRE(secret_to_public(compressed, secret));
+    const auto expected_compressed = compressed;
+    BOOST_REQUIRE(ec_negate(compressed));
+    BOOST_REQUIRE(ec_negate(compressed));
+    BOOST_REQUIRE_EQUAL(compressed, expected_compressed);
+
+    ec_uncompressed uncompressed;
+    BOOST_REQUIRE(secret_to_public(uncompressed, secret));
+    const auto expected_uncompressed = uncompressed;
+    BOOST_REQUIRE(ec_negate(uncompressed));
+    BOOST_REQUIRE(ec_negate(uncompressed));
+    BOOST_REQUIRE_EQUAL(uncompressed, expected_uncompressed);
+}
 BOOST_AUTO_TEST_SUITE_END()
